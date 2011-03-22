b7792fabe9535e210ce6b7eeaec0f9b2f880d84d
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-03-22 10:34:30 +0100

    Monkeypatch ts init to not rebuild config
    See
    http://groups.google.com/group/thinking-sphinx/browse_thread/thread/70dd3a663c3b9792#

--- features/support/env_thinking_sphinx.rb
+++ features/support/env_thinking_sphinx.rb
@@ -2,4 +2,16 @@ Cucumber::Rails::World.use_transactional_fixtures = false
 
 # http://freelancing-god.github.com/ts/en/testing.html
 require 'cucumber/thinking_sphinx/external_world'
+
+# Monkeypatch thinking sphinx init in order to NOT overwrite
+# the config - otherwise all our custom changes to the config
+# are lost! See:
+# http://groups.google.com/group/thinking-sphinx/browse_thread/thread/70dd3a663c3b9792#
+class ThinkingSphinx::Test
+  def self.start
+    config.controller.index
+    config.controller.start
+  end
+end
+
 Cucumber::ThinkingSphinx::ExternalWorld.new
