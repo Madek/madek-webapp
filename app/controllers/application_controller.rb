cf134ee1a3c215f4328da2d57961bbc7e2c49a1e
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-05-18 15:37:35 +0200

    redirect back on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,23 +31,27 @@ class ApplicationController < ActionController::Base
 
   def root
     if logged_in?
-      # TODO refactor to UsersController#show and dry with MediaEntriesController#index
-      params[:per_page] ||= PER_PAGE.first
-      viewable_ids = current_user.accessible_resource_ids
-      @disabled_paginator = true # OPTIMIZE
-
-      search_options = {:per_page => (2**30), :star => true }
-      paginate_options = {:page => params[:page], :per_page => params[:per_page].to_i}
-      all_ids = MediaEntry.by_user(current_user).search_for_ids search_options
-      @my_media_entries_paginated_ids = (all_ids & viewable_ids).paginate(paginate_options)
-      @my_media_entries_json = Logic.data_for_page(@my_media_entries_paginated_ids, current_user).to_json
-      all_ids = MediaEntry.not_by_user(current_user).search_for_ids search_options
-      @accessible_media_entries_paginated_ids = (all_ids & viewable_ids).paginate(paginate_options)
-      @accessible_media_entries_json = Logic.data_for_page(@accessible_media_entries_paginated_ids, current_user).to_json
-      
-      respond_to do |format|
-        format.html { render :template => "/users/show" }
-        format.js { render :partial => "/media_entries/index" }
+      if session[:return_to]
+        redirect_back_or_default('/')
+      else
+        # TODO refactor to UsersController#show and dry with MediaEntriesController#index
+        params[:per_page] ||= PER_PAGE.first
+        viewable_ids = current_user.accessible_resource_ids
+        @disabled_paginator = true # OPTIMIZE
+  
+        search_options = {:per_page => (2**30), :star => true }
+        paginate_options = {:page => params[:page], :per_page => params[:per_page].to_i}
+        all_ids = MediaEntry.by_user(current_user).search_for_ids search_options
+        @my_media_entries_paginated_ids = (all_ids & viewable_ids).paginate(paginate_options)
+        @my_media_entries_json = Logic.data_for_page(@my_media_entries_paginated_ids, current_user).to_json
+        all_ids = MediaEntry.not_by_user(current_user).search_for_ids search_options
+        @accessible_media_entries_paginated_ids = (all_ids & viewable_ids).paginate(paginate_options)
+        @accessible_media_entries_json = Logic.data_for_page(@accessible_media_entries_paginated_ids, current_user).to_json
+        
+        respond_to do |format|
+          format.html { render :template => "/users/show" }
+          format.js { render :partial => "/media_entries/index" }
+        end
       end
     else
       render :layout => false
@@ -83,6 +87,7 @@ class ApplicationController < ActionController::Base
   
   def login_required
     unless logged_in?
+      store_location
       flash[:error] = "Bitte anmelden."
       redirect_to root_path
     end
