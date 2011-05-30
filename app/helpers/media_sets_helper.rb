8500eb634a0c05fe8b50acd585c1a4db2092ff54
Olivier Heitz <olivier.heitz@zhdk.ch>
2011-05-30 14:02:36 +0200

    - better typography

--- app/helpers/media_sets_helper.rb
+++ app/helpers/media_sets_helper.rb
@@ -8,8 +8,10 @@ module MediaSetsHelper
       div_class, thumb_class = media_set.is_a?(Media::Project) ? ["set-box project-box", "thumb_box_project"] : ["set-box", "thumb_box_set"]
       haml_tag :div, :class => div_class do
         haml_tag :div, thumb_for(media_set, :small_125), :class => thumb_class if with_main_thumb
-        haml_tag :span, media_set.title, :style => "font-weight: bold; font-size: 1.2em;"
+        haml_tag :span, media_set.title, :style => "font-weight: bold; font-size: 1.1em;"
         #2001# " (%d/%d Medieneinträge)" % [visible_media_entries.count, media_set.media_entries.count]
+        haml_tag :br
+        haml_tag :br
         haml_concat " (%d Medieneinträge)" % [media_set.media_entries.count]
         haml_tag :br
         haml_concat "von #{media_set.user}"
