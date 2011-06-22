e9708299174295382e470f9e27569f4f46140fd4
Ramón Cahenzli <rca@psy-q.ch>
2011-06-22 17:11:02 +0200

    [include=foo] includes the content from page 'foo' on wiki pages.

--- lib/red_cloth_madek.rb
+++ lib/red_cloth_madek.rb
@@ -13,29 +13,55 @@ class RedClothMadek
   # Transforms the follwing Textile markups:
   # 
   #   [media=210      | Das Huhn] -> <a href  ="/media_entries/210">Das Huhn</a>
-  #   [screenshot=210 | Das Huhn] -> <img src="/media_entries/210/image" title="Das Huhn"/>
+  #   [thumbnail=210  | Das Huhn] -> <img src="/media_entries/210/image" title="Das Huhn"/>
   #   [video=210      | Das Huhn] -> <video src="/media_entries/210/image" title="Das Huhn"/>
   #                                    <a href='/media_entries/210'>(see video)</a>
   #                                  </video>
+  #   [include=page] -> Whatever content is on page 'page'
   #
   def replace_madek_tags( text )
-
-    # unfortunately having multiple matches in gsub doesn't seem to work, therefore
-    # we fall back to $1 $2
-    #
-    text.gsub(/\[\s*media\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,txt|
-
-           "<a href='/media_entries/#{$1}'>#{h($2.strip)}</a>"                 }.
-
-         gsub(/\[\s*screenshot\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,title|
-
-           "<img src='/media_entries/#{$1}/image' title='#{h($2.strip)}'/>"    }.
-
-         gsub(/\[\s*video\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,title|
-           "<video src='/media_entries/#{$1}/image' title='#{h($2.strip)}'>" +
-             "<a href='/media_entries/#{$1}'>(see '#{h($2.strip)}' Video)</a>" +
-           "</video>"  }
+    text = media_tag(text)
+    text = thumbnail_tag(text)
+    text = video_tag(text)
+    text
   end
 
+  def media_tag(text)
+    # unfortunately having multiple matches in gsub doesn't seem to work, therefore
+    # we fall back to $1 $2
+    text.gsub(/\[\s*media\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { 
+      "<a href='/media_entries/#{$1}'>#{h($2.strip)}</a>"
+    }
+  end
+
+  def thumbnail_tag(text)
+    text.gsub(/\[\s*thumbnail\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { 
+      "<img src='/media_entries/#{$1}/image' title='#{h($2.strip)}'/>"
+    }
+  end
+
+  def include_tag(text)
+    text.gsub(/\[\s*include\s*=\s*(.+)\s*\]/) {
+      page = WikiPage.where(:path => $1).first
+      unless page.nil?
+        wiki_pages_helper = Object.new
+        wiki_pages_helper.send :extend, ActionView::Helpers::TextHelper
+        #wiki_pages_helper.send :extend, ActionView::Helpers::SanitizeHelper
+        wiki_pages_helper.send :extend, Irwi::Helpers::WikiPagesHelper
+        ActionController::Base.helpers.sanitize(wiki_pages_helper.auto_link( Irwi.config.formatter.format( wiki_pages_helper.wiki_linkify( page.content ) ) ))
+      else
+        return "Page '#{$1}' does not exist."
+      end
+    }
+  end
+
+  def video_tag(text)
+    text.gsub(/\[\s*video\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { 
+      "<video src='/media_entries/#{$1}/image' title='#{h($2.strip)}'>" +
+        "<a href='/media_entries/#{$1}'>(see '#{h($2.strip)}' Video)</a>" +
+      "</video>"
+    }
+  end
+  
 end
 
