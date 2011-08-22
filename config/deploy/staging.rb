c2fa26ba20230772f39e54da7b746a68186ccc29
Ramón Cahenzli <rca@psy-q.ch>
2011-08-22 10:29:08 +0200

    Even better RVM integration.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,7 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+set :rvm_type, :user  # Copy the exact line. I really mean :user here
 
 set :application, "madek"
 
