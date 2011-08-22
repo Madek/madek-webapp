a978fb36e212137ecbd69e0ca59f047fb3fa627e
Ramón Cahenzli <rca@psy-q.ch>
2011-08-22 15:54:59 +0200

    Moving towards Ruby 1.9.2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,6 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
-set :rvm_type, :user  # Copy the exact line. I really mean :user here
 
 require "bundler/capistrano"
 
