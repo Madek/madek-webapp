775009736b1655937b25999139e47b8606b63d10
Ramón Cahenzli <rca@psy-q.ch>
2011-08-30 15:14:13 +0200

    That was wrong in this branch.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 
 # Don't switch to 1.9.2 until we're 100% ready
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-#require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-#set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
 require "bundler/capistrano"
 
 set :application, "madek"
