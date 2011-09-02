b0c09674ac5cf6ed78652688eb9015e4b86ff95d
Ramón Cahenzli <rca@psy-q.ch>
2011-09-02 12:07:09 +0200

    Porting our RedCloth hack to Ruby 1.9.2, new regex backreferencing.

--- lib/red_cloth_madek.rb
+++ lib/red_cloth_madek.rb
@@ -23,17 +23,11 @@ class RedClothMadek
     # unfortunately having multiple matches in gsub doesn't seem to work, therefore
     # we fall back to $1 $2
     #
-    text.gsub(/\[\s*media\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,txt|
-
-           "<a href='/media_entries/#{$1}'>#{h($2.strip)}</a>"                 }.
-
-         gsub(/\[\s*screenshot\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,title|
-
-           "<img src='/media_entries/#{$1}/image' title='#{h($2.strip)}'/>"    }.
-
+    text.gsub(/\[\s*media\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/, "<a href='/media_entries/\\1'>\\2</a>").
+         gsub(/\[\s*screenshot\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/, "<img src='/media_entries/\\1/image' title='\\2'/>").
          gsub(/\[\s*video\s*=\s*(\d+)\s*\|\s*([^\]]+)\s*\]/) { |number,title|
-           "<video src='/media_entries/#{$1}/image' title='#{h($2.strip)}'>" +
-             "<a href='/media_entries/#{$1}'>(see '#{h($2.strip)}' Video)</a>" +
+           "<video src='/media_entries/\\1/image' title='\\2'>" +
+             "<a href='/media_entries/\\1'>(see '\\2' Video)</a>" +
            "</video>"  }
   end
 
