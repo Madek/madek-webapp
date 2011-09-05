c27a504ea35513dd19768349dc18eaa28a01c817
Ramón Cahenzli <rca@psy-q.ch>
2011-09-05 11:38:45 +0200

    Wrong branch.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,7 @@ set :application, "madek"
 
 set :scm, :git
 set :repository, "git://github.com/psy-q/madek.git"
-set :branch, "deployable"
+set :branch, "master"
 set :deploy_via, :remote_cache
 
 set :db_config, "/home/rails/madek-test/database.yml"
