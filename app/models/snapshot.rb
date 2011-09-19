111625baf5823f874988efd13dbc09eff32551d9
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-09-19 11:57:56 +0200

    snapshot meta_data clone

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -14,9 +14,7 @@ class Snapshot < ActiveRecord::Base
   
   after_create do
     media_entry.meta_data.each do |md|
-      nmd = md.clone
-      nmd.resource = self
-      nmd.save
+      meta_data.create(:meta_key_id => md.meta_key_id, :value => md.value )
     end
     descr_author_value = meta_data.get("description author").value
     meta_data.get("description author before snapshot").update_attributes(:value => descr_author_value) if descr_author_value
