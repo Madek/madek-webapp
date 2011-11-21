adf5524acfc5e6fe53af02882215a0e2b900550a
Thomas Schank <DrTom@schank.ch>
2011-11-21 15:10:35 +0100

    added user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     firstname {Faker::Name.first_name}
   end
 
+  factory :user do
+    person {FactoryGirl.create :person}
+    email {"#{person.firstname}.#{person.lastname}@example.com".downcase} 
+    login {email}
+  end
 
 end
