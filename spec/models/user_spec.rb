c21c33d73b4547ba29e75345243723b8639b1299
Thomas Schank <DrTom@schank.ch>
2011-11-23 14:06:45 +0100

    foreign key constraint user person, also added spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
+require 'sqlhelper'
 
 describe User do
 
   it "should be producible by a factory" do
     (FactoryGirl.create :user).should_not == nil
+  end
+
+  context "referential integrity" do
+
+    it "should raise an execption if the person is deleted on the database layer" do
+      user = FactoryGirl.create :user
+      expect { SQLHelper.execute_sql "DELETE FROM people WHERE ID = #{user.person.id};" }.to raise_error
+    end
+
   end
 
 end
