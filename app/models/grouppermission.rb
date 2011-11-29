15bf218167582dd935e569fdb28df18035e9b07b
Thomas Schank <DrTom@schank.ch>
2011-11-29 20:07:47 +0100

    adjuested gropuppermission model

--- app/models/grouppermission.rb
+++ app/models/grouppermission.rb
@@ -1,2 +1,5 @@
 class Grouppermission < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :resource, :polymorphic => true
+
 end
--- app/models/userpermission.rb
+++ app/models/userpermission.rb
@@ -1,6 +1,7 @@
-class Userpermission < ActiveRecord::Base
-
+class Userpermission < ActiveRecord::Base 
   belongs_to :user
   belongs_to :resource, :polymorphic => true
+
+
 
 end
