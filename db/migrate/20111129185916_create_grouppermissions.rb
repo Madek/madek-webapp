3c4a7a1e1a147c07dc767e9ab7f5519000845b5e
Thomas Schank <DrTom@schank.ch>
2011-11-29 20:06:44 +0100

    adjuested grouppermission migration

--- db/migrate/20111129185916_create_grouppermissions.rb
+++ db/migrate/20111129185916_create_grouppermissions.rb
@@ -1,8 +1,36 @@
 class CreateGrouppermissions < ActiveRecord::Migration
-  def change
+  def up
     create_table :grouppermissions do |t|
+
+      t.belongs_to  :resource, :polymorphic => true, :null => false
+      t.references :group, :null => false
+
+      t.boolean :may_view, :default => false
+      t.boolean :may_download, :default => false # high-res
+      t.boolean :may_edit_metadata, :default => false 
 
       t.timestamps
     end
+
+    # TODO can't constrain on fkey with polymorphic
+    sql = <<-SQL
+
+      ALTER TABLE grouppermissions ADD CONSTRAINT grouppermissions_group_id_fkey FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE;
+
+      CREATE INDEX grouppermissions_may_view_idx ON grouppermissions (may_view);
+
+      CREATE INDEX grouppermissions_resource_id_idx on grouppermissions (resource_id);
+      CREATE INDEX grouppermissions_group_id_idx on grouppermissions (group_id);
+
+    SQL
+
+    sql.split(/;\s*$/).each {|cmd| execute cmd} if SQLHelper.adapter_is_mysql?
+    execute sql if SQLHelper.adapter_is_postgresql?
+
+
+  end
+
+  def down
+    drop_table :grouppermissions
   end
 end
