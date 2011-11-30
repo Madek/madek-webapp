78e1a5bd74a11747c8d4dabaf6d664c0a118ade0
Thomas Schank <DrTom@schank.ch>
2011-11-30 14:46:23 +0100

    added add owner migration

--- spec/models/userpermission_spec.rb
+++ spec/models/userpermission_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Userpermission do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should be producible by a factory" do
+    (FactoryGirl.create :Userpermission).should_not == nil
+  end
+
+  context "consistency constraints " do
+    before :each do
+     @user  = FactoryGirl.create :user
+     @resource = FactoryGirl.create :media_set, :owner => (FactoryGirl.create :user)
+    end
+
+    it "should remove mediaresourceuserpermission if the user is destroyed" do
+      id = (FactoryGirl.create :mediaresourceuserpermission, :user => u, :mediaresource => mr).id
+      (Userppermission.find_by_id id).should_not be_nil
+      u.destroy
+      (Userppermission.find_by_id id).should be_nil
+    end
+
+    it "should remove mediaresourceuserpermission if the mediaresource is destroyed" do
+      id = (FactoryGirl.create :mediaresourceuserpermission, :user => u, :mediaresource => mr).id
+      (Userppermission.find_by_id id).should_not be_nil
+      mr.destroy
+      (Userppermission.find_by_id id).should be_nil
+    end
+
+  end
+
 end
