9fc232ba2524ec71cd13a0c5e1ba7afdb909cdf9
Thomas Schank <DrTom@schank.ch>
2011-12-02 13:55:01 +0100

    uses FactoryGrirl hooks to replace model factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,42 +9,31 @@ module FactoryHelper
   end
 end
 
-module ModelFactory 
-
-  # some more complicated factories are defined here; e.g. we define methods in
-  # the eigenclass, so they shadow the ones from the real class; 
-  #
-  # unfortunately, this is not as flexible as it would be using FactoryGirl
-
-  def self.create_media_file
-    mf = MediaFile.new( :uploaded_data => { :type=> "image/png",
-                       :tempfile=> File.new("#{Rails.root}/app/assets/images/icons/eye.png", "r"),
-                       :filename=> "eye.png"},
-                       :content_type => "image/png",
-                       :guid => (Digest::SHA1.hexdigest Time.now.to_f.to_s),
-                       :filename => "dummy.png",
-                       :access_hash => UUIDTools::UUID.random_create.to_s)
-    def mf.assign_access_hash; end
-    def mf.validate_file; end
-    def mf.store_file; end
-    mf.save!
-    mf
-  end
-
-  def self.create_media_entry 
-    me = MediaEntry.new( :upload_session => (FactoryGirl.create :upload_session),
-                        :owner => (User.find_random || (FactoryGirl.create :user)),
-                        :media_file => create_media_file )
-    def me.extract_subjective_metadata; end
-    def me.set_copyright; end
-    def me.set_descr_author_value; end
-    me.save!
-    me
-  end
-
-end
-
 FactoryGirl.define do
+
+  factory :media_entry do
+    upload_session {FactoryGirl.create :upload_session}
+    owner  {User.find_random || (FactoryGirl.create :user)}
+    media_file {FactoryGirl.create :media_file}
+    after_build do |me|
+      def me.extract_subjective_metadata; end
+      def me.set_copyright; end
+      def me.set_descr_author_value; end
+    end
+  end
+
+  factory :media_file  do 
+    uploaded_data  {{ :type=> "image/png", :tempfile=> File.new("#{Rails.root}/app/assets/images/icons/eye.png", "r"), :filename=> "eye.png"}} 
+    content_type "image/png"
+    guid  (Digest::SHA1.hexdigest Time.now.to_f.to_s)
+    filename  "dummy.png"
+    access_hash  UUIDTools::UUID.random_create.to_s
+    after_build do |mf|
+      def mf.assign_access_hash; end
+      def mf.validate_file; end
+      def mf.store_file; end
+    end
+  end
 
   factory :media_set, :class => Media::Set do
     user {User.find_random || (FactoryGirl.create :user)}
