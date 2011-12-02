f64370becea80485a705b0ab60329a45f09a0690
Thomas Schank <DrTom@schank.ch>
2011-12-02 13:58:34 +0100

    removed low level permissions, clashed with old existing

--- spec/models/media_entry_spec.rb
+++ spec/models/media_entry_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MediaEntry do
 
   it "should be producible by a factory" do
-    (ModelFactory.create_media_entry).should_not == nil
+    (FactoryGirl.create :media_entry).should_not == nil
   end
 
 end
