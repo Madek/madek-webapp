bb5c81eef85fa8e40b8e1ba7afe2bdd5ea11ae1c
Ramón Cahenzli <rca@psy-q.ch>
2011-12-08 09:20:32 +0100

    Important behavior for a nested set.

---
+++ spec/models/set_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Media::Set do
+
+  
+  pending "should contain sets and media entries" do
+    
+  end
+  
+  pending "should be able to contain media entries that are also in another set" do
+    
+  end
+  
+  context "sets in sets" do
+
+    pending "should contain other sets" do
+      
+    end
+    
+    pending "should have all the contexts of all their ancestors" do
+      
+    end
+
+  end
+ 
+
+end
