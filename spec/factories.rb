48b8bdecae7a731b3fe738da1eddec25cda90087
Thomas Schank <DrTom@schank.ch>
2011-12-09 11:54:47 +0100

    new feature branch dag-to-dg with some content from permissions branch

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,33 @@
+# encoding: utf-8
+
 FactoryGirl.define do
 
   factory :media_entry do
     upload_session {FactoryGirl.create :upload_session}
     media_file {FactoryGirl.create :media_file}
+    after_build do |me|
+      def me.extract_subjective_metadata; end
+      def me.set_copyright; end
+      def me.set_descr_author_value; end
+    end
   end
 
-  factory :media_file do
-    uploaded_data { { :type=> "image/png",
-              :tempfile=> File.new("#{Rails.root}/app/assets/images/icons/eye.png", "r"),
-              :filename=> "eye.png"} }
+  factory :media_file  do 
+    uploaded_data  {{ :type=> "image/png", :tempfile=> File.new("#{Rails.root}/app/assets/images/icons/eye.png", "r"), :filename=> "eye.png"}} 
+    content_type "image/png"
+    guid  (Digest::SHA1.hexdigest Time.now.to_f.to_s)
+    filename  "dummy.png"
+    access_hash  UUIDTools::UUID.random_create.to_s
+    after_build do |mf|
+      def mf.assign_access_hash; end
+      def mf.validate_file; end
+      def mf.store_file; end
+    end
+  end
 
+  factory :media_set, :class => Media::Set do
+    user {User.find_random || (FactoryGirl.create :user)}
+    owner {User.find_random || (FactoryGirl.create :user)}
   end
 
   factory :group do
@@ -22,14 +40,14 @@ FactoryGirl.define do
   end
 
   factory :upload_session do
-    user {FactoryGirl.create :user}
+    user {User.find_random || (FactoryGirl.create :user)}
+    is_complete true
   end
 
   factory :user do
     person {FactoryGirl.create :person}
-    email {"#{person.firstname}.#{person.lastname}@example.com".downcase} 
+    email {UUIDTools::UUID.random_create.hexdigest.slice(0,20)+"@example.com"}
     login {email}
   end
-
 
 end
