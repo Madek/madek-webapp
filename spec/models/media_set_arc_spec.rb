d9d43c52b5cb1c3412a0b90aba9e34b4e142a748
Thomas Schank <DrTom@schank.ch>
2011-12-12 15:17:20 +0100

    added spec for child of media_set beeing in the resources of the parent

--- spec/models/media_set_arc_spec.rb
+++ spec/models/media_set_arc_spec.rb
@@ -12,18 +12,36 @@ describe Media::SetArc do
     (FactoryGirl.create :media_set_arc, :parent => @set1 , :child => @set2).should_not == nil
   end
 
-  context "set in set relations" do
+  context "sets in sets" do
 
-    before :all do
-      @arc = (FactoryGirl.create :media_set_arc, :parent => @set1 , :child => @set2)
+    context "graph relations" do
+
+      before :all do
+        @arc = (FactoryGirl.create :media_set_arc, :parent => @set1 , :child => @set2)
+      end
+
+      it "the arcs child should be in the child_sets of the parent" do
+        @arc.parent.child_sets.should include @arc.child
+      end
+
+      it "the arcs parent should be in the parent_sets of the child" do
+        @arc.child.parent_sets.should include @arc.parent
+      end
+
     end
 
-    it "the arcs child should be in the child_sets of the parent" do
-      @arc.parent.child_sets.should include @arc.child
-    end
 
-    it "the arcs parent should be in the parent_sets of the child" do
-      @arc.child.parent_sets.should include @arc.parent
+    context "rails relations" do
+
+      before :all do
+        @set1.child_sets << @set2
+      end
+
+      it "the set2 should be included in by_media_set" do
+        by_ms = MediaResource.by_media_set(@set1)
+        by_ms.first.should == @set2
+      end
+
     end
 
   end
