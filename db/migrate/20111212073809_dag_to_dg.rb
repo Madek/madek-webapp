8542d2f93928acbbc7206c7daac3ec76317e1b19
Thomas Schank <DrTom@schank.ch>
2011-12-12 21:25:51 +0100

    added unique index parent_child on set_arcs and made data migration more resiliant

--- db/migrate/20111212073809_dag_to_dg.rb
+++ db/migrate/20111212073809_dag_to_dg.rb
@@ -6,17 +6,21 @@ class DagToDg < ActiveRecord::Migration
     create_table :media_set_arcs do |t|
       t.integer :parent_id, :null => false
       t.integer :child_id, :null => false
+#      t.index([:parent_id,:child_id], :unique => false)
     end
 
     add_index :media_set_arcs, :parent_id
     add_index :media_set_arcs, :child_id
+    add_index :media_set_arcs, [:parent_id, :child_id], :unique => true
 
     MigrationHelpers::fkey_cascade_on_delete :media_set_arcs, :parent_id, :media_sets 
     MigrationHelpers::fkey_cascade_on_delete :media_set_arcs, :child_id, :media_sets 
 
 
     Media::SetLink.where(direct: true).each do |link|
-      Media::SetArc.create child_id: link.descendant_id, parent_id: link.ancestor_id
+      if (Media::Set.find link.descendant_id) and (Media::Set.find link.ancestor_id)
+        Media::SetArc.create child_id: link.descendant_id, parent_id: link.ancestor_id
+      end
     end
 
     drop_table :media_set_links
