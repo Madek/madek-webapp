4d2053a0129d08a4fe09641b95bab92aabbc6624
Thomas Schank <DrTom@schank.ch>
2011-12-13 13:36:54 +0100

    added no self reference validation form MySQL, all specs green

--- app/models/media/set_arc.rb
+++ app/models/media/set_arc.rb
@@ -1,5 +1,16 @@
 class Media::SetArc < ActiveRecord::Base
+  validate :no_self_reference
+
   belongs_to  :child, :class_name => "Media::Set",  :foreign_key => :child_id
   belongs_to  :parent, :class_name => "Media::Set",  :foreign_key => :parent_id
+
+  private 
+
+  def no_self_reference
+    if child.id == parent.id
+      error "parent and child must not be equal"
+    end
+  end
+
 end
 
