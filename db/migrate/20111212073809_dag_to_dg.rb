77a0c26f86a45d508a04323803974c6b19152798
Thomas Schank <DrTom@schank.ch>
2011-12-14 15:47:04 +0100

    removed Media::SetLink model

--- db/migrate/20111212073809_dag_to_dg.rb
+++ db/migrate/20111212073809_dag_to_dg.rb
@@ -1,3 +1,20 @@
+# -*- encoding : utf-8 -*-
+
+class Media::SetLink < ActiveRecord::Base
+  def self.table_name_prefix
+    "media_"
+  end
+
+  # TODO use dagnabit gem instead ??
+  acts_as_dag_links :node_class_name => 'Media::Set'  
+
+  validate :validations
+
+  def validations
+    errors.add_to_base("A collection cannot be nested") if descendant.type == "Media::Collection"
+  end
+end
+
 class DagToDg < ActiveRecord::Migration
   include MigrationHelpers
 
