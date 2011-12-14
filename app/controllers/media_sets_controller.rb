196b1c6e189b6b3c2af39740c36640ae87bdacee
Thomas Schank <DrTom@schank.ch>
2011-12-14 16:27:56 +0100

    removed last occurences of children (hopefully)

--- app/controllers/media_sets_controller.rb
+++ app/controllers/media_sets_controller.rb
@@ -14,7 +14,7 @@ class MediaSetsController < ApplicationController
     
         @media_sets, @my_media_sets, @my_title, @other_title = if @media_set
           # all media_sets I can see, nested within a media set (for now only used with featured sets)
-          [resources.where(:id => @media_set.children), nil, "#{@media_set}", nil]
+          [resources.where(:id => @media_set.child_sets), nil, "#{@media_set}", nil]
         elsif @user and @user != current_user
           # all media_sets I can see that have been created by another user
           [resources.by_user(@user), nil, "Sets von %s" % @user, nil]
