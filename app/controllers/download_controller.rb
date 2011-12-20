81a4cba12fa19cd0f640a715345ba4bc8b8d4fc6
Ramón Cahenzli <rca@psy-q.ch>
2011-12-20 10:48:09 +0100

    Refactored download logic to be a controller, cleaned up all method calls, fixed out of memory problem when sending files via File.read, now uses send_file (X-SendFile)

---
+++ app/controllers/download_controller.rb
@@ -0,0 +1,178 @@
+# -*- encoding : utf-8 -*-
+
+class DownloadController < ApplicationController
+  def download
+    
+# e.g.
+# 'zip' param present means original file + xml sidecar of meta-data all zipped as one file
+# 'update' param present means original file updated by exiftool with current state of madek meta-data for that mediaentry
+# (update and zip should be treated as mutally exclusive in the context of one download call)
+# neither zip nor update present? just give the original file, as it was uploaded.
+# WE SHOULD NEVER UPDATE AN UPLOADED FILE WITH MADEK METADATA.
+
+#####################################################################################################################
+#####################################################################################################################
+
+      unless params['id'].blank? 
+
+        @media_entry = MediaEntry.where(:id => params['id']).first
+        # This is broken, presumably because of ruby 1.8.x not having any native idea of character encodings.
+        # If we move the gsub to execute after the unescape has processed, we can easily lose part of the 
+        # filename if it contains diacritics and spaces.       
+        @filename = CGI::unescape(@media_entry.media_file.filename.gsub(/\+/, '_'))
+        @@content_type = "text/plain"
+        
+        unless @media_entry.nil?
+
+          is_preview = (!params['size'].nil? or !params['video_thumbnail'].nil? or !params['audio_preview'].nil?)
+          if is_preview
+            render :text => 'Sie haben nicht die notwendige Zugriffsberechtigung.', :status => 500 unless Permission.authorized?(current_user, :view, @media_entry) 
+            @size = params['size'].try(:to_sym)
+                        
+            # This isn't video or audio, it's a plain old image
+            if (!@size.nil? and params['video_thumbnail'].nil? and params['audio_preview'].nil?)
+              preview = @media_entry.media_file.get_preview(@size)
+              @content_type = preview.content_type
+              @filename = [@filename.split('.', 2).first, preview.filename.gsub(@media_entry.media_file.guid, '')].join
+              
+            # Video files get a WebM preview file
+            elsif !params['video_thumbnail'].nil?
+              @content_type = "video/webm"
+              preview = @media_entry.media_file.previews.where(:@content_type => content_type).last
+              if preview.nil?
+                render :text => 'Preview file not found.', :status => 404
+              else
+                @filename = preview.filename # The filename going out to the browser
+                path = "#{THUMBNAIL_STORAGE_DIR}/#{@media_entry.media_file.shard}/#{preview.filename}"
+              end     
+              
+            # Audio files get an Ogg Vorbis preview file
+            elsif !params['audio_preview'].nil?
+              @content_type = "audio/ogg"
+              preview = @media_entry.media_file.previews.where(:@content_type => content_type).last
+              if preview.nil?
+                render :text => 'Preview file not found.', :status => 404
+              else
+                @filename = preview.filename # The filename going out to the browser
+                path = "#{THUMBNAIL_STORAGE_DIR}/#{@media_entry.media_file.shard}/#{preview.filename}"
+              end
+              
+            # This isn't any preview we can handle
+            else
+              render :text => "Don't know how to handle this type of preview.", :status => 500
+            end
+          else
+            @content_type = @media_entry.media_file.content_type
+            render :text => 'Sie haben nicht die notwendige Zugriffsberechtigung.', :status => 500 unless Permission.authorized?(current_user, :hi_res, @media_entry) 
+          end
+
+          if !params['zip'].blank?
+            send_as_zip
+          elsif !params['update'].blank?
+            send_updated_file
+          elsif !params['naked'].blank?
+            send_naked_file
+          else
+            send_original_file
+          end
+          
+        end
+      end
+
+  end # download 
+  
+ 
+  
+  #####################################################################################################################
+  #####################################################################################################################
+  # A media file updated with current madek meta-data, zipped up together with a bunch of side-car meta-data files.
+  # At present these are yaml and xml files, but they are pretty raw ATM - exposing the internals of the model/schema
+  # instead of following a well formed and easier to comprehend xml/yml schema..
+  #####################################################################################################################
+  #####################################################################################################################
+  def send_as_zip
+    path = @media_entry.updated_resource_file(false, @size) # false means we don't want to blank all the tags
+
+    # create the zipfile - we need a name that hopefully won't collide as it's being written to..
+    race_free_filename = [Time.now.to_i.to_s, @media_entry.id.to_s, @filename].join("_")
+
+    Zip::ZipOutputStream.open("#{ZIP_STORAGE_DIR}/#{race_free_filename}.zip") do |zos|
+      zos.put_next_entry(@filename)
+      zos.print IO.read(path)
+      zos.put_next_entry("#{@filename}.xml")
+      zos.print @media_entry.to_xml(:include => {:meta_data => {:include => :meta_key}} )
+      
+      # FIXME yaml not working properly anymore!
+      # YAML::ENGINE.yamler='psych'
+      # zos.put_next_entry("#{filename}.yml")
+      # zos.print @media_entry.to_yaml(:include => {:meta_data => {:include => :meta_key}} )
+      # YAML::ENGINE.yamler='syck'
+    end
+
+    if path
+        send_file("#{ZIP_STORAGE_DIR}/#{race_free_filename}.zip",
+                  :filename => "#{race_free_filename}.zip",
+                  :type          =>  @content_type,
+                  :disposition  =>  'attachment',
+                  :stream    =>  true,
+                  :buffer_size  =>  4096)  
+    else
+      render :status => 500
+    end
+
+    # TODO - Background job submission to remove the unlocked (ie downloaded) zipfile.
+    # since it fails if we try here (because the file is locked while the user 
+    # downloads it at some arbitrarily slow speed)
+  end
+  
+  
+  # An updated file - updated with the current set of madek meta-data  
+  def send_updated_file
+    # path = @media_entry.media_file.update_file_metadata(@media_entry.to_metadata_tags)
+    path = @media_entry.updated_resource_file(false, @size) # false means we don't want to blank all the tags
+    if path
+      send_file(path,
+                :filename => @filename,
+                :type          =>  @content_type,
+                :disposition  =>  'attachment',
+                :stream    =>  true,
+                :buffer_size  =>  4096)            
+    else
+      render :status => 500
+    end
+  end
+
+  
+  # A bare file - as little meta-data as can be allowed without breaking the file.  
+  def send_naked_file
+    path = @media_entry.updated_resource_file(true, @size) # true means we do want to blank all the tags
+
+    if path
+        send_file(path,
+                  :filename => @filename,
+                  :type          =>  @content_type,
+                  :disposition  =>  'attachment',
+                  :stream    =>  true,
+                  :buffer_size  =>  4096)            
+    else
+      render :status => 500
+    end    
+  end
+  
+  def send_original_file
+    # Provide a copy of the original file, not updated or nuffin'
+    path = @media_entry.media_file.file_storage_location
+    if @size
+      outfile = File.join(DOWNLOAD_STORAGE_DIR, filename)
+      `convert "#{path}" -resize "#{THUMBNAILS[@size]}" "#{outfile}"`
+      path = outfile
+    end
+    send_file(path,
+              :filename => @filename,
+              :type          =>  @content_type,
+              :disposition  =>  'attachment',
+              :stream    =>  true,
+              :buffer_size  =>  4096)
+  end
+  
+end # class
