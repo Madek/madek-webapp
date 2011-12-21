716851a75f22079a6bc0cafe397489d075f77942
Thomas Schank <DrTom@schank.ch>
2011-12-21 21:06:45 +0100

    authorized is now fully speced for the view action, all green

--- lib/permissions.rb
+++ lib/permissions.rb
@@ -1,19 +1,21 @@
-
 module Permissions
   extend self 
 
   class << self
 
-    def authorized?(subject, action, resource)
+    def authorized?(user, action, resource)
+      # the old authorized acceted subjects 
+      raise "authorized? can only be called with a user" if user.class != User
+
       if resource.owner == user
         true
       elsif action == :view && resource.perm_public_may_view 
         true
-      elsif userpermission_disallows action, mediaresource, user
+      elsif userpermission_disallows action, resource, user
         false
-      elsif userpermission_allows action, mediaresource, user
+      elsif userpermission_allows action, resource, user
         true
-      elsif grouppermissions_allows action, mediaresource, user
+      elsif grouppermission_allows action, resource, user
         true
       else
         false
@@ -43,55 +45,6 @@ module Permissions
         .where("grouppermissions.may_#{action} = true") \
         .first
     end
-
- 
-
-#    def can_view?(mediaresource, user)
-#
-#      return true if mediaresource.owner == user
-#
-#      return true if mediaresource.perm_public_may_view 
-#
-#      return false if mediaresourceuserpermission_disallows :view, mediaresource, user
-#
-#      return true if mediaresourceuserpermission_allows :view, mediaresource, user
-#
-#      return true if mediaresourcegrouppermission_allows :view, mediaresource, user
-#
-#      false
-#
-#    end
-#
-#
-#    def mediaresourceuserpermission_disallows what, mediaresource, user 
-#      (Mediaresourceuserpermission.count_by_sql %Q@ 
-#        SELECT count(*) from mediaresourceuserpermissions 
-#          WHERE mediaresource_id=#{mediaresource.id} 
-#            AND user_id=#{user.id} 
-#            AND maynot_#{what.to_s} = true; 
-#       @) > 0
-#    end
-#
-#    def mediaresourceuserpermission_allows what, mediaresource, user 
-#      (Mediaresourceuserpermission.count_by_sql %Q@ 
-#        SELECT count(*) from mediaresourceuserpermissions 
-#          WHERE mediaresource_id=#{mediaresource.id} 
-#            AND user_id=#{user.id} 
-#            AND may_#{what.to_s} = true; 
-#       @) > 0
-#    end
-#
-#    def mediaresourcegrouppermission_allows what, mediaresource, user
-#      (Mediaresourcegrouppermission.count_by_sql %Q@ 
-#      SELECT count(*) from mediaresourcegrouppermissions, users, usergroups_users 
-#        WHERE users.id = usergroups_users.user_id 
-#          AND mediaresourcegrouppermissions.usergroup_id = usergroups_users.usergroup_id
-#          AND mediaresourcegrouppermissions.mediaresource_id = #{mediaresource.id}
-#          AND users.id = #{user.id}
-#          AND may_#{what.to_s} = true
-#          LIMIT 1; 
-#          @) > 0
-#    end
 
   end
 
---
+++ spec/permissions_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe "Permissions" do
+
+  describe "A public viewable Mediaresource" do
+
+    before(:each) do
+      @owner = FactoryGirl.create :user
+      @media_set = FactoryGirl.create :media_set, perm_public_may_view:  true, owner: @owner 
+      @user = FactoryGirl.create :user
+    end
+
+    it "should be viewalbe by an unrelated user" do
+      (Permissions.authorized? @user, :view , @media_set).should == true
+    end
+
+    context "the user is not allowed by user permissions" do
+
+      before(:each) do
+        FactoryGirl.create :userpermission, user: @user, resource: @media_set, maynot_view: true
+      end
+
+      it "should be viewable by the user" do
+        (Permissions.authorized? @user, :view, @media_set).should == true
+      end
+
+    end
+  end
+
+
+  describe "A non public viewable Mediaresource" do 
+
+
+    before(:each) do
+      @owner = FactoryGirl.create :user
+      @media_set = FactoryGirl.create :media_set, perm_public_may_view:  false, owner: @owner 
+      @user = FactoryGirl.create :user
+    end
+
+    it "can be viewed by its owner"  do
+      (Permissions.authorized? @owner, :view , @media_set).should == true
+    end
+
+    it "can be viewed by its owner even if the owner is disallowed by a userpermission"  do
+      FactoryGirl.create :userpermission, user: @user, resource: @media_set, maynot_view: true
+      (Permissions.authorized? @owner, :view , @media_set).should == true
+    end
+
+    it "should not be viewable by an user without any permissions" do
+      (Permissions.authorized? @user, :view , @media_set).should == false
+    end
+
+    context "when a userpermission allows the user" do
+
+      before(:each) do
+        FactoryGirl.create :userpermission, user: @user, resource: @media_set, may_view: true
+      end
+
+      it "should be be viewable by the user" do
+        (Permissions.authorized? @user, :view , @media_set).should == true
+      end
+
+    end
+
+    context "a mediaresourcegrouppermission allows the user to view" do
+
+      before(:each) do
+        @group = FactoryGirl.create :group
+        @group.users << @user
+        FactoryGirl.create :grouppermission, may_view: true, group: @group, resource: @media_set
+      end
+
+      it "should be be viewable for the user" do
+        (Permissions.authorized? @user, :view , @media_set).should == true
+      end
+
+      context "when a mediaresourceuserpermission denies the user to view" do
+
+        before(:each) do
+          FactoryGirl.create :userpermission, may_view: false, maynot_view: true, resource: @media_set, user: @user
+        end
+
+        it "should not be viewable for the user" do
+          (Permissions.authorized? @user, :view , @media_set).should == false
+        end
+      end
+    end
+  end
+end
+
