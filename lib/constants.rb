bb7739ec551b3760c6e4f6141ffda257d7f243f6
Thomas Schank <DrTom@schank.ch>
2012-01-03 08:54:14 +0100

    added Actions module in constans with conveniece methods

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,14 +1,40 @@
 module Constants 
   extend self
 
-  #TODO refactor this into a module with old2new 
-  
   
   PUBLIC_PREFIX= "perm_public_may_"
   PUBLIC_ACTIONS= %w{view download_high_resolution}
   ACTIONS= PUBLIC_ACTIONS.concat %w{manage edit}
   
+  NEW_OLD_ACTIONS_MAP = {
+      download_high_resolution: :hi_res,
+      edit: :edit,
+      manage: :manage,
+      view: :view
+    }
 
+
+  module Actions 
+    include Enumerable
+    extend self
+
+    class << self 
+
+      def new2old old_action
+        Constants::NEW_OLD_ACTIONS_MAP.fetch old_action.to_sym
+      end
+
+      def old2new new_action
+        Constants::NEW_OLD_ACTIONS_MAP.invert.fetch new_action.to_sym
+      end
+
+      def each
+        ACTIONS.each {|action| yield action}
+      end
+
+    end
+
+  end
 
   
   module PublicActions
