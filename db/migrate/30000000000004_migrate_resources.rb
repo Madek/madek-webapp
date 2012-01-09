97152141011b5a1ac217eb947d32b1882fd4ed70
Thomas Schank <DrTom@schank.ch>
2012-01-09 15:38:10 +0100

    updated doc graffles

---
+++ db/migrate/30000000000004_migrate_resources.rb
@@ -0,0 +1,43 @@
+class MigrateResources < ActiveRecord::Migration
+  include MigrationHelpers
+  include Constants
+
+  def up
+
+    [:media_sets,:media_entries].each do |resource|
+      add_column resource, :media_resource_id, :integer, :null => false 
+      add_index resource, :media_resource_id
+    end
+
+    MediaEntry.all.each do |me| 
+      mr = MediaResource.create :owner => me.upload_session.user
+      me.media_resource = mr
+      me.save!
+    end
+
+    Media::Set.all.each do |ms| 
+      mr = MediaResource.create :owner => ms.user
+      mr.media_resource = mr
+      ms.save!
+    end
+
+    add_fkey_referrence_constraint MediaEntry, MediaResource
+    add_fkey_referrence_constraint Media::Set, MediaResource
+
+    add_not_null_constraint MediaEntry, MediaResource
+    add_not_null_constraint Media::Set, MediaResource
+
+    add_unique_constraint MediaEntry, MediaResource
+    add_unique_constraint Media::Set, MediaResource
+
+  end
+
+  def down
+
+    [:media_sets,:media_entries].each do |resource|
+      remove_column resource, :media_resource_id
+    end
+
+  end
+
+end
