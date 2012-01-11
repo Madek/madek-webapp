1a4f22b625a4fdc306d7446fc69efa11f7b419e2
Thomas Schank <DrTom@schank.ch>
2012-01-11 12:29:24 +0100

    yeah spec for permissions module are green

--- spec/permissions_spec.rb
+++ spec/permissions_spec.rb
@@ -2,26 +2,31 @@ require 'spec_helper'
 
 describe "Permissions" do
 
+  before :all do
+    @permissionset_view_false  = FactoryGirl.create :permissionset, view: false, download: false, edit: false, manage: false
+    @permissionset_view_true  = FactoryGirl.create :permissionset, view: true, download: false, edit: false, manage: false
+  end
+
   describe "A public viewable Mediaresource" do
 
     before(:each) do
       @owner = FactoryGirl.create :user
-      @media_set = FactoryGirl.create :media_set, perm_public_may_view:  true, owner: @owner 
+      @media_resource = FactoryGirl.create :media_resource, owner: @owner, permissionset: @permissionset_view_true
       @user = FactoryGirl.create :user
     end
 
     it "should be viewalbe by an unrelated user" do
-      (Permissions.authorized? @user, :view , @media_set).should == true
+      (Permissions.authorized? @user, :view , @media_resource).should == true
     end
 
     context "the user is not allowed by user permissions" do
 
       before(:each) do
-        FactoryGirl.create :userpermission, user: @user, media_resource: @media_set, maynot_view: true
+        FactoryGirl.create :userpermission, user: @user, media_resource: @media_resource, permissionset: @permissionset_view_false
       end
 
       it "should be viewable by the user" do
-        (Permissions.authorized? @user, :view, @media_set).should == true
+        (Permissions.authorized? @user, :view, @media_resource).should == true
       end
 
     end
@@ -33,31 +38,31 @@ describe "Permissions" do
 
     before(:each) do
       @owner = FactoryGirl.create :user
-      @media_set = FactoryGirl.create :media_set, perm_public_may_view:  false, owner: @owner 
+      @media_resource = FactoryGirl.create :media_resource, owner: @owner, permissionset: @permissionset_view_false
       @user = FactoryGirl.create :user
     end
 
     it "can be viewed by its owner"  do
-      (Permissions.authorized? @owner, :view , @media_set).should == true
+      (Permissions.authorized? @owner, :view , @media_resource).should == true
     end
 
     it "can be viewed by its owner even if the owner is disallowed by a userpermission"  do
-      FactoryGirl.create :userpermission, user: @user, media_resource: @media_set, maynot_view: true
-      (Permissions.authorized? @owner, :view , @media_set).should == true
+      FactoryGirl.create :userpermission, user: @owner, media_resource: @media_resource, permissionset: @permissionset_view_false
+      (Permissions.authorized? @owner, :view , @media_resource).should == true
     end
 
     it "should not be viewable by an user without any permissions" do
-      (Permissions.authorized? @user, :view , @media_set).should == false
+      (Permissions.authorized? @user, :view , @media_resource).should == false
     end
 
     context "when a userpermission allows the user" do
 
       before(:each) do
-        FactoryGirl.create :userpermission, user: @user, media_resource: @media_set, may_view: true
+        FactoryGirl.create :userpermission, user: @user, media_resource: @media_resource, permissionset: @permissionset_view_true
       end
 
       it "should be be viewable by the user" do
-        (Permissions.authorized? @user, :view , @media_set).should == true
+        (Permissions.authorized? @user, :view , @media_resource).should == true
       end
 
     end
@@ -67,21 +72,20 @@ describe "Permissions" do
       before(:each) do
         @group = FactoryGirl.create :group
         @group.users << @user
-        FactoryGirl.create :grouppermission, may_view: true, group: @group, media_resource: @media_set
+        FactoryGirl.create :grouppermission, permissionset: @permissionset_view_true, group: @group, media_resource: @media_resource
       end
 
       it "should be be viewable for the user" do
-        (Permissions.authorized? @user, :view , @media_set).should == true
+        (Permissions.authorized? @user, :view , @media_resource).should == true
       end
 
-      context "when a mediaresourceuserpermission denies the user to view" do
-
+      context "when a userpermission denies the user to view" do
         before(:each) do
-          FactoryGirl.create :userpermission, may_view: false, maynot_view: true, media_resource: @media_set, user: @user
+          FactoryGirl.create :userpermission, user: @user, media_resource: @media_resource, permissionset: @permissionset_view_false
         end
 
         it "should not be viewable for the user" do
-          (Permissions.authorized? @user, :view , @media_set).should == false
+          (Permissions.authorized? @user, :view , @media_resource).should == false
         end
       end
     end
