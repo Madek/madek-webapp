e72570d0363aad1576c6bad78ab3709890ad1c5b
Thomas Schank <DrTom@schank.ch>
2012-01-12 19:54:18 +0100

    added login bypass for development

---
+++ lib/development_helpers.rb
@@ -0,0 +1,15 @@
+
+module DevelopmentHelpers
+
+  AUTH_XML = {"memberof"=> {"group"=> ["zhdk/SER_SUP.alle",
+    "zhdk/SER_SUP.personal", "zhdk/SER.personal", "zhdk/SER.alle",
+    "zhdk/SER_SUP_ITZ.personal", "zhdk/personal", "zhdk/SER_SUP_ITZ.alle"]},
+    "id"=>"999999", "uniqueid"=>"e999999|zhdk", "phone_private"=>nil,
+    "local_username"=>"jdeveloper@login.itz", "phone_mobile"=>nil,
+    "firstname"=>"Joe", "phone_business"=>nil, "lastname"=>"Developer",
+    "phone_function"=>nil, "fax"=>nil, "email"=>"joe.developer@zhdk.ch"}
+
+end
+
+
+
