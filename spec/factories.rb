19c9fe828daefd20ae7f05a828c4bce430163dd8
Thomas Schank <DrTom@schank.ch>
2012-01-18 10:47:28 +0100

    fixed min with user login

--- spec/factories.rb
+++ spec/factories.rb
@@ -87,7 +87,7 @@ FactoryGirl.define do
   factory :user do
     person {FactoryGirl.create :person}
     email {Faker::Internet.email}
-    login {Faker::Internet.user_name}
+    login {Faker::Internet.user_name + "_#{UUIDTools::UUID.random_create.to_s.first 5}"}
     usage_terms_accepted_at {Time.now}
   end
 
