066d6327de144e8dcd6539e59d66555f4f3876bb
Thomas Schank <DrTom@schank.ch>
2012-01-18 14:54:10 +0100

    not creating intermediate views makes schema clearer

--- db/migrate/30000000000010_create_viewable_resources_view.rb
+++ db/migrate/30000000000010_create_viewable_resources_view.rb
@@ -19,30 +19,24 @@ class CreateViewableResourcesView < ActiveRecord::Migration
         actionable_by_ownership= actionable_media_resources_users_by_ownership action
 
         actionable_users= <<-SQL 
-          SELECT * FROM  #{action}able_media_resources_by_userpermission
-            UNION SELECT * from #{action}able_media_resources_by_gp_not_denied_by_up
-            UNION SELECT * from #{action}able_media_resources_by_publicpermission
-            UNION SELECT * from #{action}able_media_resources_by_ownership;
+          ( #{actionable_by_userpermission.to_sql} )
+            UNION  ( #{actionable_by_gp_not_denied_by_up.to_sql} )
+            UNION ( #{actionable_by_publicpermission.to_sql} )
+            UNION ( #{actionable_by_ownership.to_sql} )
         SQL
 
-        create_view "#{action}able_media_resources_by_userpermission", actionable_by_userpermission
-        create_view "#{action}able_media_resources_disallowed_by_userpermission", actionable_disallowed_by_userpermission
-        create_view "#{action}able_media_resources_by_grouppermission", actionable_by_grouppermission
-        create_view "#{action}able_media_resources_by_gp_not_denied_by_up",actionable_by_gp_not_denied_by_up
-        create_view "#{action}able_media_resources_by_publicpermission", actionable_by_publicpermission
-        create_view "#{action}able_media_resources_by_ownership",actionable_by_ownership
         create_view "#{action}able_media_resources_users",actionable_users
 
-        {media_entry: MediaEntry, media_set: MediaSet}.each do |singular,model|
-          sql= <<-SQL 
-              SELECT media_resources.id as #{singular.to_s}_id, #{action}able_media_resources_users.user_id as user_id 
-                FROM media_resources
-                INNER JOIN #{action}able_media_resources_users 
-                  ON #{action}able_media_resources_users.media_resource_id = media_resources.id
-                WHERE media_resources.type = '#{model.name}'
-          SQL
-          create_view "#{action}able_#{singular.to_s.pluralize}_users", sql
-        end
+#        {media_entry: MediaEntry, media_set: MediaSet}.each do |singular,model|
+#          sql= <<-SQL 
+#              SELECT media_resources.id as #{singular.to_s}_id, #{action}able_media_resources_users.user_id as user_id 
+#                FROM media_resources
+#                INNER JOIN #{action}able_media_resources_users 
+#                  ON #{action}able_media_resources_users.media_resource_id = media_resources.id
+#                WHERE media_resources.type = '#{model.name}'
+#          SQL
+#          create_view "#{action}able_#{singular.to_s.pluralize}_users", sql
+#        end
 
 
     end
@@ -52,16 +46,10 @@ class CreateViewableResourcesView < ActiveRecord::Migration
   def down
 
     Constants::Actions.each do |action|
-      {media_entry: MediaEntry, media_set: MediaSet}.each do |singular,model|
-        drop_view "#{action}able_#{singular.to_s.pluralize}_users"
-      end
+#      {media_entry: MediaEntry, media_set: MediaSet}.each do |singular,model|
+#        drop_view "#{action}able_#{singular.to_s.pluralize}_users"
+#      end
       drop_view "#{action}able_media_resources_users"
-      drop_view "#{action}able_media_resources_by_ownership"
-      drop_view "#{action}able_media_resources_by_publicpermission"
-      drop_view "#{action}able_media_resources_by_gp_not_denied_by_up"
-      drop_view "#{action}able_media_resources_by_grouppermission"
-      drop_view "#{action}able_media_resources_disallowed_by_userpermission"
-      drop_view "#{action}able_media_resources_by_userpermission"
     end
 
   end
