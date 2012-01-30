05f128c4c4ced38d1394216a2935180b8c7526f8
Thomas Schank <DrTom@schank.ch>
2012-01-30 17:22:16 +0100

    basic xml output works for a few models

--- lib/development_helpers/xml.rb
+++ lib/development_helpers/xml.rb
@@ -1,19 +1,35 @@
 module DevelopmentHelpers
   module Xml
 
+
+    # get superlist of the following with 
+    #   ActiveRecord::Base.connection.tables.each { |t| puts "#{t}: #{t.to_s.camelize.singularize}," }
+    # now, filter and oder appropriately
+
+    TablesModels = { \
+      users: :User,
+      people: :Person,
+      groups: :Group 
+    }
+
     def self.to_xml
       require 'builder' unless defined? ::Builder
 
       xml = Builder::XmlMarkup.new(:indent => 2)
       xml.instruct!
 
-      xml.people do |people|
-        Person.all.each do |person|
-          people << person.to_xml(skip_instruct: true)
+      xml.data do 
+        TablesModels.each do |table_name,model|
+          eval " 
+          xml.#{table_name} do |table|
+            #{model}.all.each do |instance|
+              table << instance.to_xml(skip_instruct: true).gsub(/^/, '    ')
+            end
+          end
+          "
         end
       end
 
     end
-
   end
 end
