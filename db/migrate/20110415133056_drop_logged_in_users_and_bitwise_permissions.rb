b1cf51271029dc7bbb5e7919ab73090f26a68e04
Thomas Schank <DrTom@schank.ch>
2012-01-31 19:12:38 +0100

    disabled part of mirgration that created a group

--- db/migrate/20110415133056_drop_logged_in_users_and_bitwise_permissions.rb
+++ db/migrate/20110415133056_drop_logged_in_users_and_bitwise_permissions.rb
@@ -8,25 +8,25 @@ class DropLoggedInUsersAndBitwisePermissions < ActiveRecord::Migration
       t.index [:action_bits, :action_mask]
     end
 
-    group = Group.find_or_create_by_name(:name => "ZHdK (Zürcher Hochschule der Künste)")    
-
-    Permission.where("actions_object LIKE '%:logged_in_users%'").each do |p|
-      q = group.permissions.build(:media_resource => p.media_resource)
-      h = YAML.load(p.actions_object).ivars["keys"]
-      actions = h.select {|k,v| v == :logged_in_users}.map(&:first)
-      h = {}
-      actions.each {|a| h[a] = true}
-      q.set_actions(h)
-      h = {}
-      actions.each {|a| h[a] = false}
-      p.set_actions(h)
-    end
-
-    Permission.where("actions_object IS NOT NULL").each do |permission|
-      h = YAML.load(permission.actions_object).ivars["keys"]
-      h[:hi_res] ||= h[:high_res] if h[:high_res]
-      permission.set_actions(h)
-    end
+#    group = Group.find_or_create_by_name(:name => "ZHdK (Zürcher Hochschule der Künste)")    
+#
+#    Permission.where("actions_object LIKE '%:logged_in_users%'").each do |p|
+#      q = group.permissions.build(:media_resource => p.media_resource)
+#      h = YAML.load(p.actions_object).ivars["keys"]
+#      actions = h.select {|k,v| v == :logged_in_users}.map(&:first)
+#      h = {}
+#      actions.each {|a| h[a] = true}
+#      q.set_actions(h)
+#      h = {}
+#      actions.each {|a| h[a] = false}
+#      p.set_actions(h)
+#    end
+#
+#    Permission.where("actions_object IS NOT NULL").each do |permission|
+#      h = YAML.load(permission.actions_object).ivars["keys"]
+#      h[:hi_res] ||= h[:high_res] if h[:high_res]
+#      permission.set_actions(h)
+#    end
 
     change_table :permissions do |t|
       t.remove :actions_object
