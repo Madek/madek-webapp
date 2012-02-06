de2101e4baafd922f800faaba81e31f503d3128f
Ramón Cahenzli <rca@psy-q.ch>
2012-02-06 17:17:32 +0100

    New persona: Beat

---
+++ features/data/persona/beat.rb
@@ -0,0 +1,34 @@
+# Persona:  Beat
+# Job:      Publishing assistant
+# Age:      57
+#
+#
+# == Responsibilities
+# 
+# * Picking photographs for the next issue of the in-house university magazine.
+#
+# == Biography
+#
+# Beat likes going to concerts and doing amateur photography. He has a good eye
+# for photographic quality, that's why he is part of a team choosing the photo-
+# graphs that are printed in each issue of the organization's in-house magazine.
+#
+# He doesn't like technology very much and would prefer an analog workflow. Since
+# he doesn't spend too much time on the web, most user interface standards from
+# popular web applications aren't known to him.
+#
+# During his work, he mostly has to do with photographers. His position is
+# between photography and layout, preparing the right photos for layouters
+# to use.
+# 
+# == Goals
+#
+# 1. Picking the best photos for ten different magazine articles each month.
+# 2. Quickly getting an overview of the photos submitted by photographers.
+# 3. Assembling photographs into different sets, one set per issue of the maga-
+#    zine, one set for each article in each set.
+#
+# == Frustrations
+#
+# 1. Not finding the photographers' work in the system.
+# 2. Getting confused about how to handle the system.
