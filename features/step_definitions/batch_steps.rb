d79ed7376c2736b902d37929a674f2a0b652c1b8
Sebastian Pape <email@sebastianpape.com>
2012-02-10 12:06:22 +0100

    Try to putting debugging stuf to the log to found the error, why this test is not running on jenkins

--- features/step_definitions/batch_steps.rb
+++ features/step_definitions/batch_steps.rb
@@ -1,6 +1,12 @@
 Then /^I should see that all visible resources are in my batch bar$/ do
   visible_resources = all(".item_box", :visible => true)
+  visible_resources.each do |item|
+    puts item.text
+  end
   batch_elements = all(".task_bar .thumb_mini")
+  batch_elements.each do |item|
+    puts item.text
+  end
   visible_resources.size.should eql batch_elements.size
 end
 
