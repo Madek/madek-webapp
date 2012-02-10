f84dcd478c75431587f2b9df6e0822d7354ced7f
Sebastian Pape <email@sebastianpape.com>
2012-02-10 17:12:59 +0100

    [FIX Jenkins] Remove the elements from the batch bar first, befor testing select all functionalities

--- features/step_definitions/batch_steps.rb
+++ features/step_definitions/batch_steps.rb
@@ -1,20 +1,15 @@
 Then /^I should see that all visible resources are in my batch bar$/ do
   visible_resources = all(".item_box", :visible => true)
-  puts "VISIBLE RESOURCES"
-  puts visible_resources.size
-  visible_resources.each do |item|
-    puts item.text
-  end
   batch_elements = all(".task_bar .thumb_mini")
-  puts "BATCH ELEMENTS"
-  puts batch_elements.size
-  batch_elements.each do |item|
-    puts item.text
-  end
   visible_resources.size.should eql batch_elements.size
 end
 
 When /^I use batch's select all$/ do
   find("#batch-select-all").click()
+  sleep 1.0
+end
+
+When /^I use batch's deselect all$/ do
+  find("#batch-deselect-all").click()
   sleep 1.0
 end
