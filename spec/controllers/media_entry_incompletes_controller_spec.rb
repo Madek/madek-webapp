b80c2f0bf967dcf3129c01fda0a1fb5c0ec685c6
Thomas Schank <DrTom@schank.ch>
2012-02-10 16:51:25 +0100

    added media_entry_incompletes_controller; tests pending

---
+++ spec/controllers/media_entry_incompletes_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe MediaEntryIncompletesController do
+
+#  before :all do
+#    @user = FactoryGirl.create :user
+#  end
+#
+#  before :each do
+#    @mei= FactoryGirl.create :media_entry_incomplete, user: @user
+#  end
+#
+#  it "should respond with error to delete with non json request" do
+#    delete :destroy, {id: @mei.id}, {user_id: @user.id}
+#    response.should_not  be_success
+#  end
+#
+#  describe "request delete with json" do
+#    it "should respond with success" do
+#      delete :destroy, {format: 'json', id: @mei.id}, {user_id: @user.id}
+#      response.should  be_success
+#    end
+#
+#    it "should have deleted the upload_session instance" do
+#      delete :destroy, {format: 'json', id: @mei.id}, {user_id: @user.id}
+#      MediaEntryIncomplete.exists?(@mei.id).should == false
+#    end
+#  end
+
+end
