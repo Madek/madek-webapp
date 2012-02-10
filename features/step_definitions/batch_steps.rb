b57594a3961d9b57bf5947687908fafe65758436
Sebastian Pape <email@sebastianpape.com>
2012-02-10 09:51:14 +0100

    fixes the batch select integration test

--- features/step_definitions/batch_steps.rb
+++ features/step_definitions/batch_steps.rb
@@ -1,7 +1,7 @@
 Then /^I should see that all visible resources are in my batch bar$/ do
   visible_resources = all(".item_box", :visible => true)
   batch_elements = all(".task_bar .thumb_mini")
-  visible_resource.size.should eql batch_elements.size
+  visible_resources.size.should eql batch_elements.size
 end
 
 When /^I use batch's select all$/ do
