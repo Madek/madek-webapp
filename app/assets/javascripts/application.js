215be305992bb4b2eaa3d3b097718007cc3c60f1
Sebastian Pape <email@sebastianpape.com>
2012-02-13 10:45:00 +0100

    require the rest of plupload for the asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,6 +35,14 @@
 
 /////////// PLUPLOAD /////////////
 //= require plupload
+//= require plupload.settings
+//= require jquery.plupload.queue
+//= require plupload.flash
+//= require plupload.silverlight
+//= require plupload.html4
+//= require plupload.html5
+//= require plupload.gears 
+//= require plupload.browserplus
 
 function document_ready(){
 	$("textarea").elastic();
