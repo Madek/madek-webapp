05b11566e29d7c37ce45e407eaafe835b1d5c5b1
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-02-16 23:20:23 +0100

    Admin: removing batch import

--- app/controllers/admin/media_entries_controller.rb
+++ app/controllers/admin/media_entries_controller.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Admin::MediaEntriesController < Admin::AdminController
 
-  def import
-  end
-
   def dropbox
     if request.post?
       AppSettings.dropbox_root_dir = params[:dropbox_root_dir]
