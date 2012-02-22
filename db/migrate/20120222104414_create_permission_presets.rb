fb483f9ae64e07860c16981490585355da52205a
Thomas Schank <DrTom@schank.ch>
2012-02-22 16:31:58 +0100

    added specs for permission_presets model

--- db/migrate/20120222104414_create_permission_presets.rb
+++ db/migrate/20120222104414_create_permission_presets.rb
@@ -1,11 +1,18 @@
 class CreatePermissionPresets < ActiveRecord::Migration
+  include Constants
+
   def change
+
     create_table :permission_presets do |t|
       t.string :name
-      t.boolean :download
-      t.boolean :view
-      t.boolean :edit
-      t.boolean :manage
+      Actions.each do |action|
+        t.boolean action, null: false, default: false
+      end
     end
+
+    # this is mainly to provide a hard condition on uniqueness 
+    add_index :permission_presets, [:view,:edit,:download,:manage], unique: true, name: :idx_bools_unique
+    add_index :permission_presets, :name, unique: true, name: :idx_name_unique
+
   end
 end
