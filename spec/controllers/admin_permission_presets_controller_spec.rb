4bf17b4f2c106c32ed4e3413df80c93858e43fb4
Thomas Schank <DrTom@schank.ch>
2012-02-22 15:16:21 +0100

    fixed routing spec for permission_presets

--- spec/controllers/admin_permission_presets_controller_spec.rb
+++ spec/controllers/admin_permission_presets_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe PermissionPresetsController do
+describe Admin::PermissionPresetsController do
 
   # This should return the minimal set of attributes required to create a valid
   # PermissionPreset. As you add validations to PermissionPreset, be sure to
--- spec/routing/permission_presets_routing_spec.rb
+++ spec/routing/permission_presets_routing_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
-describe PermissionPresetsController do
+describe Admin::PermissionPresetsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/permission_presets").should route_to("permission_presets#index")
+      get("/admin/permission_presets").should route_to("admin/permission_presets#index")
     end
 
     it "routes to #new" do
-      get("/permission_presets/new").should route_to("permission_presets#new")
+      get("/admin/permission_presets/new").should route_to("admin/permission_presets#new")
     end
 
     it "routes to #show" do
-      get("/permission_presets/1").should route_to("permission_presets#show", :id => "1")
+      get("/admin/permission_presets/1").should route_to("admin/permission_presets#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/permission_presets/1/edit").should route_to("permission_presets#edit", :id => "1")
+      get("/admin/permission_presets/1/edit").should route_to("admin/permission_presets#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/permission_presets").should route_to("permission_presets#create")
+      post("/admin/permission_presets").should route_to("admin/permission_presets#create")
     end
 
     it "routes to #update" do
-      put("/permission_presets/1").should route_to("permission_presets#update", :id => "1")
+      put("/admin/permission_presets/1").should route_to("admin/permission_presets#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/permission_presets/1").should route_to("permission_presets#destroy", :id => "1")
+      delete("/admin/permission_presets/1").should route_to("admin/permission_presets#destroy", :id => "1")
     end
 
   end
