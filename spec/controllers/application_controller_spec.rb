aca27e7259c06a360697764d8230225783717b6a
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-02-24 14:00:06 +0100

    start testing application_controller#root

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  describe "as guest user" do
+    it "should respond with success" do
+      get :root, {}, {}
+      response.should  be_success
+    end
+  end
+
+  describe "as logged in user" do
+    before :all do
+      @user = FactoryGirl.create :user
+    end
+    it "should respond with success" do
+      get :root, {}, {user_id: @user.id}
+      response.should  be_success
+    end
+  end
+  
+end
