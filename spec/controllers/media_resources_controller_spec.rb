98a6b232501765fb95fba74cdd99af2dff89077f
Sebastian Pape <email@sebastianpape.com>
2012-03-07 22:41:39 +0100

    [WIP] start the controller test for the extended media resources output

--- spec/controllers/media_resources_controller_spec.rb
+++ spec/controllers/media_resources_controller_spec.rb
@@ -6,37 +6,42 @@ describe MediaResourcesController do
     @user = FactoryGirl.create :user
   end
 
-  before :each do
-    @mei= FactoryGirl.create :media_entry_incomplete, user: @user
+  context "fetch an index of media resources" do
+    before :each do
+      100.times do
+        if rand > 0.5
+          Factory :media_entry, user: @user
+        else
+          Factory :media_set, user: @user
+        end
+      end
+    end
+    
+    describe "as JSON" do
+      binding.pry
+      # TODO GO ON HERE TOMORRRROOOOOOOOOOW
+    end
   end
 
+
   describe "a PUT json request on a single resource" do
-
+    before :each do
+      @mei= FactoryGirl.create :media_entry_incomplete, user: @user
+    end
+    
     describe  'with {type: "MediaEntry"}' do
-
       it "should convert the resource to a media entry" do
-
         put :update, {format: 'json', id: @mei.id, media_resource: {type: "MediaEntry"} }, {user_id: @user.id}
-
         response.should  be_success
         (MediaEntry.exists? @mei.id).should == true
-
       end
-
     end
 
     describe  'with {download: true}' do
-
       it "should set the public download permisssion to true " do
-
         put :update, {format: 'json', id: @mei.id, media_resource: {download: true} }, {user_id: @user.id}
-
         (MediaResource.find @mei.id).download.should == true
-
       end
-
     end
-
   end
-
 end
