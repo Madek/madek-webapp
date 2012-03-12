dbb35100eabcf747812d734f3bcede146ab1fec3
Thomas Schank <DrTom@schank.ch>
2012-03-12 09:53:59 +0100

    added relation meta_context -> meta_context_group

--- db/migrate/20120309092925_create_meta_context_groups.rb
+++ db/migrate/20120309092925_create_meta_context_groups.rb
@@ -1,9 +1,17 @@
 class CreateMetaContextGroups < ActiveRecord::Migration
-  def change
+  include MigrationHelpers
+
+  def up 
     create_table :meta_context_groups do |t|
       t.string :name
-
-      t.timestamps
     end
+    add_column :meta_contexts, :meta_context_group_id, :integer
+    add_fkey_referrence_constraint MetaContext, MetaContextGroup
   end
+
+  def down 
+    remove_column :meta_contexts, :meta_context_group_id
+    drop_table :meta_context_groups
+  end
+
 end
