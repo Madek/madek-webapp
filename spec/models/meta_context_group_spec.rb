a88a7e90f00cc2fc7ea719b1c59b4f7176a389be
Thomas Schank <DrTom@schank.ch>
2012-03-12 10:02:36 +0100

    added meta_context_group factory and spec

--- spec/models/meta_context_group_spec.rb
+++ spec/models/meta_context_group_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe MetaContextGroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should be producible by a factory" do
+    (FactoryGirl.create :meta_context_group).should_not == nil
+  end
+
 end
