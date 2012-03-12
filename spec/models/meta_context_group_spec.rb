022a274d45392dc18e311347734c50162f2cfd00
Thomas Schank <DrTom@schank.ch>
2012-03-12 10:21:06 +0100

    added specs for relation of meta_context and its group

--- spec/models/meta_context_group_spec.rb
+++ spec/models/meta_context_group_spec.rb
@@ -6,4 +6,24 @@ describe MetaContextGroup do
     (FactoryGirl.create :meta_context_group).should_not == nil
   end
 
+  context "existing context_group and context" do
+
+    before :all do
+      @context_group = (FactoryGirl.create :meta_context_group)
+      @context = (FactoryGirl.create :meta_context)
+    end
+
+    it "should be possible to append an context" do
+      expect { @context_group.meta_contexts << @context }.not_to raise_error
+      @context.meta_context_group.should == @context_group
+    end
+
+    it "should remove the group from the conext if the group is destroyed" do
+      @context_group.meta_contexts << @context
+      @context_group.destroy
+      @context.reload().meta_context_group.should == nil
+    end
+
+  end
+
 end
