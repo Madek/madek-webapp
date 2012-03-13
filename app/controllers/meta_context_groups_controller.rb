5160c91f2bebab740d95ea36c39bf271e0190694
Thomas Schank <DrTom@schank.ch>
2012-03-13 09:50:07 +0100

    added api docu for meta_context_groups controller

--- app/controllers/meta_context_groups_controller.rb
+++ app/controllers/meta_context_groups_controller.rb
@@ -1,5 +1,15 @@
 class MetaContextGroupsController< ApplicationController
 
+  ##
+  #
+  # @resouce /meta_context_groups
+  #
+  # @action GET
+  #
+  # @example_request {}
+  # @example_response  [{"id":1,"name":"TheGroupName"}]
+  #
+  ##
   def index
     @meta_context_groups = MetaContextGroup.all
   end
