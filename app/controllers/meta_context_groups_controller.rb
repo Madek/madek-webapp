19cf9f65524e93c92247354c50394eb9d52f5419
Thomas Schank <DrTom@schank.ch>
2012-03-14 11:40:08 +0100

    updated api docu for MetaContextGroups

--- app/controllers/meta_context_groups_controller.rb
+++ app/controllers/meta_context_groups_controller.rb
@@ -1,15 +1,19 @@
 class MetaContextGroupsController< ApplicationController
 
   ##
+  # returns a list of all MetaContextGroups
   #
-  # @resouce /meta_context_groups
+  # @resource /meta_context_groups
   #
   # @action GET
   #
   # @example_request {}
   # @example_response  [{"id":1,"name":"TheGroupName"}]
   #
-  ##
+  # @response_field [integer] id  The id of the MetaContextGroup.
+  # @response_field [string] name   The name of the MetaContextGroup.
+  # 
+  #
   def index
     @meta_context_groups = MetaContextGroup.all
   end
