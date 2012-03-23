ae9ee1ebd2ce6bd99c2fa51195018b5d6412dd13
Ramón Cahenzli <rca@psy-q.ch>
2012-03-23 11:40:16 +0100

    Fixed demo recipe.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -41,12 +41,11 @@ task :retrieve_db_config do
 end
 
 task :link_config do
-  on_rollback { run "rm #{release_path}/config/database.yml" }
-  run "rm #{release_path}/config/database.yml"
+  run "rm -f #{release_path}/config/database.yml"
   run "ln -s #{db_config} #{release_path}/config/database.yml"
   run "ln -s #{ldap_config} #{release_path}/config/LDAP.yml"
 
-  run "rm #{release_path}/config/zencoder.yml"
+  run "rm -f #{release_path}/config/zencoder.yml"
   run "ln -s #{zencoder_config} #{release_path}/config/zencoder.yml"
 end
 
