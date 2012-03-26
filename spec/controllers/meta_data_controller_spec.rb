c1d32203e7c81f27bed6d4b4dc23b16062d4e9e7
Thomas Schank <DrTom@schank.ch>
2012-03-26 15:41:10 +0200

    added spec for API of meta_data controller

--- spec/controllers/meta_data_controller_spec.rb
+++ spec/controllers/meta_data_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe MediaResourcesController do
+describe MetaDataController do
   render_views
 
 
@@ -15,5 +15,19 @@ describe MediaResourcesController do
   end
 
 
+  describe "as JSON" do
+    it "" do
+      get :index, {format: 'json', media_set_id: @media_resource.id}, session
+      response.should  be_success
+      json = JSON.parse(response.body)
+
+      json.detect{|e| e["key"] == "title"}.should be
+      json.detect{|e| e["key"] == "title"}["type"].should == "String"
+
+      json.detect{|e| e["key"] == "owner"}.should be
+      json.detect{|e| e["key"] == "owner"}["type"].should == "User"
+    end
+
+  end
 
 end
