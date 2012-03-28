a022f4352e6701cd858ad01748bf6d36f7f4e759
Thomas Schank <DrTom@schank.ch>
2012-03-28 10:48:43 +0200

    fixed meta_... factories

--- spec/factories/models/meta_key_definition.rb
+++ spec/factories/models/meta_key_definition.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :meta_key_definition do
     meta_key {FactoryGirl.create :meta_key}
     meta_context  {FactoryGirl.create :meta_context}
+    position {(MetaKeyDefinition.order("position desc").first.try(&:position) || 0) + 1 }
   end
 end
