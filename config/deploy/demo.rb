4d26e8f5ccc15f37b49c4d744a1c34e054a42431
Ramón Cahenzli <rca@psy-q.ch>
2012-03-30 10:24:49 +0200

    The Capistrano guys changed everything about RVM support.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -2,6 +2,7 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+set :rvm_type, :system
 
 require "bundler/capistrano"
 
