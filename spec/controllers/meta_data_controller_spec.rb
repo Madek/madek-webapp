58dc94def7b8513bfb9ff5bd47999924ff194ca2
Thomas Schank <DrTom@schank.ch>
2012-03-30 12:35:53 +0200

    fixet test for update of meta_data

--- spec/controllers/meta_data_controller_spec.rb
+++ spec/controllers/meta_data_controller_spec.rb
@@ -6,6 +6,7 @@ describe MetaDataController do
 
   before :each do
     @user = FactoryGirl.create :user
+    @other_user = FactoryGirl.create :user
     @media_set= FactoryGirl.create :media_set, user: @user
     @media_set.meta_data.create(:meta_key => MetaKey.find_by_label("title"), :value => Faker::Lorem.words(4).join(' '))
     @title_meta_datum =  @media_set.meta_data.joins(:meta_key).where("meta_keys.label = ?","title").first
@@ -15,32 +16,18 @@ describe MetaDataController do
     {:user_id => @user.id}
   end
 
+
   describe "PUT" do
 
     it "should update a title value" do
-      put :update, {media_resource_id: @media_set.id, id: "title", value: "My new title"}, valid_session
+      put :update, {media_resource_id: @media_set.id, id: "title", value: "My new title", format: "json"}, valid_session
       response.should be_success
       @media_set.title.should == "My new title"
     end
 
-    it "should not update meta_data if the user doesn't have manage permissions" do
-      other_user = FactoryGirl.create :user
-      other_media_set= FactoryGirl.create :media_set, user: other_user
-      other_media_set.meta_data.create(:meta_key => MetaKey.find_by_label("title"), :value => Faker::Lorem.words(4).join(' '))
-      other_title_meta_datum =  other_media_set.meta_data.joins(:meta_key).where("meta_keys.label = ?","title").first
-
-      put :update, {id: other_title_meta_datum.id, meta_datum: {value: "BLAH"}}, valid_session
-      response.should_not  be_success
-      other_title_meta_datum.reload.value.should_not  == "BLAH"
-
-    end
-
-    it "should not be possible to change the media_resource_id " do
-      other_user = FactoryGirl.create :user
-      other_media_set= FactoryGirl.create :media_set, user: other_user
-      put :update, {id: @title_meta_datum.id, meta_datum: {media_resource_id: other_media_set.id}}, valid_session
-      response.should_not  be_success
-      @title_meta_datum.reload.media_resource_id.should_not == @media_set.id
+    it "should not update meta_data if the user doesn't have edit permissions" do
+      put :update, {media_resource_id: @media_set.id, id: "title", value: "My new title", format: "json"}, {user_id: @other_user_id}
+      response.should_not be_success
     end
 
   end
