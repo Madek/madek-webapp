382b7b0c7f5642fc6352f8107230afcaa5b95eae
Ramón Cahenzli <rca@psy-q.ch>
2012-04-04 16:54:34 +0200

    Application config needs to be changed so that precompiling assets works properly.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
+
 module MAdeK
   class Application < Rails::Application
 
