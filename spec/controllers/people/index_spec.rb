11b545b334c7a21f7cf8e22301f8ddc6e6484cf1
Sebastian Pape <email@sebastianpape.com>
2012-04-04 13:59:53 +0200

    Fixing spec which defines the format of persons

--- spec/controllers/people/index_spec.rb
+++ spec/controllers/people/index_spec.rb
@@ -4,13 +4,12 @@ describe PeopleController do
   render_views
   
   before :all do
-    @normin = FactoryGirl.create :user, login: "normin"
-    @adam= FactoryGirl.create :user, login: "adam", 
-      person: FactoryGirl.create(:person, firstname: "Adam"  )
+    @user1 = FactoryGirl.create :user
+    @user2= FactoryGirl.create :user, person: FactoryGirl.create(:person)
   end
 
   let :session do
-    {user_id: @normin.id}
+    {user_id: @user1.id}
   end
 
   describe "GET index" do
@@ -18,15 +17,37 @@ describe PeopleController do
       it "should find all users" do
         get :index, {format: :json}, session
         json = JSON.parse(response.body)
-        expected = Person.all.map {|x| {"id" => x.id, "name" => x.to_s}}
-        (json | expected).eql?(json & expected).should be_true
+        expected = Person.all.map {|x| 
+          {
+            id: x.id,
+            firstname: x.firstname,
+            lastname: x.lastname,
+            birthdate: x.birthdate,
+            deathdate: x.deathdate,
+            is_group: x.is_group,
+            nationality: x.nationality,
+            pseudonym: x.pseudonym           
+          }
+        }
+        json.to_json.eql?(expected.to_json).should be_true
       end
 
       it "should find matching people" do
-        get :index, {format: :json, query: "adam"}, session
+        get :index, {format: :json, query: @user2.person.firstname.downcase}, session
         json = JSON.parse(response.body)
-        expected = [{"id" => @adam.person.id, "name" => @adam.person.to_s}]
-        (json | expected).eql?(json & expected).should be_true
+        expected = [@user2.person].map {|x| 
+          {
+            id: x.id,
+            firstname: x.firstname,
+            lastname: x.lastname,
+            birthdate: x.birthdate,
+            deathdate: x.deathdate,
+            is_group: x.is_group,
+            nationality: x.nationality,
+            pseudonym: x.pseudonym           
+          }
+        }
+        json.to_json.eql?(expected.to_json).should be_true
       end
     end
   end
