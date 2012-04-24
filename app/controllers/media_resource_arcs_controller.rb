df8766a921137d67cd2e27328b174ad907459f05
Thomas Schank <DrTom@schank.ch>
2012-04-24 09:36:25 +0200

    implemented update arc

--- app/controllers/media_resource_arcs_controller.rb
+++ app/controllers/media_resource_arcs_controller.rb
@@ -6,6 +6,24 @@ class MediaResourceArcsController < ApplicationController
   end
 
   def update_arcs
+    ActiveRecord::Base.transaction do
+
+      begin 
+        JSON.parse(request.body).each do |arc_params| 
+          MediaResourceArc \
+            .where(parent_id: arc_params[:parent_id])
+            .where(child_id: arc_params[:child_id])
+            .first.update(arc_params)
+        end
+
+      rescue  Exception => e
+        respond_to do |format|
+          format.json { render json: e, status: :unprocessable_entity }
+        end
+      end
+
+
+    end
   end
 
   def get_arc
