76e7115a4ece2b8ac134681f749e96eeccb7af7a
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-05-10 16:25:26 +0200

    [WIP] refactoring as_json to rjson

---
+++ app/helpers/json_helper.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+module JsonHelper
+
+  def render_json(source, type)
+    action = source.is_a?(Array) ? :index : :show 
+    partial = [type, action].join('/')
+    
+    with = case type
+      when :media_resources
+        { :media_type => true,
+          :image=>{:as=>"base64", :size=>"large"},
+          :meta_data => {:meta_context_names => ["core"]}}
+    end
+    
+    render(:partial => "#{partial}.json.rjson", :locals => {type => source, :with => with})
+  end
+  
+end
