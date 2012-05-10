d9b1cc92a0487b955b44674813ae571cc8244b9e
Ramón Cahenzli <rca@psy-q.ch>
2012-05-10 13:43:53 +0200

    Make this work in production (because deployment to the personas server happens in production.

--- lib/load_personas.rb
+++ lib/load_personas.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'persona'
+require Rails.root+'features/support/personas' 
 Persona.create("Adam") # Admin should be created first, he setting up the application
 Persona.create("Normin")
 Persona.create("Petra")
