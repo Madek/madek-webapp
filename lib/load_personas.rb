b9d48f432f47ab60ed2893495d249b6a4ff5f905
Ramón Cahenzli <rca@psy-q.ch>
2012-05-10 11:57:20 +0200

    Separate script to load personas (used on the personas test server)

---
+++ lib/load_personas.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'persona'
+Persona.create("Adam") # Admin should be created first, he setting up the application
+Persona.create("Normin")
+Persona.create("Petra")
+Persona.create("Beat")
+Persona.create("Liselotte")
+Persona.create("Norbert")
