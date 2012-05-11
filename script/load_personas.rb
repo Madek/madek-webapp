5c038c76cb9fac24907c1d6837163fd36d8947cc
Ramón Cahenzli <rca@psy-q.ch>
2012-05-11 11:41:45 +0200

    That was the wrong idea.

--- script/load_personas.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require Rails.root+'features/support/personas' 
-Persona.create("Adam") # Admin should be created first, he setting up the application
-Persona.create("Normin")
-Persona.create("Petra")
-Persona.create("Beat")
-Persona.create("Liselotte")
-Persona.create("Norbert")
