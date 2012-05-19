553f419ba57b0524a43ef521467321743e0ceb29
Thomas Schank <DrTom@schank.ch>
2012-05-19 10:01:17 +0200

    changed migration to be mysql compliant, to be verified

--- db/migrate/20120518204323_migrate_meta_data_strings.rb
+++ db/migrate/20120518204323_migrate_meta_data_strings.rb
@@ -1,26 +1,47 @@
 class MigrateMetaDataStrings < ActiveRecord::Migration
+
+  def migrate_to_string_meta_datum md
+    md.string = md.value
+    md.value = nil
+    md.save!
+    md.update_column :type, "MetaDatumString"
+  end
+
   def up
 
     MetaDatum.select("meta_data.*").joins(:meta_key).where(meta_keys: {object_type: nil}).each do |md|
-      md.string = md.value
-      md.save!
+      migrate_to_string_meta_datum md
     end
 
-    execute <<-SQL
-      UPDATE meta_data
-        SET type = 'MetaDatumString',
-        value = NULL
-        WHERE id in (
-        SELECT meta_data.id from meta_data, meta_keys
-        WHERE true
-        AND meta_data.meta_key_id = meta_keys.id
-        AND meta_keys.object_type is NULL)
-    SQL
+    MetaDatum.select("meta_data.*").joins(:meta_key).where(meta_keys: {object_type: 'MetaCountry'}).each do |md|
+      migrate_to_string_meta_datum md
+    end
+
+
+#    execute <<-SQL
+#      UPDATE meta_data
+#        SET type = 'MetaDatumString',
+#        value = NULL
+#        WHERE id in (
+#        SELECT meta_data.id from meta_data, meta_keys
+#        WHERE true
+#        AND meta_data.meta_key_id = meta_keys.id
+#        AND meta_keys.object_type is NULL)
+#    SQL
 
 
   end
 
   def down
-    raise "irreversible migration"
+
+    MetaDatumString.all.each do |mds|
+      mds.value = mds.string
+      mds.string = nil
+      mds.save!
+      mds.update_column :type, nil
+    end
+
+    #raise "not just yet"
   end
+
 end
