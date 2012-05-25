08ad3d0257e8951b5ed6dcd9a75a1a0ea8256b7c
Thomas Schank <DrTom@schank.ch>
2012-05-25 14:31:12 +0200

    added MetaDatumPerson model and some specs

---
+++ app/models/meta_datum_person.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+ 
+class MetaDatumPerson < MetaDatum
+  has_and_belongs_to_many :people, 
+    join_table: :meta_data_people, 
+    foreign_key: :meta_datum_id, 
+    association_foreign_key: :person_id
+end
+
+
