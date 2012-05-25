0b160c4d3f45f07a1a30fdecd78c1b829504a21d
Thomas Schank <DrTom@schank.ch>
2012-05-25 09:14:32 +0200

    updated docu and cleaned-up some stuff

--- lib/try_stuff.rb
+++ lib/try_stuff.rb
@@ -1,20 +1,3 @@
-
-class MetaDate
-  attr_accessor :timestamp, 
-                :timezone,
-                :free_text
-end
-
-
-
 module TryStuff
-
-
-SerDate =%Q{---
-- !ruby/object:MetaDate            
-  free_text: 08.12.2010, 14:28     
-  timestamp: 1291814880            
-  timezone: "+01:00"               
-}
 
 end
