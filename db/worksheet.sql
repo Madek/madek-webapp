a695ba6e7db0e18ed7b2067d89dffdf0221986a0
Thomas Schank <DrTom@schank.ch>
2012-05-25 10:42:40 +0200

    stable order of minimal_meta data; new minimal meta data

--- db/worksheet.sql
+++ db/worksheet.sql
@@ -1,4 +1,10 @@
 
+SELECT * FROM meta_data, meta_keys
+  WHERE true
+  AND meta_data.meta_key_id = meta_keys.id
+  AND meta_keys.object_type = 'Person';
+  
+
 select * from meta_data 
 where true
 AND meta_data.media_resource_id = 27
--- lib/development_helpers/meta_data_preset.rb
+++ lib/development_helpers/meta_data_preset.rb
@@ -4,34 +4,36 @@ module DevelopmentHelpers
       def create_hash
         h = {}
 
-        h[:meta_terms] = MetaTerm.all.collect(&:attributes)
+        h[:usage_terms] = UsageTerm.current.attributes
 
-        h[:meta_keys] = MetaKey.all.collect do |meta_key|
+        h[:meta_keys] = MetaKey.order("id").all.collect do |meta_key|
           a = meta_key.attributes
-          a["meta_terms"] = meta_key.meta_terms.collect(&:id) if meta_key.object_type == "MetaTerm"
+          a["meta_terms"] = meta_key.meta_terms.order("id").collect(&:id) if meta_key.object_type == "MetaTerm"
           a
         end
 
-        h[:meta_context_groups] = MetaContextGroup.all.collect do |meta_context_group|
-          meta_context_group.attributes.select {|k,v| not v.blank? }
-        end
+        h[:meta_terms] = MetaTerm.order("id").all.collect(&:attributes)
 
-        h[:meta_contexts] = MetaContext.all.collect do |meta_context|
+
+        h[:meta_contexts] = MetaContext.order("id").all.collect do |meta_context|
           meta_context.attributes.select {|k,v| not v.blank? }
         end
 
-        h[:meta_key_definitions] = MetaKeyDefinition.all.collect do |meta_key_definition|
+        h[:meta_context_groups] = MetaContextGroup.order("id").all.collect do |meta_context_group|
+          meta_context_group.attributes.select {|k,v| not v.blank? }
+        end
+
+        h[:meta_key_definitions] = MetaKeyDefinition.order("id").all.collect do |meta_key_definition|
           meta_key_definition.attributes.select {|k,v| not v.blank? and not k =~ /ated_at$/ }
         end
 
-        h[:permission_presets] = PermissionPreset.all.collect do |permission_preset|
+        h[:permission_presets] = PermissionPreset.order("id").all.collect do |permission_preset|
           permission_preset.attributes.select {|k,v| not v.blank? and not k =~ /ated_at$/ }
         end
 
         #future#
         #      h[:copyrights] = Copyright.all.collect(&:attributes)
 
-        h[:usage_terms] = UsageTerm.current.attributes
 
         h
       end 
