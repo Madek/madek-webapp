4a77dfb99c5627cebee3602cf2d4ee64d026c1ee
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-07 12:25:36 +0200

    Revert "using update_column instead of update_attributes"
    This reverts commit 3af2218430ee42da42886d6b1da5e85b5a1d6455.

--- db/migrate/20120523133512_migrate_meta_dates_data.rb
+++ db/migrate/20120523133512_migrate_meta_dates_data.rb
@@ -11,8 +11,7 @@ module MigrationHelpers
           else
             ""
           end
-        raw_meta_datum.update_column :string, new_string_value 
-        raw_meta_datum.update_column :value, nil
+        raw_meta_datum.update_attributes(string: new_string_value, value: nil)
         raw_meta_datum.save!
       end
 
