8d57a792433e91817599c990d9a8635a315b7c5a
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-08 14:06:40 +0200

    "better" monkey patch

--- config/initializers/active_record_patch.rb
+++ config/initializers/active_record_patch.rb
@@ -1,8 +1,10 @@
-#sellittf#
+#sellittf# FIXME this is just a monkey patch
 class ActiveRecord::Associations::Association
   def build_record_with_owner(attributes, options)
-    new_attributes = scoped.scope_for_create.symbolize_keys.merge(attributes)
-    build_record_without_owner(new_attributes, options)
+    if reflection.klass == MetaDatum and (attributes.keys & [:media_resource, :media_resource_id]).empty?
+      attributes = {:media_resource => owner}.merge(attributes)
+    end
+    build_record_without_owner(attributes, options)
   end
   alias_method_chain :build_record, :owner
 end
