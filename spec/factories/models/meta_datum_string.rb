0d279aec894b8e567ca70014020bd0be75969387
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-12 19:37:28 +0200

    Factory: make sure :meta_datum_string has a :meta_key

--- spec/factories/models/meta_datum_string.rb
+++ spec/factories/models/meta_datum_string.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :meta_datum_string do
     string {Faker::Lorem.words.join(" ")}
-    meta_key {MetaKey.find_by_label "title"}
+    meta_key { MetaKey.find_by_label("title") || FactoryGirl.create(:meta_key, :label => "title") }
     media_resource {FactoryGirl.create :media_resource}
   end
 
