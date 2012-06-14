e6173f18f06d7e67c18d357b5cf43935489f311e
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-15 00:28:00 +0200

    never ending meta_data refactoring

--- app/models/meta_datum_departments.rb
+++ app/models/meta_datum_departments.rb
@@ -2,15 +2,27 @@
 
 class MetaDatumDepartments < MetaDatum
   has_and_belongs_to_many :meta_departments, 
-    class_name: MetaDepartment.name,
     join_table: :meta_data_meta_departments,
     foreign_key: :meta_datum_id,
     association_foreign_key: :meta_department_id
  
-  alias_attribute :value, :meta_departments
-
   def to_s
     deserialized_value.map(&:to_s).join("; ")
+  end
+
+  def value
+    meta_departments
+  end
+
+  def value=(new_value)
+    meta_departments.clear
+    meta_departments << Array(new_value).map do |v|
+      if v.is_a?(Fixnum) or (v.respond_to?(:is_integer?) and v.is_integer?)
+        MetaDepartment.find_by_id(v)
+      else
+        v
+      end
+    end
   end
 
 end
--- app/models/meta_datum_meta_terms.rb
+++ app/models/meta_datum_meta_terms.rb
@@ -6,10 +6,30 @@ class MetaDatumMetaTerms < MetaDatum
     foreign_key: :meta_datum_id, 
     association_foreign_key: :meta_term_id
 
-  alias_attribute :value, :meta_terms
-
   def to_s
     deserialized_value.map(&:to_s).join("; ")
+  end
+
+  def value
+    meta_terms
+  end
+
+  def value=(new_value)
+    meta_terms.clear
+    meta_terms << Array(new_value).map do |v|
+      if v.is_a?(Fixnum) or (v.respond_to?(:is_integer?) and v.is_integer?)
+        # TODO check if is member of meta_key.meta_terms
+        MetaTerm.find_by_id(v)
+      elsif meta_key.is_extensible_list?
+        h = {}
+        LANGUAGES.each do |lang|
+          h[lang] = v
+        end
+        term = MetaTerm.find_or_build_by_en_gb_and_de_ch(h)
+        meta_key.meta_terms << term unless meta_key.meta_terms.include?(term)
+        term
+      end
+    end
   end
 
 end
