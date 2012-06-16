1f332ef7ca70e3f49d9e1f02fd52c9a9ceb45258
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-16 21:50:29 +0200

    copyright json

--- app/helpers/json/copyright_helper.rb
+++ app/helpers/json/copyright_helper.rb
@@ -2,12 +2,7 @@ module Json
   module CopyrightHelper
 
     def hash_for_copyright(copyright, with = nil)
-        h = copyright.as_json
-        h["children"] = copyright.children
-        h["children"].each do |child|
-          child["children"] = child.children
-        end
-        h
+      copyright.as_json(:include => {:children => {:include => :children}})
     end
   end
 end
