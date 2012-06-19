4bd9c23a846d1d3e118e32d786083beb19d0efdf
Thomas Schank <DrTom@schank.ch>
2012-06-19 15:20:13 +0200

    Factory with only one title for stable sorting

--- spec/factories/models/media_set.rb
+++ spec/factories/models/media_set.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
   factory :media_set_with_title, class: "MediaSet" do
     user {User.find_random || (FactoryGirl.create :user)}
     after(:create) do |ms| 
-      MetaDatum.create media_resource: ms, meta_key: MetaKey.find_by_label(:title), value: Faker::Lorem.words.join(" ")
+      MetaDatum.create media_resource: ms, meta_key: MetaKey.find_by_label(:title), value: Faker::Lorem.words[0]
     end
   end
 
