5d9babcc422dd24b1f3649ad9a5a63591fbecd0c
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-06-21 13:31:31 +0200

    simpler migration

--- db/migrate/20120620125825_add_zhdkid_to_users.rb
+++ db/migrate/20120620125825_add_zhdkid_to_users.rb
@@ -4,13 +4,7 @@ class AddZhdkidToUsers < ActiveRecord::Migration
   def up
     add_column :users, :zhdkid, :integer
 
-    user_klass = Class.new(ActiveRecord::Base) do
-      self.table_name = "users"
-    end
-
-    user_klass.all.each do |user|
-      user.update_attributes({zhdkid: user.id}) unless user.password
-    end
+    User.update_all("zhdkid = id", {password: nil})
 
   end
 
