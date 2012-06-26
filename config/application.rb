f61bc5370d525e895b176487a8eb46b430225c27
Ramón Cahenzli <rca@psy-q.ch>
2012-06-26 14:11:15 +0200

    Bumping app version.

--- config/application.rb
+++ config/application.rb
@@ -105,4 +105,4 @@ DEFAULT_LANGUAGE = :de_ch
 ENCODING_BASE_URL = "http://test:MAdeK@test.madek.zhdk.ch"
 ENCODING_TEST_MODE = 1 # 1 for true, 0 for false
 
-RELEASE_VERSION = "0.6.0"
+RELEASE_VERSION = "0.7.0"
