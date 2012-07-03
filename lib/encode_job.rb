3eebd41f92de569c31040670bcd339fac65227bd
Ramón Cahenzli <rca@psy-q.ch>
2012-07-03 17:12:13 +0200

    Fix to encode_job

--- lib/encode_job.rb
+++ lib/encode_job.rb
@@ -155,16 +155,22 @@ class EncodeJob
   def self.http_get(source_url, target_filename)
     require 'net/http'
     uri = URI.parse(source_url)
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
 
-      request = Net::HTTP::Get.new uri.request_uri
-      http.request request do |response|
-        open target_filename, 'wb' do |io|
-          response.read_body do |chunk|
-            io.write chunk
+    begin
+      Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        request = Net::HTTP::Get.new uri.request_uri
+        http.request request do |response|
+          open target_filename, 'wb' do |io|
+            response.read_body do |chunk|
+              io.write chunk
+            end
           end
         end
       end
+      return true
+    rescue
+      # So that callers know something went wrong. We must handle the proper exceptions above
+      return false
     end
   end
 
