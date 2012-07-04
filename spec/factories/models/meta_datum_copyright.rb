163554e1260862e82bcf625f89f055dbb066948e
Thomas Schank <DrTom@schank.ch>
2012-07-04 10:32:04 +0200

    Fixed reading of MetaDatumCopyright

--- spec/factories/models/meta_datum_copyright.rb
+++ spec/factories/models/meta_datum_copyright.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :meta_datum_copyright do
     meta_key { MetaKey.find_by_label "copyright status" } 
     media_resource {FactoryGirl.create :media_resource}
+    copyright Copyright.first
   end
 
 end
