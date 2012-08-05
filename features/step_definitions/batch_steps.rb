95fb08f6c6657f0e1cac8f7b90a8a101411cec4e
Sebastian Pape <email@sebastianpape.com>
2012-08-05 20:15:02 +0200

    [FIX CI] Batch edit media entries.Use the batch's Select all button

--- features/step_definitions/batch_steps.rb
+++ features/step_definitions/batch_steps.rb
@@ -10,6 +10,8 @@ When /^I use batch's select all$/ do
 end
 
 When /^I use batch's deselect all$/ do
+  wait_until {find("#batch-deselect-all")}
+  sleep 1.0
   find("#batch-deselect-all").click()
   sleep 1.0
 end
