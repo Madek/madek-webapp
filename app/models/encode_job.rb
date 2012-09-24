245e530b4ec2da26b076d647d69974d00897da2b
Ramón Cahenzli <rca@psy-q.ch>
2012-09-24 14:46:36 +0200

    Moving encode job to a proper location, writing proper docs.

--- app/models/encode_job.rb
+++ app/models/encode_job.rb
@@ -9,18 +9,57 @@ require 'net/ftp'
 
 # use the API builder to build requests: https://app.zencoder.com/api_builder
 
+# @author Ramón Cahenzli <rca@psy-q.ch>
 class EncodeJob
-
   @@config_path = File.join(Rails.root,"config/zencoder.yml")
 
-  attr_accessor :job_id # Unique job ID that the encoder system (e.g. Zencoder) should assign to us
-  attr_accessor :base_url # Output location where finished encodes should be stored
-                          # (FTP or SFTP URL including username/password)
-  attr_accessor :size    # The target sizes for the encode job. Ignored for audio-only jobs.
+  # Unique job ID that the encoder system (e.g. Zencoder) should assign to us
+  # @return [Integer]
+  attr_accessor :job_id 
+
+  # Output location where finished encodes should be stored. FTP or SFTP URL including username/password.
+  # @return [String]
+  # @example 
+  #   "http://foo:bar@ftp.foo.com/encoded"
+  attr_accessor :base_url
+
+  # The target sizes for the encode job. Ignored for audio-only jobs.
+  # @return [String]
+  # @example 
+  #   "640x480"
+  # @see https://app.zencoder.com/docs/api/encoding/resolution/size
+  attr_accessor :size
+
+  # Video codec to use. Usually this is 'vp8' (this encodes to WebM)
+  # @return [String]
+  # @example
+  #   "vp8"
+  # @see https://app.zencoder.com/docs/api/encoding/format-and-codecs/video-codec
   attr_accessor :video_codec  # Usually 'vp8'
-  attr_accessor :audio_codec # Usually 'vorbis'
+
+  # Audio codec to use. Usually this is 'vorbis' (Ogg Vorbis)
+  # @return [String]
+  # @example
+  #   "vorbis"
+  # @see https://app.zencoder.com/docs/api/encoding/format-and-codecs/audio-codec
+  attr_accessor :audio_codec
+
+  # Whether this is a video or an audio job. For audio jobs, video settings are
+  # ignored.
+  # @return [String]
+  # @example Setting for a video job
+  #   "video"
+  # @example Setting for an audio job
+  #   "audio"
   attr_accessor :job_type # video or audio
 
+  # Initialize with some necessary defaults, such as usable audio and video codecs.
+  # This is done in the constructor so that when we fire off a new job at Zencoder, we're
+  # reasonably sure that it has a valid configuration by default.
+  #
+  # @example job = EncodeJob.new("12347727983")
+  # @param [Integer] job_id Job ID as returned by Zencoder's API
+  # @return nil
   def initialize(job_id = nil)
 
     raise 'Configuration @@config_path not found or malformed.' unless configured?
@@ -46,6 +85,11 @@ class EncodeJob
     Zencoder.api_key = api_key
   end
 
+  # Checks if there is a Zencoder API key and FTP base URL in a configuration file (by default
+  # this is config/zencoder.yml). If everything *seems* OK, returns true. Note that just because
+  # the configuration is OK, there can be other things stopping the encode from working (e.g.
+  # network not reachable, wrong API key, unreachable FTP destination...)
+  # @return [Boolean]
   def configured?
     configured = false
     if File.exists?(@@config_path)
@@ -57,11 +101,10 @@ class EncodeJob
     return configured
   end
 
-  # TODO: Add notification callback URLs
-  # :notifications => ["http://medienarchiv.zhdk.ch/encode_jobs/notification"]
-
+  # @param [String] url The publicly-accessible URL of the file you want encoded.
   def start_by_url(url)
-
+    # TODO: Add notification callback URLs
+    # :notifications => ["http://medienarchiv.zhdk.ch/encode_jobs/notification"]
     test_mode = 0
     test_mode = 1 if ENCODING_TEST_MODE == 1
 
@@ -104,7 +147,7 @@ class EncodeJob
     state == "finished"
   end
 
-  # Not THAT useful, we should instead extract all info we need manually from +details+
+  # @return [Array] Array of URLs (strings) of the encoded files.
   def encoded_file_urls
     paths = []
     details['output_media_files'].each do |file|
@@ -113,6 +156,7 @@ class EncodeJob
     return paths
   end
 
+  # @return [Array] Array of URLs (strings) of the thumbnails extracted during encoding (only for video jobs)
   def thumbnail_file_urls
     paths = []
     details['thumbnails'].each do |tn|
@@ -121,6 +165,9 @@ class EncodeJob
     return paths
   end
 
+  # @param [String] source_url URL of the file to retrieve by FTP. Must include username and password if necessary.
+  # @param [String] target_filename The filename (full, absolute path) where you want to store the retrieved file.
+  # @option options [Boolean] :delete_after (false) Delete the files from the FTP server after retrieval
   def self.ftp_get(source_url, target_filename, options = {:delete_after => false})
     require 'net/ftp'
     uri = URI.parse(source_url)
@@ -155,6 +202,8 @@ class EncodeJob
     end
   end
 
+  # @param [String] source_url URL of the file to retrieve by HTTP. Must include username and password if necessary.
+  # @param [String] target_filename The filename (full, absolute path) where you want to store the retrieved file.
   def self.http_get(source_url, target_filename)
     require 'net/http'
     uri = URI.parse(source_url)
