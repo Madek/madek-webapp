bac5db3f1db9bf0c6c43de96f320cffcbdd3b4dc
Ramón Cahenzli <rca@psy-q.ch>
2012-09-27 13:20:49 +0200

    More modularization.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -97,12 +97,6 @@ task :generate_documentation do
 end
 
 
-task :clear_cache do
-  # We have to run it this way (in a subshell) because Rails.cache is not available
-  # in Rake tasks, otherwise we could stick a task into lib/tasks/madek.bundle exec rake
-  run "cd #{release_path} && RAILS_ENV=production  bundle exec rails runner 'Rails.cache.clear'"
-end
-
 before "deploy", "retrieve_db_config"
 before "deploy:create_symlink", :make_tmp
 
