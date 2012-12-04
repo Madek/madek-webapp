86f577101bac43849558ebe031307e6dc7af9681
Ramón Cahenzli <rca@psy-q.ch>
2012-12-04 15:50:10 +0100

    Prevent DoS on the server :)

--- app/controllers/media_files_controller.rb
+++ app/controllers/media_files_controller.rb
@@ -10,8 +10,19 @@ class MediaFilesController < ApplicationController
     if @media_file.nil?
       render :text => 'Media file not found. Direct media access is only possible on a request-by-request basis using an access hash. Do you have a valid access hash? If so, append it to the query string: ?access_hash=123-456-789', :status => :not_found
     else
-      send_file @media_file.file_storage_location.to_s, {:type => @media_file.content_type, :filename => @media_file.filename, :disposition => 'attachment'}
+      fixed_send_file @media_file.file_storage_location.to_s, {:type => @media_file.content_type, :filename => @media_file.filename, :disposition => 'attachment'}
     end
+  end
+
+  # send_file() as above seems to be broken in Rails 3.1.3 and onwards?
+  # The Rack::Sendfile#call method never seems to receive a body that respons to :to_path, even though it SHOULD,
+  # therefore Sendfile is never triggered (!!), that's why we need this hacked Sendfile header implementation
+  def fixed_send_file(path, options = {})
+    headers["Content-Type"] = options[:type]
+    headers["Content-Disposition"] = "attachment; filename=\"#{options[:filename]}\""
+    headers["X-Sendfile"] = path.to_s
+    headers["Content-Length"] = '0'
+    render :nothing => true
   end
 
 end
