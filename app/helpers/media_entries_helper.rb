9fe2ebb3fb5a52d2eb0c7bd7cf0f07b611449f9a
Ramón Cahenzli <rca@psy-q.ch>
2012-12-04 18:05:05 +0100

    Read height from the right location.

--- app/helpers/media_entries_helper.rb
+++ app/helpers/media_entries_helper.rb
@@ -75,7 +75,7 @@ module MediaEntriesHelper
           tag :img, options.merge({:src => media_file.thumb_base64(size)})  
         end
       else
-        content_tag :video, {:width => video_preview.width, :height => video_preview.height, :autoplay => '', :controls => 'controls'} do
+        content_tag :video, {:width => video_preview_webm.width, :height => video_preview_webm.height, :autoplay => '', :controls => 'controls'} do
           # This src points to a symlink to the actual file, so that Apache serves it. This lets us support
           # seeking, partial content (HTTP status code 206) and request ranges without any additional work.
           if video_preview_apple
