1f3520735c5021c6a3b05728830da294cea36af1
Ramón Cahenzli <rca@psy-q.ch>
2012-12-05 15:34:36 +0100

    Randomize temp database connection config file.

--- config/deploy/recipes/retrieve_db_config.rb
+++ config/deploy/recipes/retrieve_db_config.rb
@@ -1,7 +1,9 @@
 task :retrieve_db_config do
   # DB credentials needed by mysqldump etc.
-  get(db_config, "/tmp/madek_db_config.yml")
-  dbconf = YAML::load_file("/tmp/madek_db_config.yml")["production"]
+  filename = "madek_db_config_#{rand(1000..50000)}.yml"
+  tmp_dir = "/tmp"
+  get(db_config, "#{tmp_dir}/#{filename}")
+  dbconf = YAML::load_file("#{tmp_dir}/#{filename}")["production"]
   set :sql_database, dbconf['database']
   set :sql_host, dbconf['host']
   set :sql_username, dbconf['username']
