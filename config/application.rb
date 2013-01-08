674a1b81059fc149d12ad26181977d9f5a50860e
Thomas Schank <DrTom@schank.ch>
2013-01-08 09:31:34 +0100

    using concerns

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,11 @@ module MAdeK
     Dir["lib/**/*.rb"].each do |path|
         require_dependency path
     end
-    config.autoload_paths += %W(#{Rails.root}/lib)
+
+    config.autoload_paths += [
+      Rails.root.join("lib"),
+      Rails.root.join("app","models","concerns")
+    ]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
