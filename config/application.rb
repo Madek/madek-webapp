481ebc9b6c62fcf35caffd525decad90bf33c331
Thomas Schank <DrTom@schank.ch>
2013-01-08 13:58:22 +0100

    Factored out graph query

--- config/application.rb
+++ config/application.rb
@@ -18,15 +18,19 @@ module MAdeK
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    #
-    Dir["lib/**/*.rb"].each do |path|
-        require_dependency path
-    end
+    config.autoload_paths += %W(
+      #{Rails.root.join "lib"} 
+      #{Rails.root.join "app","models","concerns"}
+    )
 
     config.autoload_paths += [
       Rails.root.join("lib"),
       Rails.root.join("app","models","concerns")
     ]
+
+    Dir["lib/**/*.rb"].each do |path|
+        require_dependency path
+    end
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
