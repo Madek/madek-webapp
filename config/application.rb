b97eb4aad77c16c96eafe3fb271bee05a68fd477
Sebastian Pape <email@sebastianpape.com>
2013-01-17 14:01:45 +0100

    add upload.js to precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,7 @@ module MAdeK
                                     test.js
                                     pdf.js
                                     pdf-viewer.css
+                                    upload.js
                                   )
 
     # So that the Content-Length header is sent, so that e.g. video files
