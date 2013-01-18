e5e8662650017e6fd3ccdcee2e995d90c1ef9b0e
Sebastian Pape <email@sebastianpape.com>
2013-01-18 17:34:30 +0100

    [FIX #42797671] sorting of featured_set_children

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,7 +53,7 @@ end
       @splashscreen_set = MediaSet.splashscreen
       @splashscreen_set_children = @splashscreen_set.child_media_resources.accessible_by_user(current_user).shuffle if @splashscreen_set
       @featured_set = MediaSet.featured
-      @featured_set_children = @featured_set.child_media_resources.accessible_by_user(current_user).limit(6) if @featured_set
+      @featured_set_children = @featured_set.child_media_resources.accessible_by_user(current_user).ordered_by(:updated_at).limit(6) if @featured_set
       @catalog_set = MediaSet.catalog
       @catalog_set_categories = @catalog_set.categories.accessible_by_user(current_user).limit(3) if @catalog_set
       @latest_media_entries = MediaResource.media_entries.accessible_by_user(current_user).ordered_by(:created_at).limit(12)
