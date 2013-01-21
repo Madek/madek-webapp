c6f3925c5e085d13105ee2f02a559184902766aa
Sebastian Pape <email@sebastianpape.com>
2013-01-21 13:35:28 +0100

    [FIX CI] Dashboard links

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -7,11 +7,11 @@ Then /^I see a block of resources showing my content$/ do
 end
 
 Then /^There is a link to my resources$/ do
-  expect(find "#latest_user_resources_block a[href='/my/media_resources']").to be
+  expect(find "#latest_user_resources_block a[href*='/my/media_resources']").to be
 end
 
 When /^I follow the link to my resources$/ do
-  (find "#latest_user_resources_block a[href='/my/media_resources']").click
+  (find "#latest_user_resources_block a[href*='/my/media_resources']").click
 end
 
 Then /^I am on the my resources page$/ do
@@ -28,11 +28,11 @@ Then /^I see a block of resources showing my favorites$/ do
 end
 
 Then /^There is a link to my favorites$/ do
-  expect(find "#user_favorite_resources_block a[href='/my/favorites']").to be
+  expect(find "#user_favorite_resources_block a[href*='/my/favorites']").to be
 end
 
 When /^I follow the link to my favorites page$/ do
-  (find "#user_favorite_resources_block a[href='/my/favorites']").click
+  (find "#user_favorite_resources_block a[href*='/my/favorites']").click
 end
 
 Then /^I am on the my favorites$/ do
@@ -45,11 +45,11 @@ Then /^I see a block of my keywords$/ do
 end
 
 Then /^There is a link to my keywords$/ do
-  expect(find "#user_keywords_block a[href='/my/keywords']").to be
+  expect(find "#user_keywords_block a[href*='/my/keywords']").to be
 end
 
 When /^I follow the link to my keywords$/ do
-  (find "#user_keywords_block a[href='/my/keywords']").click
+  (find "#user_keywords_block a[href*='/my/keywords']").click
 end
 
 Then /^I am on the my keywords page$/ do
@@ -62,11 +62,11 @@ Then /^I see a block of resources showing content assigned to me$/ do           
 end
 
 Then /^There is a link to content assigned to me$/ do
-  expect(find "#user_entrusted_resources_block a[href='/my/entrusted_media_resources']").to be
+  expect(find "#user_entrusted_resources_block a[href*='/my/entrusted_media_resources']").to be
 end
 
 When /^I follow the link to content assigned to me$/ do
-  (find "#user_entrusted_resources_block a[href='/my/entrusted_media_resources']").click
+  (find "#user_entrusted_resources_block a[href*='/my/entrusted_media_resources']").click
 end
 
 Then /^I am on the content assigned to me page$/ do
@@ -79,12 +79,12 @@ Then /^I see a list of my groups$/ do
 end
 
 Then /^There is a link to my groups$/ do
-  expect(find "#my_groups_block a[href='/my/groups']").to be
+  expect(find "#my_groups_block a[href*='/my/groups']").to be
 end
 
 When /^I follow the link to my groups$/ do
   pending
-  (find "#my_groups_block a[href='/my/groups']").click
+  (find "#my_groups_block a[href*='/my/groups']").click
 end
 
 Then /^I am on the my groups page$/ do
