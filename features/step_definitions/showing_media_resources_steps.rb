d8f13a586be654f1a338550dfcaec8a7c2e90751
Thomas Schank <DrTom@schank.ch>
2013-05-21 14:35:01 +0200

    FIX view movies for guests and add feature test
    Squashed commit of the following:
    
    commit b5433d07c5715ac3a263ad04e4bb38e77f8c2129
    Author: Thomas Schank <DrTom@schank.ch>
    Date:   Tue May 21 10:09:43 2013 +0200
    
        Added feature test for watching a movie as a guest
    
    commit e19a171d97b078cac9fcf90c3448d83586390484
    Author: Thomas Schank <DrTom@schank.ch>
    Date:   Tue May 21 09:53:51 2013 +0200
    
        Enabled Guest to view previews
    
    commit 018eeafa36eb2d2f5691563a9968a3a21a7b41aa
    Author: Thomas Schank <DrTom@schank.ch>
    Date:   Tue May 21 09:11:49 2013 +0200
    
        Added tar of media_files_with_movie for feature test
    
    commit 9037a54c2524ac9d3d0580450ddde964eb2cdca1
    Author: Thomas Schank <DrTom@schank.ch>
    Date:   Tue May 21 09:05:47 2013 +0200
    
        Personas db with movie to test public viewing

--- features/step_definitions/showing_media_resources_steps.rb
+++ features/step_definitions/showing_media_resources_steps.rb
@@ -5,3 +5,13 @@ end
 Then(/^I can see at least "(.*?)" included resources$/) do |snum|
   wait_until(3*Capybara.default_wait_time){ all("ul#ui-resources-list li.ui-resource").size >= snum.to_i }
 end
+
+Given(/^There is a movie with previews and public viewing\-permission$/) do
+  System.execute_cmd! "tar xf #{Rails.root.join "features/data/media_files_with_movie.tar.gz"} -C #{Rails.root.join  "db/media_files/", Rails.env}"
+  @movie = MediaResource.find 113
+  binding.pry
+end
+
+When(/^I visit the page of that movie$/) do
+  visit media_resource_path(@movie)
+end
