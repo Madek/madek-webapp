178a3ea4b69b1ac374af17392bc5ac0445e4fe90
Ramón Cahenzli <rca@psy-q.ch>
2013-05-23 12:58:00 +0200

    Centralized configuration for the stable version.

--- config/deploy/test.arch.ethz.madek.zhdk.ch.rb
+++ config/deploy/test.arch.ethz.madek.zhdk.ch.rb
@@ -9,7 +9,7 @@ require "bundler/capistrano"
 
 set :scm, :git
 set :repository, "git://github.com/zhdk/madek.git"
-set :branch, "1.3.0"
+load 'config/deploy/stable_version'
 set :deploy_via, :remote_cache
 
 set :app, "test.arch.ethz.madek.zhdk.ch"
