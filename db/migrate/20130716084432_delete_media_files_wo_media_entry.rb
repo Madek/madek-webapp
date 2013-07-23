805a22f58477e2eef9a27ef5a439ba463a38da75
Thomas Schank <DrTom@schank.ch>
2013-07-23 10:05:06 +0200

    Use the given MediaFile for deleting unrelated mf in migration

--- db/migrate/20130716084432_delete_media_files_wo_media_entry.rb
+++ db/migrate/20130716084432_delete_media_files_wo_media_entry.rb
@@ -1,25 +1,7 @@
 class DeleteMediaFilesWoMediaEntry < ActiveRecord::Migration
 
-  class ::MyMediaFile < ActiveRecord::Base
-    self.table_name = :media_files
-    has_many :previews, foreign_key: 'media_file_id',dependent:  :destroy 
-
-    def shard
-      self.guid[0..0]
-    end
-
-    after_commit on: :destroy do
-      File.delete(file_storage_location)
-    end
-
-    def file_storage_location
-      File.join(FILE_STORAGE_DIR, shard, guid)
-    end
-
-  end
-
   def up
-    to_be_destroyed = MyMediaFile.where <<-SQL 
+    to_be_destroyed = MediaFile.where <<-SQL 
         NOT EXISTS 
           ( SELECT id FROM media_resources WHERE media_resources.media_file_id = media_files.id )
     SQL
