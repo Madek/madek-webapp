24939964e52005e800ac2cb6bf3977a0ac9159ea
Thomas Schank <DrTom@schank.ch>
2013-08-22 10:41:59 +0200

    Removed database cleaner from rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,7 +49,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
 
   # If true, the base class of anonymous controllers will be inferred
@@ -62,26 +62,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-end
-
-
-# Include DatabaseCleaner automatically
-
-RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
- 
-  config.before :each  do
-    DatabaseCleaner.start
-  end
-
-  config.after :each  do
-    DatabaseCleaner.clean
-  end
-
 
 end
