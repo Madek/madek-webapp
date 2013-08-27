947185d32ff7639e8c8aace141a29df55aa8ed4f
Thomas Schank <DrTom@schank.ch>
2013-08-27 11:49:47 +0200

    Feature tests run in strict mode
    and refactored domina tasks creation to write to stdout

--- domina_rails/bin/create_feature_tasks.rb
+++ domina_rails/bin/create_feature_tasks.rb
@@ -4,15 +4,13 @@ require 'pry'
 
 def task_for_feature_file file_path
   name= file_path.match(/features\/(.*)\.feature/).captures.first
-  exec = %{bundle exec cucumber "#{file_path}"}
+  exec = %{bundle exec cucumber --strict "#{file_path}"}
   {"name" => name,
     "scripts" => {
     "main" => {
     "body" => exec } } }
 end
 
-ARGV[0] and File.open(ARGV[0],'w') do |file|
-  file.write(
-    Dir.glob("features/**/*.feature") \
-    .map{|f| task_for_feature_file(f)}.to_yaml)
-end
+STDOUT.write \
+  Dir.glob("features/**/*.feature") \
+  .map{|f| task_for_feature_file(f)}.to_yaml
--- domina_rails/bin/create_rspec_tasks.rb
+++ domina_rails/bin/create_rspec_tasks.rb
@@ -11,8 +11,6 @@ def task_for_rspec_file file_path
     "body" => exec } } }
 end
 
-ARGV[0] and File.open(ARGV[0],'w') do |file|
-  file.write(
-    Dir.glob("spec/**/*_spec.rb") \
-    .map{|f| task_for_rspec_file(f)}.to_yaml)
-end
+STDOUT.write \
+  Dir.glob("spec/**/*_spec.rb") \
+  .map{|f| task_for_rspec_file(f)}.to_yaml
