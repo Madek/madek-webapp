72f4f73b453c69b9326653da3fad87979a28a373
Thomas Schank <DrTom@schank.ch>
2013-11-11 09:49:03 +0100

    Deploying staging will use the "stanging" branch instead of next

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,7 @@ set :application, "madek"
 
 set :scm, :git
 set :repository, "git://github.com/zhdk/madek.git"
-set :branch, "next"
+set :branch, "staging"
 set :deploy_via, :remote_cache
 
 set :db_config, "/home/rails/madek-test/database.yml"
