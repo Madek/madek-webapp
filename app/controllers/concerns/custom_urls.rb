8b6ee4644931c6c328035c61abdc0a36e84a596f
Thomas Schank <DrTom@schank.ch>
2014-03-07 09:39:39 +0100

    URLs: redirect to proper controller if URL has been transfered ...
      ... to a different MediaResource Type

--- app/controllers/concerns/custom_urls.rb
+++ app/controllers/concerns/custom_urls.rb
@@ -6,24 +6,39 @@ module Concerns
 
     included do 
       helper_method :custom_url_path
-      before_action :redirect_if_not_primary_url, only: [:show]
+      before_action :redirect_to_primary_url, only: [:show]
     end
 
-    def redirect_if_not_primary_url 
-      if UUID_MATCHER.match params[:id]
+    def redirect_to_primary_custom_url custom_url
+      redirect_to custom_url_path(custom_url.media_resource) 
+      return
+    end
+
+    def redirect_to_primary_url
+      if UUID_MATCHER.match params[:id] # id is a UUID; redirect if there is a custom primary url
         if (@media_resource=MediaResource.find(params[:id])) and (primary_custom_url= @media_resource.primary_custom_url)
-          redirect_to custom_url_path(@media_resource,primary_custom_url)
-          return
+          redirect_to_primary_custom_url(primary_custom_url)
         end
       elsif custom_url= CustomUrl.find_by(id: params[:id])
-        if (self.class == MediaResourcesController) or ! custom_url.is_primary?
-          redirect_to custom_url_path(custom_url.media_resource) 
-          return
-        else
-          # this is the primary url of the correct controller
+        unless custom_url.is_primary? # custom_url is not primary so we have to redirect in any case
+          redirect_to_primary_custom_url(custom_url)
+        else # custom_url is primary; yet it could be the wrong contoller
+          case custom_url.media_resource
+          when MediaSet 
+            redirect_to_primary_custom_url(custom_url) unless (self.class == MediaSetsController)
+          when MediaEntry 
+            redirect_to_primary_custom_url(custom_url) unless (self.class == MediaEntriesController)
+          when FilterSet 
+            redirect_to_primary_custom_url(custom_url) unless (self.class == FilterSetsController)
+          else
+            # we are on the primary url of the proper controller ; do nothing
+            # (unless this es a (new) MediaResource, which is not yet handled)
+          end
         end
       else
-        raise "Illegal state exception"
+        # ID of URL is not UUID neither existing primary
+        # fake a ActiveRecord::RecordNotFound which triggers a NOT FOUND page
+        raise ActiveRecord::RecordNotFound.new("ID of URL is not a uuid neither existing primary url.")
       end
     end
 
