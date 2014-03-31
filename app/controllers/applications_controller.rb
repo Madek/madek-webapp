2e6949aa64bfd5008dffa1f21c897cc408d67270
Max F. Albrecht <1@178.is>
2014-03-31 15:57:29 +0200

    app: controller: applications: implement search function (internal API)

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,10 +9,13 @@ class ApplicationsController < ApplicationController
     limit = 5          # how many records per answer?
     whitelist = [:id]  # which properties are we allowed to send?
   
-    if true # TODO: implement searching ¯\_(ツ)_/¯
-      apps= API::Application.all
+      # binding.pry
+      # if there is a query, we search, …
+    if params[:query]
+      apps= API::Application.where("id ilike ?","#{params[:query]}%")
     else
-      # @apps= API::Application.reorder(:autocomplete).where("autocomplete ilike ?","#{params[:search_term]}%").limit(limit)    
+      # otherwise we list everything
+      apps= API::Application.all
     end
   
     # clean up answer (no spilling secrets etc.)
