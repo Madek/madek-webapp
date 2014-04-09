add49fe2d0d42e447b0f2b408868b86c83f73f93
Thomas Schank <DrTom@schank.ch>
2014-04-09 09:30:59 +0200

    Remove jenkings ci aggregation support #CLEANUP

--- lib/ci/post_job_shell_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --login
-
-mkdir -p $WORKSPACE/tmp/capybara;
-rm -rf $WORKSPACE/log && mkdir -p $WORKSPACE/log
-rm -f $WORKSPACE/tmp/*.sql
-mkdir -p $WORKSPACE/tmp/html
-command -v rvm > /dev/null 2>&1 &&  rvm use 1.9.3
-bundle exec rake madek:test:drop_ci_dbs --trace
