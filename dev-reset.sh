506ae1544ec7ef8e876386c0c3ea3a4d22530659
Max F. Albrecht <1@178.is>
2014-04-13 14:51:45 +0200

    dev: start a script collecting the needed step to setup or reset a local dev environment (not finished yet, but works)

---
+++ dev-reset.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "This does a complete reset/setup of the local dev and test environments"
+echo "(Work in Progress)"
+
+set -ex
+
+RAILS_ENV=development bundle exec rake db:drop madek:setup:dirs db:reset madek:db:truncate madek:db:load_data FILE=db/personas.data.psql
+RAILS_ENV=test bundle exec rake db:migrate
+
+
+echo -e "\n\nNow run a clean test!"
