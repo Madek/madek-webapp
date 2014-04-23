5591c3921074a749cffc2b2cbfcff23a2b91194b
Thomas Schank <DrTom@schank.ch>
2014-04-23 10:35:09 +0200

    Fix reset script and move it to dev/reset.sh

--- dev/reset.sh
+++ dev/reset.sh
@@ -6,7 +6,7 @@ echo "(Work in Progress)"
 set -ex
 
 RAILS_ENV=development bundle exec rake db:drop madek:setup:dirs db:reset madek:db:truncate madek:db:load_data FILE=db/personas.data.psql
-RAILS_ENV=test bundle exec rake db:migrate
+RAILS_ENV=test bundle exec rake db:drop madek:setup:dirs db:reset madek:db:truncate madek:db:load_data FILE=db/personas.data.psql
 
 
 echo -e "\n\nNow run a clean test!"
