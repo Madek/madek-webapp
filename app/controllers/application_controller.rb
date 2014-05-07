710ccbc9287501105084a9c9c3a6f42dcb58e27b
Thomas Schank <DrTom@schank.ch>
2014-05-07 15:25:42 +0200

    Reset session when corresponding user is not found #master-hot-fixes #FIX
    Try to avoid endless loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -133,7 +133,13 @@ class ApplicationController < ActionController::Base
     user = nil
 
     if session[:user_id]
-      self.current_user = user = User.find_by_id(session[:user_id])
+
+      begin
+        self.current_user = user = User.find_by_id(session[:user_id])
+      rescue Exception => e
+        reset_session
+        Rails.logger.error e
+      end
 
       return nil unless user
 
