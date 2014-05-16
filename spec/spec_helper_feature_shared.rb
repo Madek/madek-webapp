241f915e76bccbe8597ad60427e9522696660f19
Thomas Schank <DrTom@schank.ch>
2014-05-16 14:23:05 +0200

    Extract shared rspec feature functions

---
+++ spec/spec_helper_feature_shared.rb
@@ -0,0 +1,27 @@
+# A few global functions shared between all rspec feature tests. 
+#
+# * Keep this file lexically sorted.
+# 
+# * Keep this file small and simple.  
+#
+# * Only simple functions shall be included.
+#   
+# * Only general functions shall be included.
+#   
+# Favor clearness, and simplicity instead of dryness!
+#
+
+def click_on_text text
+  wait_until{ all("a, button", text: text, visible: true).size > 0}
+  find("a, button",text: text).click
+end
+
+def sign_in_as login, password= 'password'
+  visit "/"
+  find("a#database-user-login-tab").click
+  find("input[name='login']").set(login)
+  find("input[name='password']").set(password)
+  find("button[type='submit']").click
+  User.find_by_login login
+end
+
