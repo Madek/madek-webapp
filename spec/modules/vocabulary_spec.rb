ae84d8384e40ecf51e0083b6214ccfe384bfb122
Thomas Schank <DrTom@schank.ch>
2014-07-14 19:52:52 +0200

    Refactor modules/vocabulary_spec to use vocabulary factory

--- spec/modules/vocabulary_spec.rb
+++ spec/modules/vocabulary_spec.rb
@@ -2,34 +2,35 @@ require 'spec_helper'
 
 describe ::Vocabulary do
 
-  context "personas data, user normin, and the Landschaftsvisualisierung context, " do
+  include MetaContextTermTestFactory
 
-    # TODO we use the personas data for now since we do not have a minimal # meta data set; 
-    # this MUST BE FIXED; the personas data is to variable and messy to perform 
-    # checks in this manner
-
+  context "personas data, user normin, and the Character context from MetaContextTermTestFactory, " do
 
     before :all do
       DBHelper.truncate_tables
       DBHelper.load_data Rails.root.join('db','personas.data.psql')
-
       @user = User.find_by login: 'normin'
-      @context = Context.find_by id: 'Landschaftsvisualisierung'
+      clean_and_then_build_minimal_vocabulary
+      build_vocabulary_example_media_resources @user
     end
 
     describe "getting the included (individual) media_entries" do
 
       it "should not raise an error" do
-        expect{ ::Vocabulary.media_entries(@context, @user) }.not_to raise_error
+        expect{ ::Vocabulary.media_entries(@context_character, @user) }.not_to raise_error
       end
 
-      it "includes 'Diplom' (8b5050fe-280d-4712-9acb-fe8323e5c33e) e.g." do
-        ::Vocabulary.media_entries(@context,@user).map(&:id).should include '8b5050fe-280d-4712-9acb-fe8323e5c33e'
+      it "includes 'EmptyCharacterEntry' e.g." do
+        ::Vocabulary.media_entries(@context_character,@user).map(&:id).should include @empty_character_entry.id
+      end
+
+      it "includes 'Nike' e.g." do
+        ::Vocabulary.media_entries(@context_character,@user).map(&:id).should include @nike_entry.id
       end
 
       describe " the count" do
-        it "should be >= 5" do 
-          ::Vocabulary.media_entries_count(@context,@user).should be>= 5
+        it "should be== 3" do 
+          ::Vocabulary.media_entries_count(@context_character,@user).should be== 3
         end
       end
     end
@@ -37,28 +38,28 @@ describe ::Vocabulary do
     describe "building the vocabulary for context and user," do
 
       it "should not raise an error" do
-        expect{ ::Vocabulary.build_for_context_and_user(@context,@user) }.not_to raise_error
+        expect{ ::Vocabulary.build_for_context_and_user(@context_character,@user) }.not_to raise_error
       end
 
 
       describe "the vocabulary, " do
 
         before :each do 
-          @vocabulary= ::Vocabulary.build_for_context_and_user @context, @user
+          @vocabulary= ::Vocabulary.build_for_context_and_user @context_character, @user
         end
 
-        it "contains an item with the label 'Landschaftstyp'" do 
-          @vocabulary.select{|item| item[:label]== 'Landschaftstyp'}.should_not be_empty
+        it "contains an item with the label 'Goddess'" do 
+          @vocabulary.select{|item| item[:label]== 'Goddess'}.should_not be_empty
         end
 
-        describe "the 'Landschaftstyp' item" do
+        describe "the 'Godess' item" do
           before :each do
-            @landschaftstyp_item= @vocabulary.select{|item| item[:label]== 'Landschaftstyp'}.first
+            @goddess_item= @vocabulary.select{|item| item[:label]== 'Goddess'}.first
           end
-          it "contains a 'Agglomeration' term which is used at least once"  do
-            agglomeration_term= @landschaftstyp_item[:meta_terms].select{|mt| mt[:term] ==  'Agglomeration'}.first
-            agglomeration_term.should be
-            agglomeration_term[:usage_count].should be>= 1
+          it "contains a 'Athena' term which is used at least once"  do
+            meta_term_athena= @goddess_item[:meta_terms].select{|mt| mt[:term] ==  'Athena'}.first
+            meta_term_athena.should be
+            meta_term_athena[:usage_count].should be>= 1
           end
         end
 
@@ -68,28 +69,32 @@ describe ::Vocabulary do
 
     describe "building the vocabulary for context, set  and user," do
 
-      context "the first set of the Landschaftsvisualisierung" do
+      context "the set_for_contex_character " do
 
         before :each do
-          @set = @context.media_sets.first
+          @set = @context_character.media_sets.first
         end
 
         it "exists" do 
           expect{@set}.to be 
         end
 
+        it "is the character set" do
+          expect(@set).to be== @set_for_contex_character
+        end
+
         it "has children" do
-           @set.child_media_resources.count.should be>= 1
+           @set_for_contex_character.child_media_resources.count.should be== 3
         end
 
         describe "::Vocabulary.meta_terms_for_set" do
 
           before :each do
-            @terms = ::Vocabulary.meta_terms_for_set(@set) 
+            @terms = ::Vocabulary.meta_terms_for_set(@set_for_contex_character) 
           end
 
-          it "has terms" do
-            @terms.count.should be>= 1
+          it "has 2 terms" do
+            @terms.count.should be== 2
           end
         end
 
@@ -98,33 +103,41 @@ describe ::Vocabulary do
           it "succeeds" do
             expect{  
               ::Vocabulary.build_for_context_set_and_user(
-                @context,@set,@user)
+                @context_character,@set_for_contex_character,@user)
             }.not_to raise_error
           end
 
           describe "the vocabulary, " do
 
             before :each do 
-              @vocabulary= ::Vocabulary.build_for_context_set_and_user @context,@set, @user
+              @vocabulary= ::Vocabulary.build_for_context_set_and_user @context_character,@set_for_contex_character, @user
             end
 
-            it "contains an item with the label 'Landschaftstyp'" do 
-              @vocabulary.select{|item| item[:label]== 'Landschaftstyp'}.should_not be_empty
+            it "contains an item with the label 'Goddess'" do 
+              @vocabulary.select{|item| item[:label]== 'Goddess'}.should_not be_empty
             end
 
-            describe "the 'Stil- und Kunstrichtungen' item" do
+            describe "the 'Goddess' item" do
+
               before :each do
-                @suk_item= @vocabulary.select{|item| item[:label]== 'Stil- und Kunstrichtungen'}.first
+                @item= @vocabulary.select{|item| item[:label]== 'Goddess'}.first
               end
-              it "contains a 'Konzeptkunst' term which is used at least once"  do
-                konzeptkunst_term= @suk_item[:meta_terms].select{|mt| mt[:term] ==  'Konzeptkunst'}.first
-                konzeptkunst_term.should be
-                konzeptkunst_term[:usage_count].should be>= 1
+
+              it "contains a 'Nike' term which is used twice"  do
+                meta_term_nike= @item[:meta_terms].select{|mt| mt[:term] ==  'Nike'}.first
+                meta_term_nike.should be
+                meta_term_nike[:usage_count].should be== 2
               end
+
+              it "contains a 'Athena' term which is used once"  do
+                meta_term_athena= @item[:meta_terms].select{|mt| mt[:term] ==  'Athena'}.first
+                meta_term_athena.should be
+                meta_term_athena[:usage_count].should be== 1
+              end
+
             end
 
           end
-
 
         end
 
