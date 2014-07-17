d44fd6540559bda43dd733af7bce92a3fb2a6d6e
Max F. Albrecht <1@178.is>
2014-07-17 17:46:26 +0200

    test: fix randomly failing factory :application

--- factories/application.rb
+++ factories/application.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :application, class: API::Application do
-    id {Faker::Lorem.words(2).join('_')}
+    id {Faker::Lorem.words(2).join('_').slice(0,20)}
     description {Faker::Lorem.words(10).join(' ')}
   end
 end
