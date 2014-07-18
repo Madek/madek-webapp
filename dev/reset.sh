b04e2f051bcf5c1c29cc9b35678063600bca2354
Max F. Albrecht <1@178.is>
2014-07-18 20:21:02 +0200

    dev/reset: add db connection hint, formatting

--- dev/reset.sh
+++ dev/reset.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 echo "This does a complete reset/setup of the local dev and test environments"
+echo "If it fails, you most likely have open connections to the db."
 echo "(Work in Progress)"
+sleep 5
 
 set -ex
 
 for ENV in test development; do
-  RAILS_ENV=$ENV bundle exec rake db:drop madek:setup:dirs db:reset madek:db:truncate madek:db:load_data FILE=db/personas.data.psql
+  RAILS_ENV=$ENV bundle exec rake madek:setup:dirs db:drop db:reset db:migrate madek:db:truncate madek:db:load_data FILE=db/personas.data.psql
 done
 
 
-echo -e "\n\nNow run a clean test!"
+echo "Now run a clean test!"
