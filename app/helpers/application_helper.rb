4c737d7f316ac1a6917b9c12a1fa3dc5509ec506
Thomas Schank <DrTom@schank.ch>
2014-09-05 12:40:22 +0200

    Set version to 2.0.13; move sem version to config/settings and diplay it in footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def markdown(source)
     Kramdown::Document.new(source).to_html.html_safe
   end
+
+  def semver 
+    "#{Settings.release.version_major}.#{Settings.release.version_minor}.#{Settings.release.version_patch}" \
+     + Settings.release.version_pre.to_s + Settings.release.version_build.to_s
+  end
 end
