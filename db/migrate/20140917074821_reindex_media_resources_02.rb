c358d90528c884179b5098951bf1c9b9b0d4f5f7
Thomas Schank <DrTom@schank.ch>
2014-09-26 08:50:57 +0200

    Fix migration for production data
    *rerun reindex migration, collides with code changes

--- db/migrate/20140917074821_reindex_media_resources_02.rb
+++ db/migrate/20140917074821_reindex_media_resources_02.rb
@@ -1,13 +1,4 @@
 class ReindexMediaResources02 < ActiveRecord::Migration
   def change
-    reversible do |dir|
-      dir.up do 
-        MediaResource.find_in_batches do |mrs|
-          mrs.each do |mr|
-            mr.reindex
-          end
-        end
-      end
-    end
   end
 end
