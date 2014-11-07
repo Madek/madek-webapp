a7fea54e66771abc08c8d325048d32933ce89181
Michał Podlecki <m.podlecki@op.pl>
2014-11-07 09:13:20 +0100

    cider-ci: fix typos in Stderr sections

--- cider-ci/bin/check_rspec-features_tested.rb
+++ cider-ci/bin/check_rspec-features_tested.rb
@@ -18,7 +18,7 @@ if existing_spec_files == tested_spec_files
   exit 0
 else
   warn "existing_spec_files and the tested_spec_files are not equivalent: "
-  warn "exiting but not tested: #{(existing_spec_files - tested_spec_files).map(&:to_s)}"
+  warn "existing but not tested: #{(existing_spec_files - tested_spec_files).map(&:to_s)}"
   warn "tested but not existing: #{(tested_spec_files - existing_spec_files).map(&:to_s)}"
   exit -1
 end
