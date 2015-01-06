e79f87e0137c4f60068af822ca05a78a507da379
Thomas Schank <DrTom@schank.ch>
2015-01-06 15:52:35 +0100

    Deduplicate code for CollectionPermissions

--- app/models/permissions/collection_group_permission.rb
+++ app/models/permissions/collection_group_permission.rb
@@ -1,17 +1,6 @@
 module Permissions
-
   class CollectionGroupPermission < ActiveRecord::Base
-
-    belongs_to :collection
+    include ::Permissions::Modules::Collection
     belongs_to :group
-    belongs_to :updator, class_name: 'User'
-
-    def self.destroy_ineffective
-      CollectionGroupPermission.where(get_metadata_and_previews: false,
-                                      edit_metadata_and_relations: false)
-        .delete_all
-    end
-
   end
-
 end
--- app/models/permissions/collection_user_permission.rb
+++ app/models/permissions/collection_user_permission.rb
@@ -1,15 +1,13 @@
 module Permissions
-
   class CollectionUserPermission < ActiveRecord::Base
+    include ::Permissions::Modules::Collection
 
-    belongs_to :collection
     belongs_to :user
-    belongs_to :updator, class_name: 'User'
 
     def self.destroy_ineffective
-      CollectionUserPermission.where(get_metadata_and_previews: false,
-                                     edit_metadata_and_relations: false).delete_all
-      CollectionUserPermission.joins(:collection).where(
+      where(get_metadata_and_previews: false,
+            edit_metadata_and_relations: false).delete_all
+      joins(:collection).where(
         'collections.responsible_user_id = collection_user_permissions.user_id') \
         .delete_all
     end
