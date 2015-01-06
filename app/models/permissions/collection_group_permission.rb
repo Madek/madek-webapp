2f887a91bdd3b288646858c9a42113f47f183b9c
Matus Kmit <matus.kmit@zhdk.ch>
2015-01-06 16:18:19 +0100

    Revert "Deduplicate code for CollectionPermissions"
    This reverts commit e79f87e0137c4f60068af822ca05a78a507da379.

--- app/models/permissions/collection_group_permission.rb
+++ app/models/permissions/collection_group_permission.rb
@@ -1,6 +1,17 @@
 module Permissions
+
   class CollectionGroupPermission < ActiveRecord::Base
-    include ::Permissions::Modules::Collection
+
+    belongs_to :collection
     belongs_to :group
+    belongs_to :updator, class_name: 'User'
+
+    def self.destroy_ineffective
+      CollectionGroupPermission.where(get_metadata_and_previews: false,
+                                      edit_metadata_and_relations: false)
+        .delete_all
+    end
+
   end
+
 end
--- app/models/permissions/collection_user_permission.rb
+++ app/models/permissions/collection_user_permission.rb
@@ -1,13 +1,15 @@
 module Permissions
-  class CollectionUserPermission < ActiveRecord::Base
-    include ::Permissions::Modules::Collection
 
+  class CollectionUserPermission < ActiveRecord::Base
+
+    belongs_to :collection
     belongs_to :user
+    belongs_to :updator, class_name: 'User'
 
     def self.destroy_ineffective
-      where(get_metadata_and_previews: false,
-            edit_metadata_and_relations: false).delete_all
-      joins(:collection).where(
+      CollectionUserPermission.where(get_metadata_and_previews: false,
+                                     edit_metadata_and_relations: false).delete_all
+      CollectionUserPermission.joins(:collection).where(
         'collections.responsible_user_id = collection_user_permissions.user_id') \
         .delete_all
     end
