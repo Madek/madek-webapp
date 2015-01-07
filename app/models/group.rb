8d48538131a8d32ffb31d3ddec93f1bf02eb540d
Thomas Schank <DrTom@schank.ch>
2015-01-07 11:09:51 +0100

    Cleanup Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,57 +1,6 @@
-# -*- encoding : utf-8 -*-
 class Group < ActiveRecord::Base
 
   has_many :grouppermissions
   has_and_belongs_to_many :users
-
-  after_save :update_searchable
-
-  validates_presence_of :name
-  validates :name, uniqueness: { scope: :institutional_group_name }
-
-  scope :departments, -> { where(type: 'InstitutionalGroup') }
-
-  def to_s
-    name
-  end
-
-  def readonly?
-    # FIXME: remove zhdk
-    ['Admin', 'ZHdK (Zürcher Hochschule der Künste)'].include?(name)
-  end
-
-  ### use in case counters get broken ####################
-
-  def self.reset_users_count
-    Group.all.each do |group|
-      group.update_attributes(users_count: group.users.count)
-    end
-  end
-
-  ### text search ########################################
-
-  def update_searchable
-    update_columns searchable: [name, institutional_group_name].flatten \
-      .compact.sort.uniq.join(' ')
-  end
-
-  scope :text_search,
-        lambda { |search_term|
-          where('searchable ILIKE :term', term: "%#{search_term}%")
-        }
-
-  scope :text_rank_search, lambda{|search_term|
-    rank = text_search_rank :searchable, search_term
-    select("#{'groups.*,' if select_values.empty?}  #{rank} AS search_rank") \
-      .where("#{rank} > 0.05") \
-      .reorder('search_rank DESC')
-  }
-
-  scope :trgm_rank_search, lambda{|search_term|
-    rank = trgm_search_rank :searchable, search_term
-    select("#{'groups.*,' if select_values.empty?} #{rank} AS search_rank") \
-      .where("#{rank} > 0.05") \
-      .reorder('search_rank DESC')
-  }
 
 end
