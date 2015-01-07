cfed3f0944ac817f0f0903f556f82f6d3e869f85
Matus Kmit <matus.kmit@zhdk.ch>
2015-01-07 17:27:55 +0100

    rubucop Metrics/MethodLength 20 [#85605892]

--- doc/utilities/permissions/change_owner.rb
+++ doc/utilities/permissions/change_owner.rb
@@ -1,26 +1,33 @@
 def change_owner(resource_type, id, from_user, to_user)
   case resource_type
   when :media_set
-    ms = from_user.media_sets.find id
-    ms.update_attributes(user: to_user)
-    p = \
-      ms
-        .permissions
-        .where(subject_id: from_user, subject_type: from_user.class)
-        .first
-    p.update_attributes(subject: to_user)
+    change_owner_for_media_set!(id, from_user, to_user)
   when :upload_session
-    us = from_user.upload_sessions.find id
-    us.update_attributes(user: to_user)
-    us.media_entries.each do |me|
-      p = \
-        me
-          .permissions
-          .where(subject_id: from_user, subject_type: from_user.class)
-          .first
-      p.update_attributes(subject: to_user)
-    end
+    change_owner_for_upload_session!(id, from_user, to_user)
   end
+end
+
+def change_owner_for_media_set!(id, from_user, to_user)
+  ms = from_user.media_sets.find id
+  ms.update_attributes(user: to_user)
+  update_permission_subject_for_resource!(ms, from_user, to_user)
+end
+
+def change_owner_for_upload_session!(id, from_user, to_user)
+  us = from_user.upload_sessions.find id
+  us.update_attributes(user: to_user)
+  us.media_entries.each do |me|
+    update_permission_subject_for_resource!(me, from_user, to_user)
+  end
+end
+
+def update_permission_subject_for_resource!(resource, from_user, to_user)
+  p = \
+    resource
+      .permissions
+      .where(subject_id: from_user, subject_type: from_user.class)
+      .first
+  p.update_attributes(subject: to_user)
 end
 
 from_user = User.find 10262
