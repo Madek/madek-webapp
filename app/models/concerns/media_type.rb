855085b6b0727cab99eae0ac5e4ad4352f5682af
Max F. Albrecht <1@178.is>
2015-01-30 18:58:37 +0100

    spec: collections
    also increase coverage by commenting out unused stuff

--- app/models/concerns/media_type.rb
+++ app/models/concerns/media_type.rb
@@ -3,23 +3,21 @@ module Concerns
     extend ActiveSupport::Concern
 
     def self.map_to_media_type(content_type)
-      unless content_type
-        'other'
+      content_type ||= nil
+      case content_type
+      when /^image/
+        'image'
+      # TODO: other media types
+      # when /^video/
+      #   'video'
+      # when /^audio/
+      #   'audio'
+      # when /^text/
+      #   'document'
+      # when /^application/
+      #   'document'
       else
-        case content_type
-        when /^image/
-          'image'
-        when /^video/
-          'video'
-        when /^audio/
-          'audio'
-        when /^text/
-          'document'
-        when /^application/
-          'document'
-        else
-          'other'
-        end
+        'other'
       end
     end
   end
---
+++ spec/features/collections_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'spec_helper_feature'
+require 'spec_helper_feature_shared'
+
+feature 'Collections#show' do
+  background do
+    @user = User.find_by(login: 'normin')
+    sign_in_as @user.login
+  end
+
+  scenario 'is rendered with title and responsible user' do
+    # TODO: dynamically get it?
+    id = 'd316b369-6d20-4eb8-b76a-c83f1a4c2682'
+    @collection = Collection.find(id)
+    expect(@collection.id).to be
+
+    # TODO: visit collections_path(@collection)
+    visit "/collections/#{@collection.id}"
+    expect(page).to have_content 'Normin Normalo'
+
+    pending('re-implement title')
+    expect(page).to have_content 'Konzepte'
+  end
+
+end
