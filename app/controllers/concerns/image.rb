cfc3fc01f429e2361d883664c7eacb17c1ebd76d
Max F. Albrecht <1@178.is>
2015-02-09 15:01:03 +0100

    app: controllers: images: treat missing as 404
    hackish, but cleans up the log and does the correct thing

--- app/controllers/concerns/image.rb
+++ app/controllers/concerns/image.rb
@@ -3,10 +3,16 @@ module Concerns
     extend ActiveSupport::Concern
 
     def get_preview_and_send_image(media_entry, size)
-      preview = media_entry.media_file.preview(size)
-      send_file preview.file_path,
-                type: preview.content_type,
-                disposition: 'inline'
+      # TODO: review/cleanup
+      begin
+        preview = media_entry.media_file.preview(size)
+        send_file preview.file_path,
+                  type: preview.content_type,
+                  disposition: 'inline'
+      rescue
+        Rails.logger.warn 'image not found!'
+        render nothing: true, status: 404
+      end
     end
 
   end
