f8e7629c9d3faf7abd463ddeaef2e21177f0076a
Thomas Schank <DrTom@schank.ch>
2015-03-05 08:19:13 +0100

    Try to log migration error

--- db/migrate/151_migrate_meta_data_to_vocabulary.rb
+++ db/migrate/151_migrate_meta_data_to_vocabulary.rb
@@ -25,29 +25,29 @@ class MigrateMetaDataToVocabulary < ActiveRecord::Migration
     self.inheritance_column = false
 
     has_and_belongs_to_many :users,
-                            join_table: :meta_data_users,
-                            foreign_key: :meta_datum_id,
-                            association_foreign_key: :user_id
+      join_table: :meta_data_users,
+      foreign_key: :meta_datum_id,
+      association_foreign_key: :user_id
 
     has_and_belongs_to_many :vocables,
-                            join_table: :meta_data_vocables,
-                            foreign_key: :meta_datum_id,
-                            association_foreign_key: :vocable_id
+      join_table: :meta_data_vocables,
+      foreign_key: :meta_datum_id,
+      association_foreign_key: :vocable_id
 
     has_and_belongs_to_many :meta_terms,
-                            join_table: :meta_data_meta_terms,
-                            foreign_key: :meta_datum_id,
-                            association_foreign_key: :meta_term_id
+      join_table: :meta_data_meta_terms,
+      foreign_key: :meta_datum_id,
+      association_foreign_key: :meta_term_id
 
     has_and_belongs_to_many :people,
-                            join_table: :meta_data_people,
-                            foreign_key: :meta_datum_id,
-                            association_foreign_key: :person_id
+      join_table: :meta_data_people,
+      foreign_key: :meta_datum_id,
+      association_foreign_key: :person_id
 
     has_and_belongs_to_many :groups,
-                            join_table: :meta_data_groups,
-                            foreign_key: :meta_datum_id,
-                            association_foreign_key: :group_id
+      join_table: :meta_data_groups,
+      foreign_key: :meta_datum_id,
+      association_foreign_key: :group_id
 
     has_many :keywords
 
@@ -66,65 +66,70 @@ class MigrateMetaDataToVocabulary < ActiveRecord::Migration
   end
 
   def change
-    Vocable.reset_column_information
-    Vocabulary.reset_column_information
-    MetaKey.reset_column_information
-    MetaDatum.reset_column_information
-    MetaTerm.reset_column_information
-    MetaKeyDefinition.reset_column_information
+    begin 
+      Vocable.reset_column_information
+      Vocabulary.reset_column_information
+      MetaKey.reset_column_information
+      MetaDatum.reset_column_information
+      MetaTerm.reset_column_information
+      MetaKeyDefinition.reset_column_information
 
-    MetaKey.find_each do |meta_key|
-      if meta_key.meta_key_definitions.count == 0
+      MetaKey.find_each do |meta_key|
+        if meta_key.meta_key_definitions.count == 0
 
-        orphan_vocabulary = Vocabulary.find_or_create_by id: 'orphans', label: 'Orphans',
-                                                         description: 'The related meta_keys in this vocabulary were not related to any context before the migration.'
-
-        new_id_meta_key_part = meta_key.id.downcase.gsub(/\s+/, '_').gsub(/-/, '_').gsub(/_+/, '_')
-        new_meta_key_id = "#{orphan_vocabulary.id}:#{new_id_meta_key_part}"
-
-        new_meta_key = MetaKey.find_or_create_by(id: new_meta_key_id,
-                                                 meta_datum_object_type:  meta_key.meta_datum_object_type,
-                                                 vocables_are_user_extensible: meta_key.is_extensible_list,
-                                                 vocabulary: orphan_vocabulary)
-        Rails.logger.info "CREATED NEW META_KEY: #{new_meta_key.attributes}"
-
-        clone_meta_key_data(meta_key, new_meta_key)
-
-      else
-
-        meta_key.meta_key_definitions.each do |meta_key_definition|
-          Rails.logger.info "MIGRATING meta_key_definition: #{meta_key_definition.attributes}"
-
-          context = meta_key_definition.context
-          vocabulary_id = context.id.downcase.gsub(/\s+/, '_').gsub(/-/, '_').gsub(/_+/, '_').gsub(/[^a-z0-9\_\-]/, '')
-          vocabulary = Vocabulary.find_or_create_by(id: vocabulary_id)
-          vocabulary.update_attributes label: context.label, description: context.description
+          orphan_vocabulary = Vocabulary.find_or_create_by id: 'orphans', label: 'Orphans',
+            description: 'The related meta_keys in this vocabulary were not related to any context before the migration.'
 
           new_id_meta_key_part = meta_key.id.downcase.gsub(/\s+/, '_').gsub(/-/, '_').gsub(/_+/, '_')
-          new_meta_key_id = "#{vocabulary_id}:#{new_id_meta_key_part}"
+          new_meta_key_id = "#{orphan_vocabulary.id}:#{new_id_meta_key_part}"
 
-          new_meta_key_attributes = { id: new_meta_key_id,
-                                      meta_datum_object_type:  meta_key.meta_datum_object_type,
-                                      label: meta_key_definition.label,
-                                      description: meta_key_definition.description,
-                                      hint: meta_key_definition.hint,
-                                      is_required: meta_key_definition.is_required,
-                                      length_max: meta_key_definition.length_max,
-                                      length_min: meta_key_definition.length_min,
-                                      position: meta_key_definition.position,
-                                      input_type: meta_key_definition.input_type,
-                                      vocables_are_user_extensible: meta_key.is_extensible_list,
-                                      vocabulary: vocabulary }
+          new_meta_key = MetaKey.find_or_create_by(id: new_meta_key_id,
+                                                   meta_datum_object_type:  meta_key.meta_datum_object_type,
+                                                   vocables_are_user_extensible: meta_key.is_extensible_list,
+                                                   vocabulary: orphan_vocabulary)
+          Rails.logger.info "CREATED NEW META_KEY: #{new_meta_key.attributes}"
 
-          Rails.logger.info "CREATING NEW META_KEY: #{new_meta_key_attributes}"
+          clone_meta_key_data(meta_key, new_meta_key)
 
-          new_meta_key = MetaKey.find_or_create_by(new_meta_key_attributes)
+        else
 
-          clone_meta_key_data meta_key, new_meta_key
+          meta_key.meta_key_definitions.each do |meta_key_definition|
+            Rails.logger.info "MIGRATING meta_key_definition: #{meta_key_definition.attributes}"
 
+            context = meta_key_definition.context
+            vocabulary_id = context.id.downcase.gsub(/\s+/, '_').gsub(/-/, '_').gsub(/_+/, '_').gsub(/[^a-z0-9\_\-]/, '')
+            vocabulary = Vocabulary.find_or_create_by(id: vocabulary_id)
+            vocabulary.update_attributes label: context.label, description: context.description
+
+            new_id_meta_key_part = meta_key.id.downcase.gsub(/\s+/, '_').gsub(/-/, '_').gsub(/_+/, '_')
+            new_meta_key_id = "#{vocabulary_id}:#{new_id_meta_key_part}"
+
+            new_meta_key_attributes = { id: new_meta_key_id,
+                                        meta_datum_object_type:  meta_key.meta_datum_object_type,
+                                        label: meta_key_definition.label,
+                                        description: meta_key_definition.description,
+                                        hint: meta_key_definition.hint,
+                                        is_required: meta_key_definition.is_required,
+                                        length_max: meta_key_definition.length_max,
+                                        length_min: meta_key_definition.length_min,
+                                        position: meta_key_definition.position,
+                                        input_type: meta_key_definition.input_type,
+                                        vocables_are_user_extensible: meta_key.is_extensible_list,
+                                        vocabulary: vocabulary }
+
+            Rails.logger.info "CREATING NEW META_KEY: #{new_meta_key_attributes}"
+
+            new_meta_key = MetaKey.find_or_create_by(new_meta_key_attributes)
+
+            clone_meta_key_data meta_key, new_meta_key
+
+          end
         end
+        meta_key.delete
       end
-      meta_key.delete
+    rescue Exception => e
+      Rails.logger.info "#{e.class} #{e.message} #{e.backtrace.join(', ')}"
+      raise e
     end
   end
 
