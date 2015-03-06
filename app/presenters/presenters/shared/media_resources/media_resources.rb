24b39f499b68ccdbab417a9c4986317b0b07719c
Matus Kmit <matus.kmit@zhdk.ch>
2015-03-06 12:14:25 +0100

    ruby 2.0 keyword args work with flog now

--- app/presenters/presenters/shared/media_resources/media_resources.rb
+++ app/presenters/presenters/shared/media_resources/media_resources.rb
@@ -4,13 +4,10 @@ module Presenters
       class MediaResources < Presenter
         attr_reader :media_entries, :collections, :filter_sets
 
-        # TODO: rewrite to use ruby 2.0 keyword syntax
-        # can't be done now because of parser error of flog
-        # or in worse case disable flog for this case
-        def initialize(media_resources = {})
-          @media_entries = media_resources[:media_entries]
-          @collections = media_resources[:collections]
-          @filter_sets = media_resources[:filter_sets]
+        def initialize(media_entries: [], collections: [], filter_sets: [])
+          @media_entries = media_entries
+          @collections = collections
+          @filter_sets = filter_sets
         end
       end
     end
