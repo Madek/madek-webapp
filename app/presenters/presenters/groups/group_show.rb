ae31b49ffd3899762b32b28896549e30a767c490
Max F. Albrecht <1@178.is>
2015-03-11 18:59:16 +0100

    presenter: GroupShow: entrusted_media_resources
    instead of members!

--- app/presenters/presenters/groups/group_show.rb
+++ app/presenters/presenters/groups/group_show.rb
@@ -4,13 +4,24 @@ module Presenters
       [:name, :institutional?, :institutional_group_name] \
         .each { |m| delegate m, to: :@app_resource }
 
-      def members
-        @app_resource
-          .users
-          .includes(:person)
-          .map(&:person)
-          .map { |p| Presenters::People::PersonIndex.new(p) }
+      def initialize(app_resource, user)
+        @user = user
+        super(app_resource)
       end
+
+      def entrusted_media_resources
+        Presenters::Shared::MediaResources::MediaResources.new \
+          media_entries:
+            MediaEntry.entrusted_to_group(@app_resource)
+              .map { |r| MediaEntries::MediaEntryIndex.new(r, @user) },
+          collections:
+            Collection.entrusted_to_group(@app_resource)
+              .map { |r| Collections::CollectionIndex.new(r, @user) },
+          filter_sets:
+            FilterSet.entrusted_to_group(@app_resource)
+              .map { |r| FilterSets::FilterSetIndex.new(r, @user) }
+      end
+
     end
   end
 end
