b5104838d1d68e7cbddb8aa24b7ca17681646732
Thomas Schank <DrTom@schank.ch>
2015-03-16 21:09:38 +0100

    Rename attribute is_bunch to bunch?

--- app/presenters/presenters/people/person_show.rb
+++ app/presenters/presenters/people/person_show.rb
@@ -18,7 +18,7 @@ module Presenters
          date_of_death).each { |m| delegate m.to_sym, to: :@app_resource }
 
       def bunch?
-        @app_resource.is_bunch
+        @app_resource.bunch?
       end
 
       alias_method :related_media_resources_via_meta_data,
