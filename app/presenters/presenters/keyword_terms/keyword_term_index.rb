c4b91bc7f8f9074f607ead864feb55cbbd6732c9
Max F. Albrecht <1@178.is>
2015-04-09 20:07:25 +0200

    presenter: meta_data: rework API
    - JSON-structure
    - keyword_term#url dummy

--- app/presenters/presenters/keyword_terms/keyword_term_index.rb
+++ app/presenters/presenters/keyword_terms/keyword_term_index.rb
@@ -2,6 +2,14 @@ module Presenters
   module KeywordTerms
     class KeywordTermIndex < Presenters::Shared::AppResource
       delegate_to_app_resource :term
+
+      def url
+        # TODO: vocabulary_meta_key_term_path
+        term = @app_resource
+        meta_key = @app_resource.meta_key
+        vocabulary = meta_key.vocabulary
+        "/[not_implemented]/vocabularies/#{vocabulary.id}/#{meta_key.id}/#{term.id}"
+      end
     end
   end
 end
--- app/presenters/presenters/meta_data/meta_data_presenter.rb
+++ app/presenters/presenters/meta_data/meta_data_presenter.rb
@@ -1,28 +1,88 @@
 module Presenters
   module MetaData
+
+    # TODO: maybe cleanup once the API is 'stable'
+
     class MetaDataPresenter < Presenters::Shared::AppResource
       def initialize(app_resource, user)
         @user = user
         super(app_resource)
       end
 
-      def meta_data_by_vocabulary
-        vocabularies = Vocabulary.viewable_by_user(@user)
-        @app_resource
+      def by_vocabulary
+        # - every meta_datum for the @app_resource, that is viewable by @user
+        # - as JSON-serializable "tuples" (key/value)
+        # - grouped by vocabulary_id
+        # example_output = {
+        #   "madek_core": {
+        #     "_vocabulary": {
+        #       "id": "madek_core",
+        #       "label": "This is the core madek vocabulary",
+        #       "url": "http://example.com/vocabulary/core"
+        #     },
+        #     "_meta_data": [
+        #       {
+        #         "_key": {
+        #           "id": "madek_core:title",
+        #           "label": "bla"
+        #         },
+        #         "_values": ["My Work"]
+        #       },
+        #       … more datums …
+        #     ]
+        #   },
+        #   … more vocabularies …
+        # }
+
+        data = @app_resource
           .meta_data
           .joins(:vocabulary)
-          .where(vocabularies: { id: vocabularies })
-          .map do |meta_datum|
-            Hash[meta_datum.meta_key_id, Hash['@type', meta_datum.type,
-                                              '@value', value(meta_datum)]]
-          end
-            .group_by { |h| vocabulary_label(h.keys.first) }
+          .where(vocabularies: { id: Vocabulary.viewable_by_user(@user) })
+          .group_by(&:meta_key) # merge repeating keys
+        data = build_meta_datum_tuples(data)
+        data = data.group_by { |tuple| MetaKey.find(tuple._key.id).vocabulary }
+        data = build_vocabulary_tuples(data)
+        OpenStruct.new(data)
+      end
+
+      def vocabularies_with_meta_data
+        by_vocabulary.to_h.keys # => [:madek_core, :zhdk, …]
       end
 
       private
 
-      def value(meta_datum)
-        wrap_in_array get_value_according_to_type meta_datum
+      def build_meta_datum_tuples(data)
+        data.map do |meta_datum|
+          meta_key = meta_datum[0]
+          meta_values = meta_datum[1]
+          OpenStruct.new(Hash[
+            '_key', OpenStruct.new(
+              id: meta_key.id,
+              label: meta_key.label,
+              type: meta_key.meta_datum_object_type,
+              description: meta_key.description,
+              position: meta_key.position
+            ),
+            '_values', ensure_wrapped_in_array(meta_values)
+              .map { |value| get_value_according_to_type(value) }
+              .flatten
+          ])
+        end
+      end
+
+      def build_vocabulary_tuples(vocabularies)
+        vocabularies.each_with_object({}) do |vocab, results|
+          vocabulary = vocab[0]
+          meta_data = vocab[1]
+          results[vocabulary.id] = OpenStruct.new(Hash[
+            '_vocabulary', OpenStruct.new(
+              id: vocabulary.id,
+              label: vocabulary.label,
+              description: vocabulary.description
+            ),
+            '_meta_data', meta_data
+          ])
+        end
       end
 
       def get_value_according_to_type(meta_datum)
@@ -38,23 +98,19 @@ module Presenters
             { |g| Presenters::Groups::GroupIndex.new(g) }
         when 'MetaDatum::License'
           Presenters::Licenses::LicenseIndex.new \
-            meta_datum.license
+            License.find(meta_datum.license_id) # WTF? TODO: associations?
         when 'MetaDatum::Keywords'
           meta_datum.keywords.map \
             { |k| Presenters::KeywordTerms::KeywordTermIndex.new(k.keyword_term) }
-        else
-          # NOTE: for all other literal values
+        else # all other values are "primitive/literal/unspecified":
           meta_datum.value
         end
       end
 
-      def wrap_in_array(value)
-        value.try(:to_a).is_a?(Array) ? value : [value]
+      def ensure_wrapped_in_array(value)
+        (value.try(:to_a).is_a?(Array) ? value.to_a : [value]).flatten
       end
 
-      def vocabulary_label(meta_key_id)
-        MetaKey.find(meta_key_id).vocabulary.label
-      end
     end
   end
 end
