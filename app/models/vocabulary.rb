0de45435d779bd928c241a3dbe87b8859eaadff2
Michał Podlecki <m.podlecki@op.pl>
2015-05-22 10:25:00 +0200

    admin: fix: select correct vocabulary when meta key is edited

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -6,23 +6,18 @@ class Vocabulary < ActiveRecord::Base
   include Concerns::PermissionsAssociations
   include Concerns::Vocabularies::Visibility
   include Concerns::Vocabularies::Usability
+  include Concerns::Vocabularies::Filters
 
   has_many :meta_keys, -> { order(:id) }
   has_many :keyword_terms,
            through: :meta_keys
 
-  scope :filter_by, lambda { |term|
-    where(
-      'vocabularies.id ILIKE :t OR vocabularies.label ILIKE :t',
-      t: "%#{term}%"
-    )
-  }
+  scope :sorted, -> { order(:id) }
   scope :with_meta_keys_count, lambda {
     joins('LEFT OUTER JOIN meta_keys ON meta_keys.vocabulary_id = vocabularies.id')
       .select('vocabularies.*, count(meta_keys.id) AS meta_keys_count')
       .group('vocabularies.id')
   }
-  scope :ids_for_filter, -> { order(:id).pluck(:id) }
 
   validates :id, presence: true
 
--- spec/features/admin/meta_keys_spec.rb
+++ spec/features/admin/meta_keys_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper_feature'
 require 'spec_helper_feature_shared'
 
 feature 'Admin Meta Keys' do
+  let(:meta_key_with_keywords) { MetaKey.with_type('MetaDatum::Keywords').first }
   background do
     @admin_user = create :admin_user, password: 'password'
     sign_in_as @admin_user.login
@@ -21,5 +22,27 @@ feature 'Admin Meta Keys' do
     click_button 'Apply'
 
     expect(page).to have_select('sort_by', selected: 'Name part')
+  end
+
+  context 'Editing' do
+    scenario 'Proper values for selects' do
+      visit edit_admin_meta_key_path(meta_key_with_keywords)
+
+      expect(page).to have_select(
+        'Vocabulary',
+        selected: meta_key_with_keywords.vocabulary_id)
+      expect(page).to have_select(
+        'Meta datum object type',
+        selected: meta_key_with_keywords.meta_datum_object_type)
+      expect(page).to have_select(
+        'Keywords alphabetical order',
+        selected: selected_value_from_boolean(
+          meta_key_with_keywords.keywords_alphabetical_order)
+      )
+    end
+  end
+
+  def selected_value_from_boolean(value)
+    value == true ? 'Yes' : 'No'
   end
 end
