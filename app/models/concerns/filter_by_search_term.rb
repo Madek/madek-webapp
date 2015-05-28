c2bb03904cb9d6fef1fd729cb95d20682b8fb82a
Max F. Albrecht <1@178.is>
2015-05-28 19:20:08 +0200

    concerns: search: tokenize query

--- app/models/concerns/filter_by_search_term.rb
+++ app/models/concerns/filter_by_search_term.rb
@@ -3,11 +3,22 @@ module Concerns
     extend ActiveSupport::Concern
 
     included do
-      def self.filter_by_term_using_attributes(term, *attrs)
+      def self.filter_by_term_using_attributes(query, *attrs)
+        tokens = tokenize(query)
         sql_string = \
-          attrs.map { |attr| "#{attr} ILIKE '%#{term}%'" }.join(' OR ')
+          attrs.map { |attr| "#{attr} ILIKE ALL (array[#{tokens}])" }.join(' OR ')
         where(sql_string)
+      end
+
+      private
+
+      def self.tokenize(string)
+        return string unless string.is_a?(String)
+        string.split(/[[:space:]]+|[[:punct:]]+/)
+          .map { |token| "'%#{token}%'" }
+          .join(', ')
       end
     end
   end
+
 end
