8dd784111b71193ff4f81e057f6fcc03ddd7126e
Max F. Albrecht <1@178.is>
2015-06-05 18:22:18 +0200

    controller: app: cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   # this Pundit error is generic and means basically 'access denied'
-  rescue_from Pundit::NotAuthorizedError,
-              with: :reraise_according_to_login_state
+  rescue_from Pundit::NotAuthorizedError, with: :reraise_according_to_login_state
 
   # Give views access to these methods:
   helper_method :current_user
@@ -24,17 +23,19 @@ class ApplicationController < ActionController::Base
     redirect_to(my_dashboard_path) if authenticated?
   end
 
-  # private # <- would be nice but breaks test
-
   def current_user
     validate_services_session_cookie_and_get_user
   end
+
+  private
 
   def authenticated?
     not current_user.nil?
   end
 
   def reraise_according_to_login_state
-    raise (current_user ? Errors::ForbiddenError : Errors::UnauthorizedError)
+    raise (if authenticated? then Errors::ForbiddenError
+           else Errors::UnauthorizedError
+           end)
   end
 end
