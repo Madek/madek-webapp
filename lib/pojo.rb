6dd9f2bc455397b77fbee8dd3639aeed67b9acbd
Max F. Albrecht <1@178.is>
2015-06-26 18:06:11 +0200

    lib: pojo: #initialize, #to_a

--- lib/pojo.rb
+++ lib/pojo.rb
@@ -4,6 +4,14 @@
 
 class Pojo < OpenStruct
 
+  def initialize(hash_or_hashable)
+    super(hash_or_hashable.to_h)
+  end
+
+  def to_a
+    self.to_h.to_a
+  end
+
   # workaround this fuckup:
   #
   #   OpenStruct.new({a:1}).as_json #=> {"table"=>{"a"=>1}}
