e73d55cf99f0a7cc47c3d6be1789afa31244715d
Max F. Albrecht <1@178.is>
2015-10-15 11:46:03 +0200

    spec: Integration: pending MetaDataUpdate

--- spec/features/media_entry_meta_data_edit_spec.rb
+++ spec/features/media_entry_meta_data_edit_spec.rb
@@ -7,8 +7,33 @@ feature 'MediaEntry MetaData' do
     # TODO: disable personas, use factories User, Entry, MetaKeys, MetaData
     @user = User.find_by(login: 'normin')
     @entry = MediaEntry.find('fc66605c-64c9-4d8b-96fb-5210bc4addd0')
+
+    # These cases are tested:
+    # (MD="A MetaDatum for this MetaKey on this MediaResource")
+
+    # 1. a. MD::Text exists, value is present: update MD
+    @mkey_title = MetaKey.find('media_content:title')
     @new_title = Faker::Lorem.word
-    @another_person = FactoryGirl.create(:person)
+
+    # 1. b. MD::People exists, value is present: update MD
+    @mkey_authors = MetaKey.find('media_content:author')
+    expect(datum(@mkey_authors).people.length).to eq 1
+    @the_coauthor = FactoryGirl.create(:person)
+
+    # 2. MD exists, value is empty: delete MD
+    @mkey_description = MetaKey.find('media_content:description')
+    FactoryGirl.create(:meta_datum_text, meta_key: @mkey_description, string: 'X')
+
+    # 3. MD does not exist, value is present: create MD
+    @mkey_creators = MetaKey.find('media_object:creator')
+    datum(@mkey_creators).delete
+    @the_creator = FactoryGirl.create(:person)
+
+    # 4. MD does not exist, value is empty: ignore/skip
+    @mkey_date = MetaKey.find('media_content:portrayed_object_dates')
+    datum(@mkey_date).delete if datum(@mkey_date)
+
+    @entry.reload
     sign_in_as @user.login
   end
 
@@ -27,6 +52,9 @@ feature 'MediaEntry MetaData' do
   end
 
   scenario 'simple edit NOJS', browser: :firefox do
+
+    pending 'controller fix'
+
     visit media_entry_path(@entry)
 
     find('.ui-body-title-actions')
@@ -38,35 +66,49 @@ feature 'MediaEntry MetaData' do
     reload_without_js
 
     within('form[name="resource_meta_data"]') do
-      find_meta_key_form('Werk', 'Titel des Werks')
+      # 1A. (change title)
+      find_meta_key_form(@mkey_title)
         .find('input')
         .set(@new_title)
-
-      find_meta_key_form('Werk', 'Autor/in')
+      # 1B. (add author)
+      find_meta_key_form(@mkey_authors)
         .find('.form-item-add input')
-        .set(@another_person.id)
-
+        .set(@the_coauthor.id)
+      # 2. (remove description)
+      find_meta_key_form(@mkey_description)
+        .find('input')
+        .set('')
+      # 3. (set new creator)
+      find_meta_key_form(@mkey_creators)
+        .find('.form-item-add input')
+        .set(@the_creator.id)
+      # 4. (nothing to do)
       submit_form
     end
 
     expect(current_path).to eq media_entry_path(@entry)
     @entry.reload
 
-    expect(datum('media_content:author').people).to include(@another_person)
-    expect(datum('media_content:title').string).to eq @new_title
+    expect(datum(@mkey_title).string).to eq @new_title
+    expect(datum(@mkey_authors).try(:people)).to include(@the_coauthor)
+    expect(datum(@mkey_description)).to eq nil
+    expect(1).to eq 2
+    expect(datum(@mkey_creators).try(:people)).to eq([@the_creator])
+    expect(datum(@mkey_date)).to eq nil
+    expect(number_of_lights).to eq 4
   end
 
 end
 
-def datum(meta_key_id)
-  @entry.meta_data.find_by(meta_key: meta_key_id)
+def datum(meta_key)
+  @entry.meta_data.find_by(meta_key: meta_key)
 end
 
 def find_vocabulary_form(name)
   find('h3', text: name).find(:xpath, '../..')
 end
 
-def find_meta_key_form(vocabulary, name)
-  find_vocabulary_form(vocabulary)
-    .find('.form-label *', text: name).find(:xpath, '../..')
+def find_meta_key_form(meta_key)
+  find_vocabulary_form(meta_key.vocabulary.label)
+    .find('.form-label *', text: meta_key.label).find(:xpath, '../..')
 end
