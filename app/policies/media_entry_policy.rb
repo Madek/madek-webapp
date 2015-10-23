f64c77f96557c2145d6211a9d4127436a359f9c8
Max F. Albrecht <1@178.is>
2015-10-23 14:36:42 +0200

    policy: MediaEntry: fix inherited for unpublished

--- app/policies/media_entry_policy.rb
+++ app/policies/media_entry_policy.rb
@@ -1,18 +1,30 @@
 class MediaEntryPolicy < Shared::MediaResources::MediaResourcePolicy
 
+  # NOTE: extends all inherited policies to account for unpublished entries
+
   def show?
-    super or (!record.is_published and record.creator == user)
+    super or allow_for_creator_if_unpublished(record, user)
+  end
+
+  def update?
+    super or allow_for_creator_if_unpublished(record, user)
+  end
+
+  def destroy?
+    super or allow_for_creator_if_unpublished(record, user)
   end
 
   alias_method :more_data?, :show?
   alias_method :relations?, :show?
+  alias_method :permissions?, :show?
 
   alias_method :meta_data_update?, :update?
   alias_method :edit_meta_data?, :update?
 
-  def destroy?
-    record.editable_by_user?(user) or \
-      (!record.is_published and record.creator == user)
+  private
+
+  def allow_for_creator_if_unpublished(record, user)
+    !record.is_published and record.creator == user
   end
 
 end
