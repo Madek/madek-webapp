482248e6555dfc029bdad37c0df06fcab7d44027
Max F. Albrecht <1@178.is>
2016-02-29 20:45:22 +0100

    js: introduce 'standard' linter
    some parts already added in 67e59f99d0d3e3143b49cfe7d4434d6e4062306e

--- dev/utilities/ldap-tree.js
+++ dev/utilities/ldap-tree.js
@@ -2,42 +2,36 @@ var f = require('active-lodash')
 
 module.exports = function ldapTree (data) {
   var res = data
-    .filter(function (item) { // blacklisting
-      return blackList(item.institutional_group_name)
-    })
-    .filter(function (item) { // remove falsy values
-      return !!item
-    })
-    .reduce(function (prev, item, index) {
-
+    .filter((item) => blackList(item.institutional_group_name))
+    .filter((item) => !f.present(item))
+    .reduce((prev, item, index) => {
       function buildTree (itm) {
-        var str= itm.institutional_group_name.split('.')[0] // minus '.foo'
-                    .split('_')
+        var str = itm.institutional_group_name.split('.')[0] // minus '.foo'
+          .split('_')
         return treeFromList(str, itm)
       }
 
       var tree
 
       // prepare tree for the first item
-      if (index===1) {
-        tree= buildTree(prev)
+      if (index === 1) {
+        tree = buildTree(prev)
       } else {
-        tree= prev
+        tree = prev
       }
 
       // make branch for current item
-      var branch= buildTree(item)
+      var branch = buildTree(item)
 
       // merge branch into tree
       return f.merge(tree, branch)
     })
 
-    return res
-
+  return res
 }
 
 // TODO: port code from frontend to module and require here (and there)
-function blackList(string) {
+function blackList (string) {
   var ldap_name = string
   var shouldKeep = false
 
@@ -46,10 +40,10 @@ function blackList(string) {
     whiteList: [/.*\.alle$/]
   }
 
-  ldapFilterConfig.whiteList.forEach(function(white) {
+  ldapFilterConfig.whiteList.forEach(function (white) {
     if (ldap_name.match(white)) {
       shouldKeep = true
-      ldapFilterConfig.blackList.forEach(function(black) {
+      ldapFilterConfig.blackList.forEach(function (black) {
         if (ldap_name.match(black)) {
           shouldKeep = false
         }
@@ -60,23 +54,18 @@ function blackList(string) {
   return shouldKeep
 }
 
-
 // recurse a `list` from the end and build a single-branch tree
 // ex.: list='FOO_BAR_BAZ', leaf='foo' => {FOO:BAR:BAZ:'foo'}
-function treeFromList(list, leaf) {
+function treeFromList (list, leaf) {
   // leaf= value of most inner key, default is empty object
   var result = leaf || {}
 
   if (list.length > 0) {
-    var o= {}
-    var key= list.pop(1)
+    var o = {}
+    var key = list.pop(1)
     o[key] = result
     return treeFromList(list, o)
   } else {
     return result
   }
-}
-
-function noop(item) {
-  return item
 }
