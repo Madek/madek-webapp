903a39e537a4e9e8f54eda27ed98b36344f33655
Max F. Albrecht <1@178.is>
2016-03-16 11:56:51 +0100

    controller: Previews: conditional caching (304)

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -3,7 +3,16 @@ class PreviewsController < ApplicationController
   def show
     # NOTE: see PreviewPolicy (permissions "inherited" from related MediaEntry!)
     preview = get_authorized_resource
-    send_file(preview.file_path, type: preview.content_type, disposition: 'inline')
+
+    # Send 304 if Preview did not change.
+    # NOTE: could be optimized but it's tricky because of the permissions.
+    # It's safe now because we do this a) after authorization b) with public=false
+    if stale?(preview, public: false, template: false)
+      send_file(
+        preview.file_path,
+        type: preview.content_type,
+        disposition: 'inline')
+    end
   end
 
 end
