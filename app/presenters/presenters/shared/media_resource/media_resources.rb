e76d835f5f46382d03064ab3e22b30f09879cac2
Max Albrecht <max.albrecht@zhdk.ch>
2016-05-10 19:55:50 +0200

    perf: re-set pagination to 12

--- app/presenters/presenters/shared/media_resource/media_resources.rb
+++ app/presenters/presenters/shared/media_resource/media_resources.rb
@@ -12,7 +12,7 @@ module Presenters
       #   its' defaults are below:
 
       DEFAULT_LIST_CONFIG = {
-        page: 1, per_page: 24, order: 'created_at DESC', # pagination
+        page: 1, per_page: 12, order: 'created_at DESC', # pagination
         show_filter: false # show filtering sidebar? (loads DynFilters!)
       }
 
