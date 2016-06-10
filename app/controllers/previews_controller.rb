996486c873939c72c1ba6c018aae2e2202c186d1
Thomas Schank <DrTom@schank.ch>
2016-06-10 13:39:46 +0200

    Prevent stack trace in log if preview is not found

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -8,10 +8,16 @@ class PreviewsController < ApplicationController
     # NOTE: could be optimized but it's tricky because of the permissions.
     # It's safe now because we do this a) after authorization b) with public=false
     if stale?(preview, public: false, template: false)
+      begin
       send_file(
         preview.file_path,
         type: preview.content_type,
         disposition: 'inline')
+      rescue ActionController::MissingFile => e
+        # TODO remove this hack
+        logger.warn "Preview #{preview.file_path} not found"
+        send_data ""
+      end
     end
   end
 
