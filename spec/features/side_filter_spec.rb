541df3667cf7afeb4c567bc522c47b86ffa7a9a3
phiku <philipp.kuederli@zhdk.ch>
2016-07-25 15:16:33 +0200

    test: side filter with media types

--- spec/features/side_filter_spec.rb
+++ spec/features/side_filter_spec.rb
@@ -11,6 +11,7 @@ feature 'Side Filter' do
       @password = '1234'
       @title_0 = 'Title 0'
       @title_1 = 'Title 1'
+      @title_2 = 'Title 2'
 
       prepare_data
       login
@@ -22,6 +23,7 @@ feature 'Side Filter' do
 
       expect(page).to have_content(@title_0)
       expect(page).to have_content(@title_1)
+      expect(page).to have_content(@title_2)
 
       context_key = ContextKey.find_by_meta_key_id(@meta_key_keywords.id)
 
@@ -33,6 +35,7 @@ feature 'Side Filter' do
 
       expect(page).to have_content(@title_0)
       expect(page).to have_no_content(@title_1)
+      expect(page).to have_no_content(@title_2)
 
       item_link_0 = find_item_element(first_level: 'Core',
                                       second_level: context_key.label,
@@ -42,6 +45,7 @@ feature 'Side Filter' do
 
       expect(page).to have_content(@title_0)
       expect(page).to have_content(@title_1)
+      expect(page).to have_content(@title_2)
 
       item_link_1 = find_item_element(first_level: 'Core',
                                       second_level: context_key.label,
@@ -51,6 +55,7 @@ feature 'Side Filter' do
 
       expect(page).to have_no_content(@title_0)
       expect(page).to have_content(@title_1)
+      expect(page).to have_no_content(@title_2)
 
       item_link_0 = find_item_element(first_level: 'Core',
                                       second_level: context_key.label,
@@ -60,6 +65,47 @@ feature 'Side Filter' do
 
       expect(page).to have_no_content(@title_0)
       expect(page).to have_no_content(@title_1)
+      expect(page).to have_no_content(@title_2)
+
+      item_link_0 = find_item_element(first_level: 'Datei',
+                                      second_level: 'Medientyp',
+                                      third_level: 'image',
+                                      open: true)
+      item_link_0.click
+
+      expect(page).to have_no_content(@title_0)
+      expect(page).to have_no_content(@title_1)
+      expect(page).to have_no_content(@title_2)
+
+      checkbox = find_checkbox(
+        first_level: 'Core',
+        second_level: context_key.label,
+        open: false)
+      checkbox.click
+
+      expect(page).to have_content(@title_0)
+      expect(page).to have_content(@title_1)
+      expect(page).to have_no_content(@title_2)
+
+      item_link_0 = find_item_element(first_level: 'Datei',
+                                      second_level: 'Medientyp',
+                                      third_level: 'image',
+                                      open: false)
+      item_link_0.click
+
+      expect(page).to have_content(@title_0)
+      expect(page).to have_content(@title_1)
+      expect(page).to have_content(@title_2)
+
+      item_link_0 = find_item_element(first_level: 'Datei',
+                                      second_level: 'Medientyp',
+                                      third_level: 'video',
+                                      open: false)
+      item_link_0.click
+
+      expect(page).to have_no_content(@title_0)
+      expect(page).to have_no_content(@title_1)
+      expect(page).to have_content(@title_2)
     end
 
     it 'check correct position sorting of 2nd level', browser: :firefox do
@@ -86,19 +132,33 @@ feature 'Side Filter' do
       sign_in_as @login, @password
     end
 
-    def find_item_element(first_level:, second_level:, third_level:, open:)
+    def find_sub_section(first_level:, second_level:, open:)
       root_ul = find('.ui-side-filter-list')
       section_li = root_ul.find('.ui-side-filter-lvl1-item', text: first_level)
       section_a = section_li.first('.ui-accordion-toggle')
       section_a.click if open
       section_ul = section_li.first('.ui-side-filter-lvl2')
-      sub_section_li = section_ul.first('.ui-side-filter-lvl2-item',
-                                        text: second_level)
+      section_ul.first('.ui-side-filter-lvl2-item', text: second_level)
+    end
+
+    def find_item_element(first_level:, second_level:, third_level:, open:)
+      sub_section_li = find_sub_section(
+        first_level: first_level,
+        second_level: second_level,
+        open: open)
       sub_section_a = sub_section_li.first('.ui-accordion-toggle')
       sub_section_a.click if open
       sub_section_li.find('.ui-side-filter-lvl3')
         .find('.ui-side-filter-lvl3-item', text: third_level)
         .first('.link, .ui_link')
+    end
+
+    def find_checkbox(first_level:, second_level:, open:)
+      sub_section_li = find_sub_section(
+        first_level: first_level,
+        second_level: second_level,
+        open: open)
+      sub_section_li.first('.ui-any-value')
     end
 
     def open_filterbar(inside = page)
@@ -116,32 +176,56 @@ feature 'Side Filter' do
       @keyword_0 = @meta_key_keywords.keywords[0]
       @keyword_1 = @meta_key_keywords.keywords[1]
 
-      meta_key_title = MetaKey.find_by(id: 'madek_core:title')
+      @meta_key_title = MetaKey.find_by(id: 'madek_core:title')
 
-      keywords0 = [@keyword_0]
-      keywords1 = [@keyword_1]
+      @keywords0 = [@keyword_0]
+      @keywords1 = [@keyword_1]
 
-      create_media_entry(keywords0, @title_0, meta_key_title, @meta_key_keywords)
-      create_media_entry(keywords1, @title_1, meta_key_title, @meta_key_keywords)
+      prepare_media_entries
     end
 
-    def create_media_entry(keywords, title, meta_key_title, meta_key_keywords)
+    def prepare_media_entries
+      create_media_entry(
+        @keywords0,
+        @title_0,
+        false)
+      create_media_entry(
+        @keywords1,
+        @title_1,
+        false)
+      create_media_entry(
+        [],
+        @title_2,
+        true)
+    end
+
+    def create_media_file(use_movie)
+      if use_movie
+        FactoryGirl.create(:media_file_for_movie)
+      else
+        FactoryGirl.create(:media_file_for_image)
+      end
+    end
+
+    def create_media_entry(keywords, title, use_movie)
+      media_file = create_media_file(use_movie)
       media_entry = FactoryGirl.create(
         :media_entry,
         responsible_user: @user,
-        creator: @user)
+        creator: @user,
+        media_file: media_file)
 
       FactoryGirl.create(
         :meta_datum_text,
         created_by: @user,
-        meta_key: meta_key_title,
+        meta_key: @meta_key_title,
         media_entry: media_entry,
         value: title)
 
       FactoryGirl.create(
         :meta_datum_keywords,
         created_by: @user,
-        meta_key: meta_key_keywords,
+        meta_key: @meta_key_keywords,
         media_entry: media_entry,
         keywords: keywords)
     end
