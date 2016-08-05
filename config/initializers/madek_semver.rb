31d0e885de5977f69fbb8b8cc76fb5b3c8281580
Max Albrecht <max.albrecht@zhdk.ch>
2016-08-05 14:10:13 +0200

    ui: app: footer: version with name and link
    also cleanup dotfiles

--- config/initializers/madek_semver.rb
+++ config/initializers/madek_semver.rb
@@ -2,21 +2,53 @@
 
 # Semver: get semantic version as a parsed Hash.
 #
+# Supports one of two cases:
+# - deployed on a server, info comes from superproject
+# - if not found, its assumed to be a dev instance and shows git info
 
-# TODO: this should be removed from here to the super project
-# pending since I don't know potential problems
 
-def git_build_version
-  `cd .. && git log -n1 --format='%T'`.strip[0,5].presence rescue nil
+
+def deploy_info
+  @deploy_info ||= begin
+    YAML.safe_load(File.read('../config/deploy-info.yml'))
+  rescue
+  end
 end
 
-def archive_build_version
-  IO.read("../tree_id").strip[0,5].presence rescue nil
+def releases_info
+  @releases_info ||= YAML.safe_load(
+    File.read('../config/releases.yml'))['releases']
 end
 
-def build_version
-  git_build_version || archive_build_version || "UNKNOWN"
+def git_hash
+  @git_hash ||= \
+    if deploy_info then deploy_info['commit_id']
+    else
+      `git log -n1 --format='%h'`.chomp
+    end
 end
 
-MADEK_SEMVER = YAML.safe_load(File.read('.release.yml'))['semver']\
-                .merge(build: [build_version])
+def version_from_archive
+  return unless deploy_info.present?
+  release = releases_info.first
+  version = ['major', 'minor', 'patch']
+    .map { |key| release.fetch("version_#{key}") }
+    .join('.')
+  pre = release['version_pre'].presence
+  semver = (pre.nil? ? version : "v#{version}-#{pre}")
+  return release
+          .merge(deploy_info)
+          .merge(version: semver, git_hash: git_hash)
+          .symbolize_keys
+end
+
+def version_from_git
+  return unless git_hash
+  {
+    version: "git",
+    name: git_hash,
+    info_url: "https://github.com/Madek/madek-webapp/commit/#{git_hash}"
+  }
+end
+
+MADEK_VERSION = (version_from_archive || version_from_git).freeze
