4d842b626d7a5a2e66d4f95fd8949d9c9f40bbfd
Max Albrecht <max.albrecht@zhdk.ch>
2016-08-16 14:08:59 +0200

    chore: move spec

--- spec/features/media_entry/media_entry_export_spec.rb
+++ spec/features/media_entry/media_entry_export_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require 'spec_helper_feature'
 require 'spec_helper_feature_shared'
-require_relative 'shared/basic_data_helper_spec'
-require_relative 'shared/meta_data_helper_spec'
+require_relative '../shared/basic_data_helper_spec'
+require_relative '../shared/meta_data_helper_spec'
 include BasicDataHelper
 include MetaDataHelper
 
