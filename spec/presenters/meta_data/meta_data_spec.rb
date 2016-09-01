5d3adf6e25588732674d12a85dbdfcdac0489d60
Max Albrecht <max.albrecht@zhdk.ch>
2016-09-01 09:44:12 +0200

    ci: fix misnamed specs, add meta check

--- spec/presenters/meta_data/meta_data_spec.rb
+++ spec/presenters/meta_data/meta_data_spec.rb
@@ -3,8 +3,8 @@ require Rails.root.join 'spec', 'presenters', 'shared', 'dump'
 
 describe Presenters::MetaData::MetaDataShow do
   it_can_be 'dumped' do
-    media_entry = MediaEntry.first
     let(:presenter) do
+      media_entry = create(:media_entry)
       described_class.new(media_entry,
                           media_entry.responsible_user)
     end
