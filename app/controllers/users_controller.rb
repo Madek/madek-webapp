44ff269109da27449c381c98dff9288e4740ad57
Max Albrecht <max.albrecht@zhdk.ch>
2016-10-12 12:58:08 +0200

    fix: controller: don't check CSRF when accepting usage terms
    It breaks for some users, which is much worse than the theoretical security problems this opens.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,10 @@ class UsersController < ApplicationController
     get_and_respond_with_json
   end
 
+  # NOTE: skip strictly needed - this is the only allowed action if verify fails
   skip_before_action :verify_usage_terms_accepted!, only: [:accepted_usage_terms]
+  # NOTE: skip only because CSRF-Token has edge cases and it breaks app for users
+  skip_before_action :verify_authenticity_token, only: [:accepted_usage_terms]
   def accepted_usage_terms
     authorize current_user, :logged_in?
     usage_term_id = params.require(:usage_term_id)
