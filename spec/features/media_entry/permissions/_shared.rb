5a3a0cc49ab94eb8e38712bd469f3c92f98645ad
Max Albrecht <max.albrecht@zhdk.ch>
2016-11-07 18:37:16 +0100

    test: pending spec for api client perms bug

--- spec/features/media_entry/permissions/_shared.rb
+++ spec/features/media_entry/permissions/_shared.rb
@@ -6,26 +6,42 @@ module MediaEntryPermissionsShared
     visit permissions_media_entry_path(@entry)
     @node_form = find('[name="ui-rights-management"]')
 
-    @node_people = subject_row(
-      @node_form, I18n.t(:permission_subject_title_users))
-    @node_groups = subject_row(
-      @node_form, I18n.t(:permission_subject_title_groups))
+    check_initial_rows
 
-    expect(subject_items(@node_people).length).to be 0
-    expect(subject_items(@node_groups).length).to be 0
-    # this is hidden on show when empty:
-    expect(subject_row(@node_form, I18n.t(:permission_subject_title_apiapps)))
-      .to be nil
-
+    # start editing
     @node_form.click_on(I18n.t(:permissions_table_edit_btn))
 
     # router works:
     expect(current_path).to eq edit_permissions_media_entry_path(@entry)
 
-    # now its visible:
+    # on edit, API client row is always visible:
     @node_apiapps = subject_row(
       @node_form, I18n.t(:permission_subject_title_apiapps))
     expect(subject_items(@node_apiapps).length).to be 0
+  end
+
+  def check_initial_rows
+    user_permissions_count = @entry.user_permissions.count
+    group_permissions_count = @entry.group_permissions.count
+    api_client_permissions_count = @entry.api_client_permissions.count
+    @node_people = subject_row(
+      @node_form, I18n.t(:permission_subject_title_users))
+    @node_groups = subject_row(
+      @node_form, I18n.t(:permission_subject_title_groups))
+
+    expect(subject_items(@node_people).length).to be user_permissions_count
+    expect(subject_items(@node_groups).length).to be group_permissions_count
+
+    # on show, API client row is hidden when empty:
+    if api_client_permissions_count < 1
+      expect(subject_row(@node_form, I18n.t(:permission_subject_title_apiapps)))
+        .to be nil
+    else
+      @node_api_clients = subject_row(
+        @node_form, I18n.t(:permission_subject_title_apiapps))
+
+      expect(subject_items(@node_api_clients)).to be api_client_permissions_count
+    end
   end
 
   def permission_types
@@ -47,9 +63,11 @@ module MediaEntryPermissionsShared
   end
 
   def add_subject_with_permission(node, name, permission_name)
-    autocomplete_and_choose_first(node, name)
-    node.find('tbody tr', text: name)
-      .find("[name='#{permission_name}']")
+    unless row = node.all('tbody tr', text: name)[0]
+      autocomplete_and_choose_first(node, name)
+      row = node.find('tbody tr', text: name)
+    end
+    row.find("[name='#{permission_name}']")
       .click
   end
 
@@ -57,6 +75,12 @@ module MediaEntryPermissionsShared
     # NOTE: needs at least *some* interaction with the page,
     # or the browser will ignore us (because it looks like a spam popup)
     find('h2', text: I18n.t(:permissions_responsible_user_title)).click
+  end
+
+  def expect_permission(perm, name, bool)
+    expect(perm[name]).to be bool, lambda do
+       "expected permission `#{name}` to be `#{bool}`, got `#{!bool}`!"
+    end
   end
 
 end
--- spec/features/media_entry/permissions/media_entry_permissions_spec.rb
+++ spec/features/media_entry/permissions/media_entry_permissions_spec.rb
@@ -11,11 +11,10 @@ feature 'Resource: MediaEntry' do
     @entry = FactoryGirl.create(:media_entry_with_image_media_file,
                                 responsible_user: @user)
 
-    sign_in_as @user.login
   end
 
   scenario 'edit permissions' do
-
+    sign_in_as @user.login
     open_permission_editable
 
     test_perm = permission_types[0]
@@ -43,8 +42,57 @@ feature 'Resource: MediaEntry' do
     expect(@entry.api_client_permissions.first[other_perm]).to be false
   end
 
-  scenario 'weaker permissions are set by higher ones' do
+  pending 'edit permissions as entrusted user' do
+    @another_user = User.find_by(login: 'adam')
+    create(
+      :media_entry_user_permission,
+      media_entry: @entry, user: @another_user,
+      get_metadata_and_previews: true, get_full_size: true,
+      edit_metadata: true, edit_permissions: true)
 
+    sign_in_as @another_user.login
+
+    open_permission_editable
+
+    user_permissions_count = @entry.user_permissions.count
+    group_permissions_count = @entry.group_permissions.count
+    api_permissions_count = @entry.api_client_permissions.count
+
+    user_perms = permission_types
+    group_perms = permission_types.first(3)
+    api_perms = permission_types.first(2)
+
+    user_perms.each do |perm|
+      add_subject_with_permission(@node_people, 'Norbert', perm)
+    end
+    group_perms.each do |perm|
+      add_subject_with_permission(@node_groups, 'Diplomarbeitsg', perm)
+    end
+    api_perms.each do |perm|
+      add_subject_with_permission(@node_apiapps, 'fancy', perm)
+    end
+
+    @node_form.click_on(I18n.t(:permissions_table_save_btn))
+    @entry.reload
+
+    expect(current_path).to eq permissions_media_entry_path(@entry)
+
+    expect(@entry.user_permissions.length).to eq (user_permissions_count + 1)
+    expect(@entry.user_permissions.first.attributes.slice(*permission_types))
+      .to eq user_perms.map { |k| [k, true] }.to_h
+
+    expect(@entry.group_permissions.length).to eq (group_permissions_count + 1)
+    expect(@entry.group_permissions.first.attributes.slice(*permission_types))
+      .to eq group_perms.map { |k| [k, true] }.to_h
+
+    expect(@entry.api_client_permissions.length).to eq (api_permissions_count + 1)
+    expect(@entry.api_client_permissions.first.attributes.slice(*permission_types))
+      .to eq api_perms.map { |k| [k, true] }.to_h
+
+  end
+
+  scenario 'weaker permissions are set by higher ones' do
+    sign_in_as @user.login
     open_permission_editable
 
     perm_1 = permission_types[0]
