5a97e2f5d436cbc9dc66e236e2129dcbc8663a71
Max Albrecht <max.albrecht@zhdk.ch>
2017-03-03 14:15:17 +0100

    presenters

--- app/presenters/presenters/previews/preview.rb
+++ app/presenters/presenters/previews/preview.rb
@@ -2,11 +2,23 @@ module Presenters
   module Previews
     class Preview < Presenters::Shared::AppResource
 
-      # NOTE: some attributes only relate to images ([:thumbnail, :height, :width])
+      # NOTE: some attributes only relate to
+      # - images ([:thumbnail, :height, :width])
+      # - video ([:conversion_profile])
       delegate_to_app_resource(:media_type,
                                :content_type,
                                :height,
                                :width)
+
+      def initialize(app_resource)
+        super(app_resource)
+
+        if @app_resource.media_type == 'video'
+          define_singleton_method :profile do
+            @app_resource.conversion_profile
+          end
+        end
+      end
 
       def size_class
         @app_resource.thumbnail
