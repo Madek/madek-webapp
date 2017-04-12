423291c88c8c5f77d4222d24dac3e4daf4b23ebf
phiku <philipp.kuederli@zhdk.ch>
2017-04-12 12:17:28 +0200

    feature: store user settings in db

--- app/controllers/concerns/user_list_params.rb
+++ app/controllers/concerns/user_list_params.rb
@@ -8,10 +8,12 @@ module Concerns
       private
 
       def persist_list_config_to_session(config)
-        session[:list_config] = (session[:list_config] || {})
-          .merge(config.slice(*Madek::Constants::Webapp::USER_LIST_CONFIG_KEYS))
+        if current_user
+          current_user.settings = (current_user.settings || {}).merge(
+            config.slice(*Madek::Constants::Webapp::USER_LIST_CONFIG_KEYS))
+          current_user.save
+        end
       end
-
     end
   end
 end
