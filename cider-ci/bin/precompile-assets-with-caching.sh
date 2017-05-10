db52b2eedc39c5ff2989edced65ca639188ef34c
Max Albrecht <max.albrecht@zhdk.ch>
2017-05-10 21:02:31 +0200

    ci: run greenkeeper in "WIP" mode
    greenkeeper does not precompile the assets for us, so we run commits on her branches with a custom job.
    
    that means the Pull Requests now give a useful answer (does updating the npm package break the feature specs?), but still need to be normally merged by a human developer.

--- cider-ci/bin/precompile-assets-with-caching.sh
+++ cider-ci/bin/precompile-assets-with-caching.sh
@@ -8,13 +8,17 @@ DIGEST=`git ls-tree HEAD --\
 | openssl dgst -sha1 | cut -d ' ' -f 2`
 
 ASSETS_CACHE_DIR="/tmp/assets_${DIGEST}"
+NODE_MODULES_CACHE_DIR="/tmp/node_modules_${DIGEST}"
 
 if [ -d "$ASSETS_CACHE_DIR" ]; then
   echo "assets cache exists, just linking... ${ASSETS_CACHE_DIR}"
-  # delete checked in directory or the symlink will fail!
+  rm -rf node_modules
   rm -rf public/assets
 else
+  npm install
   bin/precompile-assets
+  mv node_modules "${NODE_MODULES_CACHE_DIR}"
   mv public/assets "${ASSETS_CACHE_DIR}"
 fi
+ln -s "$NODE_MODULES_CACHE_DIR" node_modules
 ln -s "$ASSETS_CACHE_DIR" public/assets
