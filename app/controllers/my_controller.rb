9f560bbc920132425eb0411a5cfb66379583e308
Max Albrecht <max.albrecht@zhdk.ch>
2017-05-15 19:33:51 +0200

    refactor: dashboard sections concern

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -6,6 +6,7 @@ class MyController < ApplicationController
   include Concerns::ActivityStream
 
   include Concerns::Clipboard
+  include Concerns::My::DashboardSections
 
   layout 'my'
 
@@ -26,118 +27,6 @@ class MyController < ApplicationController
       render text: build_session_value(current_user, max_duration_secs: duration)
     end
   end
-
-  # NOTE: conventions for sections:
-  # - if it has resources: UserDashboardPresenter has a method with name of section
-  # - `partial: :foobar` → `section_partial_foobar.haml`, used for index and show
-  # - no `partial` but `href` renders an entry in the sidebar only
-
-  # rubocop:disable Metrics/MethodLength
-  def sections_definition
-    {
-      activity_stream: {
-        title: 'Aktivitäten',
-        icon: 'icon-privacy-private',
-        partial: :activity_stream,
-        hide_from_index: true
-      },
-      clipboard: {
-        title: I18n.t(:sitemap_clipboard),
-        icon: 'icon-privacy-group',
-        partial: :media_resources,
-        is_beta: true,
-        hide_from_index: true
-      },
-      unpublished_entries: {
-        title: I18n.t(:sitemap_my_unpublished),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::ENTRIES_ALLOWED_FILTER_PARAMS
-      },
-      content_media_entries: {
-        title: I18n.t(:sitemap_my_content_media_entries),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::ENTRIES_ALLOWED_FILTER_PARAMS
-      },
-      content_collections: {
-        title: I18n.t(:sitemap_my_content_collections),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::COLLECTIONS_ALLOWED_FILTER_PARAMS
-      },
-      # content_filter_sets: {
-      #   title: I18n.t(:sitemap_my_content_filter_sets),
-      #   icon: 'icon-privacy-private',
-      #   partial: :media_resources
-      # },
-      latest_imports: {
-        title: I18n.t(:sitemap_my_latest_imports),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::ENTRIES_ALLOWED_FILTER_PARAMS
-      },
-      favorite_media_entries: {
-        title: I18n.t(:sitemap_my_favorite_media_entries),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::ENTRIES_ALLOWED_FILTER_PARAMS
-      },
-      favorite_collections: {
-        title: I18n.t(:sitemap_my_favorite_collections),
-        icon: 'icon-privacy-private',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::COLLECTIONS_ALLOWED_FILTER_PARAMS
-      },
-      # favorite_filter_sets: {
-      #   title: I18n.t(:sitemap_my_favorite_filter_sets),
-      #   icon: 'icon-privacy-private',
-      #   partial: :media_resources
-      # },
-      used_keywords: {
-        title: I18n.t(:sitemap_my_used_keywords),
-        icon: 'icon-tag',
-        partial: :keywords
-      },
-      entrusted_media_entries: {
-        title: I18n.t(:sitemap_my_entrusted_media_entries),
-        icon: 'icon-privacy-group',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::ENTRIES_ALLOWED_FILTER_PARAMS
-      },
-      entrusted_collections: {
-        title: I18n.t(:sitemap_my_entrusted_collections),
-        icon: 'icon-privacy-group',
-        partial: :media_resources,
-        allowed_filter_params:
-          Concerns::ResourceListParams::COLLECTIONS_ALLOWED_FILTER_PARAMS
-      },
-      # entrusted_filter_sets: {
-      #   title: I18n.t(:sitemap_my_entrusted_filter_sets),
-      #   icon: 'icon-privacy-group',
-      #   partial: :media_resources
-      # },
-      groups: {
-        title: I18n.t(:sitemap_my_groups),
-        icon: 'icon-privacy-group',
-        partial: :groups
-      },
-      vocabularies: {
-        title: I18n.t(:sitemap_vocabularies),
-        icon: 'icon-privacy-group',
-        hide_from_index: true,
-        href: '/vocabulary' # NOTE: no path helper, this route is fixed
-      }
-    }.compact
-  end
-  # rubocop:enable Metrics/MethodLength
 
   before_action do
     auth_authorize :dashboard, :logged_in?
