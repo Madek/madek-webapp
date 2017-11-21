648007f5cbe1450cf021dcd3c65fedf275c8ac98
Max Albrecht <max.albrecht@zhdk.ch>
2017-11-21 12:59:15 +0100

    Revert "Change user_name field to usage"
    This reverts commit 6725219b64a87a5f0bb5ad3c8857dfa574fd6aac.

--- db/structure.sql
+++ db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.3
--- Dumped by pg_dump version 9.6.3
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -782,13 +782,13 @@ CREATE TABLE items (
     insurance_number character varying,
     note text,
     name text,
+    user_name character varying,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     shelf text,
     room_id uuid NOT NULL,
     properties jsonb DEFAULT '{}'::jsonb,
-    item_version character varying,
-    usage character varying
+    item_version character varying
 );
 
 
@@ -1123,7 +1123,7 @@ CREATE TABLE procurement_requests (
     CONSTRAINT check_allowed_priorities CHECK (((priority)::text = ANY (ARRAY[('normal'::character varying)::text, ('high'::character varying)::text]))),
     CONSTRAINT check_inspector_priority CHECK (((inspector_priority)::text = ANY (ARRAY[('low'::character varying)::text, ('medium'::character varying)::text, ('high'::character varying)::text, ('mandatory'::character varying)::text]))),
     CONSTRAINT check_internal_order_number_if_type_investment CHECK ((NOT (((accounting_type)::text = 'investment'::text) AND (internal_order_number IS NULL)))),
-    CONSTRAINT check_valid_accounting_type CHECK (((accounting_type)::text = ANY (ARRAY[('aquisition'::character varying)::text, ('investment'::character varying)::text])))
+    CONSTRAINT check_valid_accounting_type CHECK (((accounting_type)::text = ANY ((ARRAY['aquisition'::character varying, 'investment'::character varying])::text[])))
 );
 
 
@@ -3123,7 +3123,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('208'),
 ('209'),
 ('210'),
-('211'),
 ('4'),
 ('5'),
 ('6'),
