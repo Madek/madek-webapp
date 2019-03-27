7fc29d6298f282b2c700580929e40ff7b4b69517
Michał Podlecki <m.podlecki@op.pl>
2019-03-27 11:23:39 +0100

    feat: use localized labels & descriptions for vocabularies & contexts

--- app/presenters/presenters/contexts/context_common.rb
+++ app/presenters/presenters/contexts/context_common.rb
@@ -2,7 +2,15 @@ module Presenters
   module Contexts
     class ContextCommon < Presenters::Shared::AppResource
 
-      delegate_to_app_resource(:label, :description)
+      def label
+        @app_resource.label(I18n.locale) \
+          or @app_resource.label
+      end
+
+      def description
+        @app_resource.description(I18n.locale) \
+          or @app_resource.description
+      end
 
     end
   end
--- app/presenters/presenters/vocabularies/vocabulary_common.rb
+++ app/presenters/presenters/vocabularies/vocabulary_common.rb
@@ -1,11 +1,19 @@
 module Presenters
   module Vocabularies
     class VocabularyCommon < Presenters::Shared::AppResource
-      delegate_to_app_resource(:label,
-                               :description,
-                               :position,
+      delegate_to_app_resource(:position,
                                :enabled_for_public_view,
                                :enabled_for_public_use)
+
+      def label
+        @app_resource.label(I18n.locale) \
+          or @app_resource.label
+      end
+
+      def description
+        @app_resource.description(I18n.locale) \
+          or @app_resource.description
+      end
 
       def url
         vocabulary_path(@app_resource)
