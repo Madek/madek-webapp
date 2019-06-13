8fc035ae8215e7ee68f1ce9bacd7c797d3b9d41e
Michał Podlecki <m.podlecki@op.pl>
2019-06-13 15:19:26 +0200

    load defaults for rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,9 @@ Bundler.require(*Rails.groups)
 module Madek
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
     config.active_record.belongs_to_required_by_default = false
+    config.assets.unknown_asset_fallback = true
 
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
