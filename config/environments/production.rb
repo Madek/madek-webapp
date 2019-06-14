f3daebd3301e18189e60f3001584770c78386167
Michał Podlecki <m.podlecki@op.pl>
2019-06-14 09:08:44 +0200

    fix: disable js compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
