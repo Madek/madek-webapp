de3f0933889593cc98ca8b9cc21fc0b6440b5b8a
Matus Kmit <matuskmit1@gmail.com>
2023-05-09 16:27:20 +0530

    chore: deprecation warnings irt rails 7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-Madek::Application.config.session_store :cookie_store,
-  key: Madek::Constants::Webapp::SESSION_NAME,
-  httponly: true,
-  secure: (Rails.env == 'production' && !Madek::Constants::MADEK_DISABLE_HTTPS)
+Rails.application.reloader.to_prepare do
+  Madek::Application.config.session_store :cookie_store,
+    key: Madek::Constants::Webapp::SESSION_NAME,
+    httponly: true,
+    secure: (Rails.env == 'production' && !Madek::Constants::MADEK_DISABLE_HTTPS)
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
