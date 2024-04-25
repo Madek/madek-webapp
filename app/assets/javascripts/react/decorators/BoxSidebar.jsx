eed2b54d0519791f28c2d0cb64f41882d8bc9df9
uvb <urs.vanbinsbergen@zhdk.ch>
2024-04-25 16:13:46 +0200

    chore: autoformat

--- app/assets/javascripts/react/decorators/BoxSidebar.jsx
+++ app/assets/javascripts/react/decorators/BoxSidebar.jsx
@@ -10,71 +10,71 @@ import t from '../../lib/i18n-translate.js'
 import { urlByType } from '../lib/resource-type-switcher.cjsx'
 
 class BoxSidebar extends React.Component {
-
   constructor(props) {
     super(props)
   }
 
   has_filename_filter() {
     var config = this.props.config
-    return (config.filter &&
+    return (
+      config.filter &&
       config.filter.media_files &&
-      !l.isEmpty(l.filter(config.filter.media_files, (entry) => {
-        entry.key == 'filename'
-      }))
+      !l.isEmpty(
+        l.filter(config.filter.media_files, entry => {
+          entry.key == 'filename'
+        })
+      )
     )
   }
 
   filename_filter_string() {
     var config = this.props.config
-    return l.first(l.filter(config.filter.media_files, (entry) => {
-      entry.key == 'filename'
-    })).value
+    return l.first(
+      l.filter(config.filter.media_files, entry => {
+        entry.key == 'filename'
+      })
+    ).value
   }
 
   fulltext_filter_string() {
     var config = this.props.config
-    if(config.filter)
-      return config.filter.search
-    else
-      return ''
+    if (config.filter) return config.filter.search
+    else return ''
   }
 
   filterExamples() {
     return {
       "Search: 'still'": {
-        "search": "still"
+        search: 'still'
       },
       "Title: 'diplom'": {
-        "meta_data": [{ "key": "madek_core:title", "match": "diplom" }]
+        meta_data: [{ key: 'madek_core:title', match: 'diplom' }]
       },
       "Uses Meta-Key 'Gattung'": {
-        "meta_data": [ { "key": "media_content:type" } ]
+        meta_data: [{ key: 'media_content:type' }]
       },
-      "Permissions: public": {
-        "permissions": [{ "key": "public", "value": true }]
+      'Permissions: public': {
+        permissions: [{ key: 'public', value: true }]
       },
-      "Media File: Content-Type jpeg": {
-        "media_files": [{ "key": "content_type", "value": "image/jpeg" }]
+      'Media File: Content-Type jpeg': {
+        media_files: [{ key: 'content_type', value: 'image/jpeg' }]
       },
-      "Media File: Extension pdf": {
-        "media_files": [{ "key": "extension", "value": "pdf" }]
+      'Media File: Extension pdf': {
+        media_files: [{ key: 'extension', value: 'pdf' }]
       }
     }
   }
 
   renderFilterPreloader() {
     return (
-      <div className='ui-slide-filter-item'>
-        <div className='title-xs by-center'>
-          Filter werden geladen</div>
-          <Preloader mods='small'/>
+      <div className="ui-slide-filter-item">
+        <div className="title-xs by-center">Filter werden geladen</div>
+        <Preloader mods="small" />
       </div>
     )
   }
 
   renderFilterExamples() {
-
     var url = this.props.config.for_url
     var query = this.props.currentQuery
 
@@ -82,19 +82,14 @@ class BoxSidebar extends React.Component {
       <div>
         <h4>Examples:</h4>
         <ul>
-          {
-            l.map(
-              this.filterExamples(),
-              (example, name) => {
-                var params = {list: {page: 1, filter: JSON.stringify(example, 0, 2)}}
-                return (
-                  <li key={name}>
-                    <Link href={setUrlParams(url, query, params)}>{name}</Link>
-                  </li>
-                )
-              }
+          {l.map(this.filterExamples(), (example, name) => {
+            var params = { list: { page: 1, filter: JSON.stringify(example, 0, 2) } }
+            return (
+              <li key={name}>
+                <Link href={setUrlParams(url, query, params)}>{name}</Link>
+              </li>
             )
-          }
+          })}
         </ul>
       </div>
     )
@@ -102,15 +97,18 @@ class BoxSidebar extends React.Component {
 
   renderSideFilterFallback() {
     var filter = this.props.config.filter
-    filter = (filter ? filter : {})
+    filter = filter ? filter : {}
     return (
-      <div className='ui-side-filter-search filter-search'>
-        <RailsForm name='list' method='get' mods='prm'>
-          <input type='hidden' name='list[show_filter]' value='true'/>
-          <textarea name='list[filter]' rows='25'
-            style={{fontFamily: 'monospace', fontSize: '1.1em', width: '100%'}}
-            defaultValue={JSON.stringify(filter, 0, 2)}/>
-          <Button type='submit'>Submit</Button>
+      <div className="ui-side-filter-search filter-search">
+        <RailsForm name="list" method="get" mods="prm">
+          <input type="hidden" name="list[show_filter]" value="true" />
+          <textarea
+            name="list[filter]"
+            rows="25"
+            style={{ fontFamily: 'monospace', fontSize: '1.1em', width: '100%' }}
+            defaultValue={JSON.stringify(filter, 0, 2)}
+          />
+          <Button type="submit">Submit</Button>
         </RailsForm>
       </div>
     )
@@ -121,46 +119,52 @@ class BoxSidebar extends React.Component {
     var currentQuery = this.props.currentQuery
     return (
       <div>
-        <div className='no-js'>
+        <div className="no-js">
           {this.renderSideFilterFallback()}
           {this.renderFilterExamples()}
         </div>
-        <div className='js-only'>
-          {this.renderFilterPreloader()}
-        </div>
+        <div className="js-only">{this.renderFilterPreloader()}</div>
       </div>
     )
   }
 
   searchValue() {
-    if(this.has_filename_filter()) {
+    if (this.has_filename_filter()) {
       return this.filename_filter_string()
     } else {
       return this.fulltext_filter_string()
     }
-
   }
 
   renderFilesearch() {
-    if(!this.props.supportsFilesearch) {
+    if (!this.props.supportsFilesearch) {
       return null
     }
 
     return (
-      <div style={{marginTop: '2px'}}>
-        <input ref='searchTypeFulltext' type='radio' name='search_type'
-          value='fulltext' defaultChecked={(!this.has_filename_filter())} />
+      <div style={{ marginTop: '2px' }}>
+        <input
+          ref="searchTypeFulltext"
+          type="radio"
+          name="search_type"
+          value="fulltext"
+          defaultChecked={!this.has_filename_filter()}
+        />
         {' ' + t('search_full_text') + ' '}
-        <input ref='searchTypeFilename' type='radio' name='search_type'
-          value='filename' defaultChecked={this.has_filename_filter()} />
+        <input
+          ref="searchTypeFilename"
+          type="radio"
+          name="search_type"
+          value="filename"
+          defaultChecked={this.has_filename_filter()}
+        />
         {' ' + t('search_filename')}
       </div>
     )
   }
 
   renderSideFilter() {
-
-    if(this.props.onlyFilterSearch) {
+    if (this.props.onlyFilterSearch) {
       return null
     }
 
@@ -168,42 +172,38 @@ class BoxSidebar extends React.Component {
       <SideFilter
         forUrl={this.props.parentState.config.for_url}
         jsonPath={this.props.jsonPath}
-        current={(this.props.config.filter ? this.props.config.filter : {})}
-        accordion={(this.props.config.accordion ? this.props.config.accordion : {})}
+        current={this.props.config.filter ? this.props.config.filter : {}}
+        accordion={this.props.config.accordion ? this.props.config.accordion : {}}
         onChange={this.props.onSideFilterChange}
       />
     )
   }
 
-
   _onSearch(event) {
     var refs = this.refs
 
-    var searchTypeFulltextChecked = (refs.searchTypeFulltext ?
-      refs.searchTypeFulltext.checked : null)
-    var searchTypeFilenameChecked = (refs.searchTypeFilename ?
-      refs.searchTypeFilename.checked : null)
+    var searchTypeFulltextChecked = refs.searchTypeFulltext ? refs.searchTypeFulltext.checked : null
+    var searchTypeFilenameChecked = refs.searchTypeFilename ? refs.searchTypeFilename.checked : null
     var filterSearchValue = refs.filterSearch.value
-    this.props.onSearch(
-      event,
-      {
-        searchTypeFulltextChecked: searchTypeFulltextChecked,
-        searchTypeFilenameChecked: searchTypeFilenameChecked,
-        filterSearchValue: filterSearchValue
-      }
-    )
+    this.props.onSearch(event, {
+      searchTypeFulltextChecked: searchTypeFulltextChecked,
+      searchTypeFilenameChecked: searchTypeFilenameChecked,
+      filterSearchValue: filterSearchValue
+    })
   }
 
   renderJs() {
     return (
-      <div className='js-only'>
-        <div className='ui-side-filter-search filter-search'>
-          <form name='filter_search_form' onSubmit={(e) => this._onSearch(e)}>
-            <input type='submit' className='unstyled'
-              value={t('resources_box_new_search')} />
-            <input type='text' className='ui-filter-search-input block'
-              ref='filterSearch'
-              defaultValue={this.searchValue()}/>
+      <div className="js-only">
+        <div className="ui-side-filter-search filter-search">
+          <form name="filter_search_form" onSubmit={e => this._onSearch(e)}>
+            <input type="submit" className="unstyled" value={t('resources_box_new_search')} />
+            <input
+              type="text"
+              className="ui-filter-search-input block"
+              ref="filterSearch"
+              defaultValue={this.searchValue()}
+            />
             {this.renderFilesearch()}
           </form>
         </div>
@@ -213,7 +213,7 @@ class BoxSidebar extends React.Component {
   }
 
   renderJsSwitch() {
-    if(!this.props.isClient) {
+    if (!this.props.isClient) {
       return this.renderNoJs()
     } else {
       return this.renderJs()
@@ -228,15 +228,11 @@ class BoxSidebar extends React.Component {
     }
 
     return (
-      <div className='mtm'>
-        {t('resources_box_filters_note_pre')}
-        {' '}
-        <Link href={urlByType(currentUrl, null, 'entries')}>{t('sitemap_entries')}</Link>
-        {' '}
-        {t('resources_box_filters_note_or')}
-        {' '}
-        <Link href={urlByType(currentUrl, null, 'collections')}>{t('sitemap_collections')}</Link>
-        {' '}
+      <div className="mtm">
+        {t('resources_box_filters_note_pre')}{' '}
+        <Link href={urlByType(currentUrl, null, 'entries')}>{t('sitemap_entries')}</Link>{' '}
+        {t('resources_box_filters_note_or')}{' '}
+        <Link href={urlByType(currentUrl, null, 'collections')}>{t('sitemap_collections')}</Link>{' '}
         {t('resources_box_filters_note_post')}
       </div>
     )
@@ -244,7 +240,7 @@ class BoxSidebar extends React.Component {
 
   render() {
     return (
-      <div className='filter-panel ui-side-filter'>
+      <div className="filter-panel ui-side-filter">
         {this.renderJsSwitch()}
         {this.renderFiltersNote()}
       </div>
