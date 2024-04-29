b924cd0a01a4efb8c44832217f235666775cc1d2
uvb <urs.vanbinsbergen@zhdk.ch>
2024-04-29 15:43:24 +0200

    fix: explore keywords + add spec

---
+++ spec/features/explore_keywords_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'spec_helper_feature'
+require 'spec_helper_feature_shared'
+
+feature 'Page: Explore Keywords' do
+
+  describe 'Action: index' do
+
+    it 'is rendered for public', browser: false do
+      visit explore_keywords_path
+      expect(page.status_code).to eq 200
+    end
+
+    it 'is rendered for a logged in user' do
+      @user = User.find_by(login: 'normin')
+      sign_in_as @user.login
+      visit explore_keywords_path
+      binding.pry
+    end
+
+  end
+
+end
