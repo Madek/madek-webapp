f0ad2288d9879838b4128ad83aa734e577696671
Matus Kmit <matuskmit1@gmail.com>
2024-10-03 11:41:44 +0200

    fix: zencoder jobs & access hashes

--- app/controllers/media_files_controller.rb
+++ app/controllers/media_files_controller.rb
@@ -8,16 +8,16 @@ class MediaFilesController < ApplicationController
   # Check if associated resource is soft-deleted and thus sort
   # of does not exist (excluded in default scope).
   before_action do
-    mf = MediaFile.find(params.require(:id))
-    unless MediaEntry.unscoped.not_deleted.find_by_id(mf.media_entry_id)
+    @media_file = MediaFile.find(params.require(:id))
+    unless MediaEntry.unscoped.not_deleted.find_by_id(@media_file.media_entry_id)
       raise ActiveRecord::RecordNotFound, "MediaFile not found"
     end
   end
   #############################################################
 
   def show
-    if access_hash_param?
-      download_by_access_hash
+    if access_token_param
+      download_by_access_token
     else
       download_by_user_permissions
     end
@@ -26,22 +26,19 @@ class MediaFilesController < ApplicationController
   private
 
   def download_by_user_permissions
-    media_file = MediaFile.find_by!(id: id_param)
-    auth_authorize(media_file)
+    auth_authorize(@media_file)
     serve_file(
-      media_file.original_store_location,
-      content_type: get_content_type(media_file),
-      filename: media_file.filename)
+      @media_file.original_store_location,
+      content_type: get_content_type(@media_file),
+      filename: @media_file.filename)
   end
 
-  def download_by_access_hash
+  def download_by_access_token
     skip_authorization # do custom auth because there is no logged in user
-    access_hash_param = params.require(:access_hash)
-    media_file = MediaFile.find_by!(id: id_param, access_hash: access_hash_param)
-    serve_file(
-      media_file.original_store_location,
-      content_type: get_content_type(media_file),
-      filename: media_file.filename)
+    validate_access_token_param!
+    serve_file(@media_file.original_store_location,
+               content_type: get_content_type(@media_file),
+               filename: @media_file.filename)
   end
 
   def get_content_type(media_file)
@@ -58,7 +55,19 @@ class MediaFilesController < ApplicationController
     params.require(:id)
   end
 
-  def access_hash_param?
-    params.permit(:access_hash).fetch(:access_hash, nil).present?
+  def access_token_param
+    params.permit(:access_token).fetch(:access_token) do
+      # Using `access_hash` fallback only for backwards compatibility
+      # with old zencoder jobs.
+      params.permit(:access_hash).fetch(:access_hash, nil)
+    end.presence
+  end
+
+  def validate_access_token_param!
+    zj = ZencoderJob.find_by!(access_token: access_token_param,
+                              media_file_id: @media_file.id)
+    unless zj.access_token_valid_until > Time.now 
+      raise "Access token expired"
+    end
   end
 end
