30ae42e03b7b83d5f7b931254ec6b71c1e656ebc
Matus Kmit <matuskmit1@gmail.com>
2025-01-08 17:00:48 +0100

    fix rails-csrf-token.js by hand

--- app/javascript/lib/rails-csrf-token.js
+++ app/javascript/lib/rails-csrf-token.js
@@ -1,16 +1,10 @@
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__, or convert again using --optional-chaining
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
  */
 let getRailsCSRFToken;
-module.exports = (getRailsCSRFToken= () => __guardMethod__(__guardMethod__(document, 'querySelector', o1 => o1.querySelector('meta[name="csrf-token"]')), 'getAttribute', o => o.getAttribute('content')));
-
-function __guardMethod__(obj, methodName, transform) {
-  if (typeof obj !== 'undefined' && obj !== null && typeof obj[methodName] === 'function') {
-    return transform(obj, methodName);
-  } else {
-    return undefined;
-  }
-}
+module.exports = (getRailsCSRFToken = () => {
+  const metaTag = typeof document !== 'undefined' && document.querySelector('meta[name="csrf-token"]');
+  return metaTag ? metaTag.getAttribute('content') : null;
+});
