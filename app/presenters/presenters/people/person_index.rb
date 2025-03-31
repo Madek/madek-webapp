af2d622ade41b27a10612471ffd62a452b82d78c
uvb <urs.vanbinsbergen@zhdk.ch>
2025-03-31 13:56:59 +0200

    wip

--- app/presenters/presenters/people/person_index.rb
+++ app/presenters/presenters/people/person_index.rb
@@ -10,14 +10,14 @@ module Presenters
       def info
         fields = AppSetting.first.person_info_fields
         fragments = []
+        if fields.include?('institutional_id') && @app_resource.institutional_id
+          fragments << "#{@app_resource.institution} #{@app_resource.institutional_id}"
+        end
         if fields.include?('identification_info') && @app_resource.identification_info
           fragments << @app_resource.identification_info
         end
         if fields.include?('institutional_directory_infos') && @app_resource.institutional_directory_infos.any?
           fragments += @app_resource.institutional_directory_infos
-        end
-        if fields.include?('institutional_id') && @app_resource.institutional_id
-          fragments << "#{@app_resource.institution} #{@app_resource.institutional_id}"
         end
         fragments
       end
