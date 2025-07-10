88bd44de78f7067041ce77623aa0470d826957ca
Matus Kmit <matuskmit1@gmail.com>
2025-07-10 10:55:57 +0200

    root haml

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -2,4 +2,8 @@ module CacheHelper
   def cache_key_for_user_menu(user, data)
     "user/#{user.id}/menu-data/#{data.hash}"
   end
+
+  def cache_key_for_root(data)
+    "root-page-data/#{data.hash}"
+  end
 end
