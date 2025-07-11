993da27c60254c66cb60690f9ae1de942a85fc1c
Matus Kmit <matuskmit1@gmail.com>
2025-07-11 13:40:43 +0200

    featured section

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -2,12 +2,4 @@ module CacheHelper
   def cache_key_for_user_menu(user, data)
     "user/#{user.id}/menu-data/#{data.hash}"
   end
-
-  # def cache_key_for_root(data)
-  #   "root-page-data/#{data.hash}"
-  # end
-
-  def cache_key_for_root_latest_media_entry(media_entry)
-    "root-latest-media-entry/#{media_entry.created_at.to_i}"
-  end
 end
