b0a03fbbf0f530acf2d1981f75bb24965247a99c
mradl <manuel.radl@zhdk.ch>
2025-08-06 13:58:06 +0200

    wip v2

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,7 +2,21 @@ SitemapGenerator::Sitemap.default_host = Settings.madek_external_base_url
 SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/"
 SitemapGenerator::Sitemap.create_index = true
 
+
+
 SitemapGenerator::Sitemap.create do
+  def create_url (path, lang = "de", with_base_url = true)
+    postfix = lang == "de" ? "" : "?lang=#{lang}"
+    # "#{Settings.madek_external_base_url}#{path}#{postfix}"
+
+    if with_base_url
+      "#{Settings.madek_external_base_url}#{path}#{postfix}"
+    else
+      "#{path}#{postfix}"
+    end
+
+  end
+
   add root_path, priority: 1.0, changefreq: 'daily'
 
   MediaEntry.viewable_by_public.find_in_batches(batch_size: 10000) do |batch|
@@ -11,26 +25,27 @@ SitemapGenerator::Sitemap.create do
       puts "Processing MediaEntry: #{media_entry.id} - #{media_entry.title}"
 
       ["de", "en"].each do |lang|
-        postfix = lang == "de" ? "" : "?lang=#{lang}"
+        # postfix = lang == "de" ? "" : "?lang=#{lang}"
         priority = lang == "de" ? 0.8 : 0.7
-        url = "#{Settings.madek_external_base_url}#{media_entry_path(media_entry)}#{postfix}"
-        puts "url : #{url}"
+        # url = "#{Settings.madek_external_base_url}#{media_entry_path(media_entry)}#{postfix}"
+        path = media_entry_path(media_entry)
+        puts "path : #{path}"
         updated_at = media_entry.updated_at
         alternatives = [{
-                          href: url,
+                          href: create_url(path, 'en'),
                           lastmod: updated_at,
                           lang: 'en'
                         }, {
-                          href: url,
+                          href: create_url(path),
                           lastmod: updated_at,
                           lang: 'de'
                         }, {
-                          href: url,
+                          href:  create_url(path),
                           lastmod: updated_at,
                           lang: 'x-default'
                         }]
 
-        add "#{media_entry_path(media_entry)}#{postfix}",
+        add  create_url(path, lang, false),
             lastmod: updated_at,
             priority: priority,
             alternates: alternatives
