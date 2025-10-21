1b5e2674524970981ccd7fb4ecc8b95f1498453b
Jan Pistor <pistor.jan@gmail.com>
2025-10-21 09:30:04 +0200

    batch 3

--- app/javascript/react/ui-components/AskModal.jsx
+++ app/javascript/react/ui-components/AskModal.jsx
@@ -1,56 +1,44 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import React from 'react'
-import createReactClass from 'create-react-class'
 import FormButton from './FormButton.jsx'
 import Modal from './Modal.jsx'
 import Preloader from './Preloader.jsx'
 
-module.exports = createReactClass({
-  displayName: 'AskModal',
-
-  render(param) {
-    if (param == null) {
-      param = this.props
-    }
-    const { title, error, onOk, onCancel, okText, cancelText, loading } = param
-    return (
-      <Modal widthInPixel="400">
-        <div className="ui-modal-head">
-          <a
-            onClick={onCancel}
-            aria-hidden="true"
-            className="ui-modal-close"
-            data-dismiss="modal"
-            title="Close"
-            type="button"
-            style={{ position: 'static', float: 'right', paddingTop: '5px' }}>
-            <i className="icon-close" />
-          </a>
-          <h3 className="title-l">{title}</h3>
-        </div>
-        <div className="ui-modal-body" style={{ maxHeight: 'none' }}>
-          {loading ? <Preloader /> : undefined}
-          {error && !loading ? (
-            <div className="ui-alerts">
-              <div className="error ui-alert">{error}</div>
-            </div>
-          ) : undefined}
-          {!loading ? this.props.children : undefined}
-        </div>
-        <div className="ui-modal-footer">
-          <div className="ui-actions">
-            <a onClick={onCancel} aria-hidden="true" className="link weak" data-dismiss="modal">
-              {cancelText}
-            </a>
-            <FormButton onClick={onOk} text={okText} />
+const AskModal = ({ title, error, onOk, onCancel, okText, cancelText, loading, children }) => {
+  return (
+    <Modal widthInPixel="400">
+      <div className="ui-modal-head">
+        <a
+          onClick={onCancel}
+          aria-hidden="true"
+          className="ui-modal-close"
+          data-dismiss="modal"
+          title="Close"
+          type="button"
+          style={{ position: 'static', float: 'right', paddingTop: '5px' }}>
+          <i className="icon-close" />
+        </a>
+        <h3 className="title-l">{title}</h3>
+      </div>
+      <div className="ui-modal-body" style={{ maxHeight: 'none' }}>
+        {loading && <Preloader />}
+        {error && !loading && (
+          <div className="ui-alerts">
+            <div className="error ui-alert">{error}</div>
           </div>
+        )}
+        {!loading && children}
+      </div>
+      <div className="ui-modal-footer">
+        <div className="ui-actions">
+          <a onClick={onCancel} aria-hidden="true" className="link weak" data-dismiss="modal">
+            {cancelText}
+          </a>
+          <FormButton onClick={onOk} text={okText} />
         </div>
-      </Modal>
-    )
-  }
-})
+      </div>
+    </Modal>
+  )
+}
+
+export default AskModal
+module.exports = AskModal
