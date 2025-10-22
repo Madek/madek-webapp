3137925fe90275431c320dc27da80bb3a242e5c5
Jan Pistor <pistor.jan@gmail.com>
2025-10-22 12:48:09 +0200

    fix: restore AsyncModal for now

--- app/javascript/react/views/Collection/AsyncModal.jsx
+++ app/javascript/react/views/Collection/AsyncModal.jsx
@@ -1,48 +1,73 @@
-import React, { useState, useEffect, useRef } from 'react'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+import React from 'react'
+import createReactClass from 'create-react-class'
 import Modal from '../../ui-components/Modal.jsx'
 import loadXhr from '../../../lib/load-xhr.js'
 
-const AsyncModal = ({ get: initialGet, getUrl, contentForGet, extractGet, widthInPixel }) => {
-  const [get, setGet] = useState(initialGet || null)
-  const [children, setChildren] = useState(initialGet ? contentForGet(initialGet) : null)
-  const isMountedRef = useRef(true)
+module.exports = createReactClass({
+  displayName: 'AsyncModal',
 
-  useEffect(() => {
-    // Always make the XHR request, even if initialGet was provided
-    loadXhr(
+  getInitialState() {
+    return {
+      mounted: false,
+      loading: false,
+      errors: null,
+      get: null,
+      searching: false,
+      searchTerm: '',
+      newSets: [],
+      children: null
+    }
+  },
+
+  lastRequest: null,
+
+  UNSAFE_componentWillMount() {
+    if (this.props.get) {
+      return this.setState({
+        get: this.props.get,
+        children: this.props.contentForGet(this.props.get)
+      })
+    }
+  },
+
+  componentDidMount() {
+    this.setState({ ready: true, mounted: true, loading: true })
+
+    return loadXhr(
       {
         method: 'GET',
-        url: getUrl
+        url: this.props.getUrl
       },
       (result, json) => {
-        if (!isMountedRef.current) {
+        if (!this.isMounted()) {
           return
         }
         if (result === 'success') {
-          const fetchedGet = extractGet(json)
-          setGet(fetchedGet)
-          setChildren(contentForGet(fetchedGet))
+          const get = this.props.extractGet(json)
+          return this.setState({ loading: false, get, children: this.props.contentForGet(get) })
         } else {
           console.error(`Cannot load dialog: ${JSON.stringify(json)}`)
+          return this.setState({ loading: false })
         }
       }
     )
+  },
 
-    return () => {
-      isMountedRef.current = false
+  render() {
+    if (!this.state.get) {
+      return <Modal loading={true} widthInPixel={this.props.widthInPixel} />
+    } else {
+      return (
+        <Modal loading={false} widthInPixel={this.props.widthInPixel}>
+          {this.state.children}
+        </Modal>
+      )
     }
-  }, [getUrl, extractGet, contentForGet])
-
-  if (!get) {
-    return <Modal loading={true} widthInPixel={widthInPixel} />
   }
-
-  return (
-    <Modal loading={false} widthInPixel={widthInPixel}>
-      {children}
-    </Modal>
-  )
-}
-
-export default AsyncModal
-module.exports = AsyncModal
+})
