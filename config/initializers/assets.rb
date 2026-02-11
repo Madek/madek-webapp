afda59a5ee7882153fe3d8a83abb84866a536f24
uvb <urs.vanbinsbergen@zhdk.ch>
2026-02-11 12:34:30 +0100

    chore: automate `npm ci` in dev env

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
+if Rails.env.development?
+  puts "run `npm ci` to make sure `node_modules` are up to date..."
+  system('npm ci')
+end
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
